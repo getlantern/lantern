759c3916fcb414dad2b3b8aa099f88967285513e
myleshorton <myles@getlantern.org>
2010-07-10 22:47:11 +0000

    Cleanups
    git-svn-id: http://svn.littleshoot.org/svn/mg/trunk@3 c7e17473-8bbb-4479-a492-429a95f5942b

--- bin/deployGData.bash
+++ bin/deployGData.bash
@@ -3,25 +3,32 @@
 # This is a script for automatically deploying the GData jars to the LittleShoot
 # maven repository. 
 
+gdataVersion=$1
+
 function die() {
+    rm -rf $gdataVersion
     echo $*
     exit 1
 }
 
+me=`basename $0`
+
 if [ $# -ne 1 ]
 then
-  die "Should be a single GData version argument, as in '2.2.1-alpha'. For versions, see: http://gdata-java-client.googlecode.com/svn/tags/"
+  "Should be a single GData version argument, as in './$me 2.2.1-alpha'. For versions, see: http://gdata-java-client.googlecode.com/svn/tags/"
+  exit 1
 fi
 
-echo "First argument should be of the form '2.2.1-alpha', for example"
-gdataVersion=$1
-gdataBase=~/code/google/gdata-svn/$gdataVersion
 
 echo "Checking out GData version $gdataVersion"
-svn co http://gdata-java-client.googlecode.com/svn/tags/$gdataVersion $gdataBase || die "Could not checkout SVN"
+svn co http://gdata-java-client.googlecode.com/svn/tags/$gdataVersion || die "Could not checkout SVN"
 
-cd $gdataBase/gdata || die "Could not move to gdata base dir"
+pushd $gdataVersion/gdata || die "Could not move to gdata base dir"
 ant dist || die "Could not build with ant"
 cd build-bin/dist/ || die "Could not cd to dist directory"
 
 mvn deploy:deploy-file -DgroupId=com.google.gdata -DartifactId=gdata -Dversion=$gdataVersion -Dfile=gdata-$gdataVersion.jar -Dpackaging=jar -DgeneratePom=true -Durl=scpexe://dev.littleshoot.org/var/maven -DrepositoryId=littleshoot || die "Could not deploy file"
+
+echo "Cleaning up"
+popd
+rm -rf $gdataVersion
