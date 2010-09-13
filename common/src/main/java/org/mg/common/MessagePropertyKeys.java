71aac343d719f75d4c3a9f7849948fcd54fc2fbf
myleshorton <myles@getlantern.org>
2010-09-13 19:24:56 +0000

    added constant keys
    git-svn-id: http://svn.littleshoot.org/svn/mg/trunk@94 c7e17473-8bbb-4479-a492-429a95f5942b

---
+++ common/src/main/java/org/mg/common/MessagePropertyKeys.java
@@ -0,0 +1,23 @@
+package org.mg.common;
+
+/**
+ * Class containing constants for message property keys.
+ */
+public class MessagePropertyKeys {
+
+    private MessagePropertyKeys() {}
+    
+    public static final String MAC = "MAC";
+    
+    public static final String HASHCODE = "HASHCODE";
+
+    public static final String SEQ = "SEQ";
+
+    public static final String MD5 = "MD5";
+
+    public static final String HTTP = "HTTP";
+
+    public static final String SERVER_MAC = "SMAC";
+
+    public static final String CLOSE = "CLOSE";
+}
