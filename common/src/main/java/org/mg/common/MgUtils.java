abb79a58bc519e4ee9b91240a6c71df09c48e255
myleshorton <myles@getlantern.org>
2010-09-14 00:39:55 +0000

    More error display
    git-svn-id: http://svn.littleshoot.org/svn/mg/trunk@108 c7e17473-8bbb-4479-a492-429a95f5942b

--- common/src/main/java/org/mg/common/MgUtils.java
+++ common/src/main/java/org/mg/common/MgUtils.java
@@ -9,6 +9,7 @@ import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelFutureListener;
 import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.XMPPError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -48,25 +49,39 @@ public class MgUtils {
     }
 
     public static void printMessage(final Message msg) {
+        final XMPPError error = msg.getError();
         final StringBuilder sb = new StringBuilder();
-        sb.append("MESSAGE:");
-        sb.append("\nBODY:");
+        sb.append("MESSAGE: ");
+        sb.append("\nBODY: ");
         sb.append(msg.getBody());
-        sb.append("\nFROM:");
+        sb.append("\nFROM: ");
         sb.append(msg.getFrom());
-        sb.append("\nTO:");
+        sb.append("\nTO: ");
         sb.append(msg.getTo());
-        sb.append("\nSUBJECT:");
+        sb.append("\nSUBJECT: ");
         sb.append(msg.getSubject());
-        sb.append("\nPACKET ID:");
+        sb.append("\nPACKET ID: ");
         sb.append(msg.getPacketID());
-        sb.append("\nERROR:");
-        sb.append(msg.getError());
-        sb.append("\nEXTENSIONS:");
+        
+        sb.append("\nERROR: ");
+        if (error != null) {
+            sb.append(error);
+            sb.append("\nCODE: ");
+            sb.append(error.getCode());
+            sb.append("\nMESSAGE: ");
+            sb.append(error.getMessage());
+            sb.append("\nCONDITION: ");
+            sb.append(error.getCondition());
+            sb.append("\nEXTENSIONS: ");
+            sb.append(error.getExtensions());
+            sb.append("\nTYPE: ");
+            sb.append(error.getType());
+        }
+        sb.append("\nEXTENSIONS: ");
         sb.append(msg.getExtensions());
-        sb.append("\nTYPE:");
+        sb.append("\nTYPE: ");
         sb.append(msg.getType());
-        sb.append("\nPROPERTY NAMES:");
+        sb.append("\nPROPERTY NAMES: ");
         sb.append(msg.getPropertyNames());
         LOG.warn(sb.toString());
     }
