ebb90b0fc4cfa737ff0a3c8d5cc9b265e03a32ab
myleshorton <myles@getlantern.org>
2010-10-21 19:06:51 +0000

    new constants class
    git-svn-id: http://svn.littleshoot.org/svn/mg/trunk@138 c7e17473-8bbb-4479-a492-429a95f5942b

---
+++ common/src/main/java/org/mg/common/LanternConstants.java
@@ -0,0 +1,14 @@
+package org.mg.common;
+
+/**
+ * Constants for Lantern code.
+ */
+public class LanternConstants {
+
+    private LanternConstants() {}
+    
+    /**
+     * The port lantern runs its HTTP proxy on.
+     */
+    public static final int LANTERN_PROXY_PORT = 8777;
+}
