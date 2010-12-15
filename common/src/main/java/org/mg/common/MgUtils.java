9960788bee449ad5bb4e18415020e35b65a421d5
myleshorton <myles@getlantern.org>
2010-12-15 20:44:48 +0000

    Cleanup
    git-svn-id: http://svn.littleshoot.org/svn/mg/trunk@152 c7e17473-8bbb-4479-a492-429a95f5942b

--- common/src/main/java/org/mg/common/MgUtils.java
+++ common/src/main/java/org/mg/common/MgUtils.java
@@ -8,8 +8,6 @@ import org.apache.commons.codec.binary.Base64;
 import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelFutureListener;
-import org.jivesoftware.smack.packet.Message;
-import org.jivesoftware.smack.packet.XMPPError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -46,43 +44,5 @@ public class MgUtils {
         buf.get(bytes);
         buf.position(mark);
         return bytes;
-    }
-
-    public static void printMessage(final Message msg) {
-        final XMPPError error = msg.getError();
-        final StringBuilder sb = new StringBuilder();
-        sb.append("MESSAGE: ");
-        sb.append("\nBODY: ");
-        sb.append(msg.getBody());
-        sb.append("\nFROM: ");
-        sb.append(msg.getFrom());
-        sb.append("\nTO: ");
-        sb.append(msg.getTo());
-        sb.append("\nSUBJECT: ");
-        sb.append(msg.getSubject());
-        sb.append("\nPACKET ID: ");
-        sb.append(msg.getPacketID());
-        
-        sb.append("\nERROR: ");
-        if (error != null) {
-            sb.append(error);
-            sb.append("\nCODE: ");
-            sb.append(error.getCode());
-            sb.append("\nMESSAGE: ");
-            sb.append(error.getMessage());
-            sb.append("\nCONDITION: ");
-            sb.append(error.getCondition());
-            sb.append("\nEXTENSIONS: ");
-            sb.append(error.getExtensions());
-            sb.append("\nTYPE: ");
-            sb.append(error.getType());
-        }
-        sb.append("\nEXTENSIONS: ");
-        sb.append(msg.getExtensions());
-        sb.append("\nTYPE: ");
-        sb.append(msg.getType());
-        sb.append("\nPROPERTY NAMES: ");
-        sb.append(msg.getPropertyNames());
-        LOG.warn(sb.toString());
     }
 }
