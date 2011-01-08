4ea448a97a5f6b87078729d44e732592d9cbddfc
myleshorton <myles@getlantern.org>
2011-01-08 20:28:10 +0000

    New utils class
    git-svn-id: http://svn.littleshoot.org/svn/mg/trunk@190 c7e17473-8bbb-4479-a492-429a95f5942b

---
+++ client/src/main/java/org/mg/client/LanternUtils.java
@@ -0,0 +1,61 @@
+package org.mg.client;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+import java.util.Random;
+
+import org.apache.commons.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LanternUtils {
+
+    private static final Logger LOG = 
+        LoggerFactory.getLogger(LanternUtils.class);
+    
+    private static String MAC_ADDRESS;
+    
+    public static String getMacAddress() {
+        if (MAC_ADDRESS != null) {
+            return MAC_ADDRESS;
+        }
+        final Enumeration<NetworkInterface> nis;
+        try {
+            nis = NetworkInterface.getNetworkInterfaces();
+        } catch (final SocketException e1) {
+            throw new Error("Could not read network interfaces?");
+        }
+        while (nis.hasMoreElements()) {
+            final NetworkInterface ni = nis.nextElement();
+            try {
+                if (!ni.isUp()) {
+                    LOG.info("Ignoring interface that's not up: {}", 
+                        ni.getDisplayName());
+                    continue;
+                }
+                final byte[] mac = ni.getHardwareAddress();
+                if (mac != null && mac.length > 0) {
+                    LOG.info("Returning 'normal' MAC address");
+                    MAC_ADDRESS = Base64.encodeBase64String(mac).trim();
+                    return MAC_ADDRESS;
+                }
+            } catch (final SocketException e) {
+                LOG.warn("Could not get MAC address?");
+            }
+        }
+        try {
+            LOG.warn("Returning custom MAC address");
+            MAC_ADDRESS = Base64.encodeBase64String(
+                InetAddress.getLocalHost().getAddress()) + 
+                System.currentTimeMillis();
+            return MAC_ADDRESS;
+        } catch (final UnknownHostException e) {
+            final byte[] bytes = new byte[24];
+            new Random().nextBytes(bytes);
+            return Base64.encodeBase64String(bytes);
+        }
+    }
+}
