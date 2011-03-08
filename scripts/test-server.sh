570696b8957f4afdc2098767d44c684f3e8f2783
Igor Minar <iiminar@gmail.com>
2011-03-08 11:27:44 -0800

    improving the test-server.sh console output

--- scripts/test-server.sh
+++ scripts/test-server.sh
@@ -3,10 +3,8 @@
 base_dir=`dirname $0`
 port=9876
 
-echo "Starting JSTD server at http://localhost:$port"
-echo ""
-echo "Please open the url above and capture one or more browsers."
-echo ""
-echo "For more info please see: http://code.google.com/p/js-test-driver/"
+echo "Starting JsTestDriver Server (http://code.google.com/p/js-test-driver/)"
+echo "Please open the following url and capture one or more browsers:"
+echo "http://localhost:$port"
 
 java -jar "$base_dir/../test/lib/jstestdriver/JsTestDriver.jar" --port $port --browserTimeout 20000 --config "$base_dir/../config/jsTestDriver.conf" --basePath "$base_dir/.."
