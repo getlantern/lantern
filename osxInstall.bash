4f78a44a29013381d31a0c6194b24c7e3232abf4
myleshorton <myles@getlantern.org>
2011-03-07 16:59:40 -0800

    tweaks for new directory structure

--- osxInstall.bash
+++ osxInstall.bash
@@ -10,14 +10,14 @@ then
     die "$0: Received $# args... version required"
 fi
 
-cd client
+#cd client
 mvn clean || die "Could not clean?"
 mvn install -Dmaven.test.skip=true || die "Could not build?"
 cd -
-cp client/target/lantern-client-*-jar-with-dependencies.jar install/common/lantern.jar || die "Could not copy jar?"
+cp target/lantern-*-jar-with-dependencies.jar install/common/lantern.jar || die "Could not copy jar?"
 
 
 VERSION=$1
-/Applications/install4j\ 5/bin/install4jc -m macos -r $VERSION ./install/mg.install4j
+/Applications/install4j\ 5/bin/install4jc -m macos -r $VERSION ./install/lantern.install4j
 
 #scp install/Lantern.exe afisk@10.0.46.68:/home/afisk || die "Could not copy file"
