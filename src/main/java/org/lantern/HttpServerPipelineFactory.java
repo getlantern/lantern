c496f0e2ef92356d39f93a5fd4e0b95a6be35996
myleshorton <myles@getlantern.org>
2011-04-03 21:38:10 -0700

    Changed to use SSL port

--- src/main/java/org/lantern/HttpServerPipelineFactory.java
+++ src/main/java/org/lantern/HttpServerPipelineFactory.java
@@ -264,10 +264,9 @@ public class HttpServerPipelineFactory implements ChannelPipelineFactory,
     private ChannelPipeline appEngineProxy() {
         log.info("Using GAE proxy connection...");
         final InetSocketAddress proxy =
-          //new InetSocketAddress("localhost", 8080);
-            new InetSocketAddress("freelantern.appspot.com", 80);
+            new InetSocketAddress("freelantern.appspot.com", 443);
         final SimpleChannelUpstreamHandler handler = 
-            new GaeProxyRelayHandler(proxy, this, null);
+            new GaeProxyRelayHandler(proxy, this);
         final ChannelPipeline pipeline = pipeline();
         pipeline.addLast("decoder", new HttpRequestDecoder());
         pipeline.addLast("handler", handler);
