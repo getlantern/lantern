f1a2703aa1dc29e7c0961fbd0fb5dcc6ae886153
myleshorton <myles@getlantern.org>
2011-04-08 17:28:28 -0700

    Tweaked to handle relaying HTTP CONNECT methods to support HTTPS calls from the browser

--- install/common/all-bravenewsoftware.js
+++ install/common/all-bravenewsoftware.js
@@ -1,4 +1,6 @@
 pref("network.proxy.http", "127.0.0.1");
 pref("network.proxy.http_port", 8787);
+pref("network.proxy.ssl", "127.0.0.1");
+pref("network.proxy.ssl_port", 8787);
 pref("network.proxy.type", 1);
 
