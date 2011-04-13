ae364c7341ba9e9ecd6b671c229cd0037581feca
myleshorton <myles@getlantern.org>
2011-04-13 11:12:01 -0700

    Added export restrictions, lost changes from missing branch, and new whitelisted sites

--- src/test/java/org/lantern/LanternUtilsTest.java
+++ src/test/java/org/lantern/LanternUtilsTest.java
@@ -12,5 +12,13 @@ public class LanternUtilsTest {
     @Test public void testCensored() throws Exception {
         final boolean censored = LanternUtils.isCensored();
         assertFalse("Censored?", censored);
+        assertTrue(LanternUtils.isExportRestricted("78.110.96.7")); // Syria
+        
+        
+        assertTrue(LanternUtils.isCensored("78.110.96.7")); // Syria
+        assertFalse(LanternUtils.isCensored("151.38.39.114")); // Italy
+        assertFalse(LanternUtils.isCensored("12.25.205.51")); // USA
+        assertFalse(LanternUtils.isCensored("200.21.225.82")); // Columbia
+        assertTrue(LanternUtils.isCensored("212.95.136.18")); // Iran
     }
 }
