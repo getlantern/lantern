c227ec3fed56ae389cb4c81a3e96fa288e3281fa
myleshorton <myles@getlantern.org>
2011-04-23 17:51:51 -0700

    All sorts of changes to properly dispatch between all types of proxies

--- src/main/java/org/lantern/ProxyProvider.java
+++ src/main/java/org/lantern/ProxyProvider.java
@@ -1,10 +1,18 @@
 package org.lantern;
 
 import java.net.InetSocketAddress;
+import java.net.URI;
 
+/**
+ * Interface for the various types of proxies Lantern uses.
+ */
 public interface ProxyProvider {
 
     InetSocketAddress getLaeProxy();
+    
+    URI getLanternProxy();
+    
+    URI getPeerProxy();
 
     InetSocketAddress getProxy();
 
