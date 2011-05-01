1e2523328a415132ca28b15909f2180699f8a530
myleshorton <myles@getlantern.org>
2011-04-30 23:24:03 -0700

    Added app path

--- install/osx/configureOsx.bash
+++ install/osx/configureOsx.bash
@@ -10,14 +10,15 @@ echo "Running as `whoami`"
 echo "USER is $USER"
 echo "User name is $userName"
 echo "Executing perl replace on Info.plist"
-perl -pi -e "s/<dict>/<dict><key>LSUIElement<\/key><string>1<\/string>/g" /Applications/Lantern.app/Contents/Info.plist || die "Could not fix Info.plist"
+APP_PATH=/Applications/Lantern/Lantern.app
+perl -pi -e "s/<dict>/<dict><key>LSUIElement<\/key><string>1<\/string>/g" $APP_PATH/Contents/Info.plist || die "Could not fix Info.plist"
 
 echo "Running in `pwd`"
 
 #PLIST_DIR=/Library/LaunchAgents
 PLIST_DIR=~/Library/LaunchAgents
 PLIST_FILE=org.bns.lantern.plist
-PLIST_INSTALL_FULL=/Applications/Lantern.app/Contents/Resources/app/$PLIST_FILE
+PLIST_INSTALL_FULL=$APP_PATH/Contents/Resources/app/$PLIST_FILE
 LAUNCHD_PLIST=$PLIST_DIR/$PLIST_FILE
 
 echo "Copying launchd plist file"
