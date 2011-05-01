23e84c05f14581e6b08f0d89feb66b0eab1fd40f
myleshorton <myles@getlantern.org>
2011-04-30 23:14:09 -0700

    More debug info

--- install/osx/configureOsx.bash
+++ install/osx/configureOsx.bash
@@ -21,7 +21,7 @@ PLIST_INSTALL_FULL=/Applications/Lantern.app/Contents/Resources/app/$PLIST_FILE
 LAUNCHD_PLIST=$PLIST_DIR/$PLIST_FILE
 
 echo "Copying launchd plist file"
-cp $PLIST_INSTALL_FULL $PLIST_DIR || die "Could not copy plist file"
+cp $PLIST_INSTALL_FULL $PLIST_DIR || die "Could not copy plist file from $PLIST_INSTALL_FULL to $PLIST_DIR"
 
 echo "Changing permissions on launchd plist file"
 chmod 644 $LAUNCHD_PLIST || die "Could not change permissions"
