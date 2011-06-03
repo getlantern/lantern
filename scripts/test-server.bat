289fab7e12ea5e3691ca6522ea6640e0474e4b46
Igor Minar <iiminar@gmail.com>
2011-06-03 14:16:45 -0700

    making the windows script crlf delimited

--- scripts/test-server.bat
+++ scripts/test-server.bat
@@ -8,12 +8,12 @@ REM - Java (http://www.java.com)
 set BASE_DIR=%~dp0
 set PORT=9876
 
-echo "Starting JsTestDriver Server (http://code.google.com/p/js-test-driver/)"
-echo "Please open the following url and capture one or more browsers:"
-echo "http://localhost:%PORT%"
+echo Starting JsTestDriver Server (http://code.google.com/p/js-test-driver/)
+echo Please open the following url and capture one or more browsers:
+echo http://localhost:%PORT%/
 
 java -jar "%BASE_DIR%/../test/lib/jstestdriver/JsTestDriver.jar" ^
-     --PORT %PORT% ^
+     --port %PORT% ^
      --browserTimeout 20000 ^
      --config "%BASE_DIR%/../config/jsTestDriver.conf" ^
      --basePath "%BASE_DIR%/.."
