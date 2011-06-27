6597e3d959a22569ff934960ea04c377f51feffa
myleshorton <myles@getlantern.org>
2011-06-27 02:00:12 -0700

    Fixes for SWT

--- run.bash
+++ run.bash
@@ -5,7 +5,9 @@ function die() {
 }
 
 #pushd ..
-mvn -o package -Dmaven.test.skip=true || die "Could not package"
+mvn install:install-file -DgroupId=org.eclipse.swt.cocoa -DartifactId=macosx -Dversion=3.6.2 -Dpackaging=jar -Dfile=lib/swt-3.6.2-cocoa-macosx.jar
+mvn install:install-file -DgroupId=org.eclipse.swt.win32.win32 -DartifactId=macosx -Dversion=3.6.2 -Dpackaging=jar -Dfile=lib/swt-3.6.2-win32-win32-x86.jar
+mvn package -Dmaven.test.skip=true || die "Could not package"
 #popd
 
 fullPath=`dirname $0`
