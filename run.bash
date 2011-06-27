c476cb79591292f924f95383de6f7e5fa1fc8738
myleshorton <myles@getlantern.org>
2011-06-27 02:58:09 -0700

    Lots more SWT tweaks

--- run.bash
+++ run.bash
@@ -5,8 +5,9 @@ function die() {
 }
 
 #pushd ..
-mvn install:install-file -DgroupId=org.eclipse.swt.cocoa -DartifactId=macosx -Dversion=3.6.2 -Dpackaging=jar -Dfile=lib/swt-3.6.2-cocoa-macosx.jar
-mvn install:install-file -DgroupId=org.eclipse.swt.win32.win32 -DartifactId=macosx -Dversion=3.6.2 -Dpackaging=jar -Dfile=lib/swt-3.6.2-win32-win32-x86.jar
+mvn install:install-file -DgroupId=org.eclipse.swt.cocoa -DartifactId=x86 -Dversion=3.7 -Dpackaging=jar -Dfile=lib/swt-3.7-cocoa-macosx.jar
+mvn install:install-file -DgroupId=org.eclipse.swt.cocoa -DartifactId=x86_64 -Dversion=3.7 -Dpackaging=jar -Dfile=lib/swt-3.7-cocoa-macosx-x86_64.jar
+mvn install:install-file -DgroupId=org.eclipse.swt.win32.win32 -DartifactId=x86 -Dversion=3.7 -Dpackaging=jar -Dfile=lib/swt-3.7-win32-win32-x86.jar
 mvn package -Dmaven.test.skip=true || die "Could not package"
 #popd
 
@@ -14,7 +15,8 @@ fullPath=`dirname $0`
 jar=`find $fullPath/target/*with-dependencies.jar`
 cp=`echo $jar | sed 's,./,'$fullPath'/,'`
 javaArgs="-jar "$cp" $*"
+uname -a | grep Darwin && extras="-XstartOnFirstThread"
 
 echo "Running using Java on path at `which java` with args $javaArgs"
-java $javaArgs || die "Java process exited abnormally"
+java $extras $javaArgs || die "Java process exited abnormally"
 #java $javaArgs org.lantern.Launcher || die "Java process exited abnormally"
