942724ea8ceac2533b409ee6ca5afd92f8abe66f
myleshorton <myles@getlantern.org>
2011-07-05 18:37:29 -0700

    Lots of changes to flesh out install sequence

--- src/main/java/org/lantern/PeerHttpConnectRequestProcessor.java
+++ src/main/java/org/lantern/PeerHttpConnectRequestProcessor.java
@@ -1,7 +1,5 @@
 package org.lantern;
 
-import static org.jboss.netty.buffer.ChannelBuffers.copiedBuffer;
-
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
@@ -11,13 +9,11 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.commons.io.IOUtils;
-import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.handler.codec.http.HttpRequest;
-import org.jboss.netty.util.CharsetUtil;
 import org.lastbamboo.common.p2p.P2PClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,37 +24,6 @@ import org.slf4j.LoggerFactory;
 public class PeerHttpConnectRequestProcessor implements HttpRequestProcessor {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
-    
-    //space ' '
-    static final byte SP = 32;
-    
-    /**
-     * Colon ':'
-     */
-     static final byte COLON = 58;
-    
-    /**
-     * Carriage return
-     */
-    static final byte CR = 13;
-
-    /**
-     * Equals '='
-     */
-    static final byte EQUALS = 61;
-
-    /**
-     * Line feed character
-     */
-    static final byte LF = 10;
-
-    /**
-     * carriage return line feed
-     */
-    static final byte[] CRLF = new byte[] { CR, LF };
-    
-    private static final ChannelBuffer LAST_CHUNK =
-        copiedBuffer("0\r\n\r\n", CharsetUtil.US_ASCII);
     
     private URI peerInfo;
     private final ProxyStatusListener proxyStatusListener;
@@ -84,6 +49,7 @@ public class PeerHttpConnectRequestProcessor implements HttpRequestProcessor {
         this.p2pClient = p2pClient;
     }
 
+    @Override
     public boolean hasProxy() {
         if (this.peerInfo != null) {
             return true;
@@ -96,19 +62,17 @@ public class PeerHttpConnectRequestProcessor implements HttpRequestProcessor {
         return false;
     }
 
+    @Override
     public void processRequest(final Channel browserToProxyChannel,
         final ChannelHandlerContext ctx, final MessageEvent me) 
         throws IOException {
-        if (this.socket == null) {
-            this.socket = LanternUtils.openOutgoingPeerSocket(
-                browserToProxyChannel, this.peerInfo, ctx, 
-                this.proxyStatusListener, this.p2pClient, peerFailureCount);
-        }
-        final HttpRequest request = (HttpRequest) me.getMessage();
         browserToProxyChannel.setReadable(false);
+        final HttpRequest request = (HttpRequest) me.getMessage();
         
         if (this.socket == null) {
-            this.socket = LanternUtils.openOutgoingPeerSocket(
+            // We can pass raw traffic here because this is all tunneled SSL
+            // using HTTP CONNECT.
+            this.socket = LanternUtils.openRawOutgoingPeerSocket(
                 browserToProxyChannel, this.peerInfo, ctx, 
                 this.proxyStatusListener, this.p2pClient, peerFailureCount);
         }
@@ -134,13 +98,15 @@ public class PeerHttpConnectRequestProcessor implements HttpRequestProcessor {
         }
     }
 
+    @Override
     public void close() {
         IOUtils.closeQuietly(this.socket);
     }
 
-    public void processChunk(ChannelHandlerContext ctx, MessageEvent me)
-            throws IOException {
-        // TODO Auto-generated method stub
-        
+    @Override
+    public void processChunk(final ChannelHandlerContext ctx, 
+        final MessageEvent me) throws IOException {
+        throw new IllegalStateException(
+            "Processing chunks on HTTP CONNECT relay?");
     }
 }
