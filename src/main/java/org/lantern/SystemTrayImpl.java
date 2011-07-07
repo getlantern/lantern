ff6759f973c2d2e4f8e9df1df62d72bc67fb5479
myleshorton <myles@getlantern.org>
2011-07-07 15:34:54 -0700

    Lots of changes to initial config, system tray handling, etc

--- src/main/java/org/lantern/SystemTrayImpl.java
+++ src/main/java/org/lantern/SystemTrayImpl.java
@@ -1,17 +1,20 @@
 package org.lantern;
 
-import java.awt.Image;
-import java.awt.MenuItem;
-import java.awt.PopupMenu;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
-import java.net.MalformedURLException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
-import javax.swing.UIManager;
-
-import org.apache.commons.lang.SystemUtils;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Tray;
+import org.eclipse.swt.widgets.TrayItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,62 +24,85 @@ import org.slf4j.LoggerFactory;
 public class SystemTrayImpl implements SystemTray {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
+    private final Display display;
+    private final Shell shell;
 
     /**
      * Creates a new system tray handler class.
+     * 
+     * @param display The SWT display. 
      */
-    public SystemTrayImpl() {
-        // createTray();
+    public SystemTrayImpl(final Display display) {
+        this.display = display;
+        this.shell = new Shell(display);
     }
 
+    @Override
     public void createTray() {
-        // This is only enabled on Windows for now because it creates a screen
-        // menu bar on OSX.
-        if (SystemUtils.isJavaVersionAtLeast(1.6f)
-                && NativeUtils.supportsTray()) {
-            if (SystemUtils.IS_OS_WINDOWS) {
-                try {
-                    UIManager.setLookAndFeel(UIManager
-                            .getSystemLookAndFeelClassName());
-                } catch (final Exception e2) {
-                    log.error("Could not set look and feel", e2);
+        final Image image = newImage();
+        
+        final Tray tray = display.getSystemTray ();
+        if (tray == null) {
+            System.out.println ("The system tray is not available");
+        } else {
+            final TrayItem item = new TrayItem (tray, SWT.NONE);
+            item.setToolTipText("Lantern");
+            item.addListener (SWT.Show, new Listener () {
+                @Override
+                public void handleEvent (final Event event) {
+                    System.out.println("show");
                 }
-            }
-            final File iconFile;
-            final File iconCandidate1 = new File(
-                    "src/main/resources/mg_16x16.png");
-            if (iconCandidate1.isFile()) {
-                iconFile = iconCandidate1;
-            } else {
-                iconFile = new File("mg_16x16.png");
-            }
-            if (!iconFile.isFile()) {
-                log.error("Still no icon file at: " + iconFile);
-            }
-            final Image image;
-            try {
-                image = Toolkit.getDefaultToolkit().getImage(
-                        iconFile.toURI().toURL());
-            } catch (final MalformedURLException e) {
-                log.error("Could not load icon", e);
-                return;
-            }
-            final PopupMenu popup = new PopupMenu();
+            });
+            item.addListener (SWT.Hide, new Listener () {
+                @Override
+                public void handleEvent (final Event event) {
+                    System.out.println("hide");
+                }
+            });
 
-            final MenuItem quitItem = new MenuItem("Quit Lantern");
-            quitItem.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
+            final Menu menu = new Menu (shell, SWT.POP_UP);
+            final MenuItem quitItem = new MenuItem(menu, SWT.PUSH);
+            quitItem.setText("Quit Lantern");
+            quitItem.addListener (SWT.Selection, new Listener () {
+                @Override
+                public void handleEvent (final Event event) {
                     System.out.println("Got exit call");
+                    display.dispose();
                     System.exit(0);
                 }
-
             });
-            popup.add(quitItem);
-            System.out.println("Adding system tray...");
-            NativeUtils.addTray(image, "Lantern", popup);
-        } else {
-            log.debug("System tray not supported..");
+            menu.setDefaultItem(quitItem);
+
+            item.addListener (SWT.MenuDetect, new Listener () {
+                @Override
+                public void handleEvent (final Event event) {
+                    System.out.println("Setting menu visible");
+                    menu.setVisible (true);
+                }
+            });
+            item.setImage (image);
         }
     }
 
+    private Image newImage() {
+        final File iconFile;
+        final File iconCandidate1 = new File("install/common/mg_16x16.png");
+        if (iconCandidate1.isFile()) {
+            iconFile = iconCandidate1;
+        } else {
+            iconFile = new File("mg_16x16.png");
+        }
+        if (!iconFile.isFile()) {
+            log.error("Still no icon file at: " + iconFile);
+        }
+        InputStream is = null;
+        try {
+            is = new FileInputStream(iconFile);
+            return new Image (display, is);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } 
+        return new Image (display, 16, 16);
+    }
 }
---
+++ src/main/java/org/lantern/httpseverywhere/HttpsRule.java
@@ -0,0 +1,11 @@
+package org.lantern.httpseverywhere;
+
+import org.jboss.netty.handler.codec.http.HttpRequest;
+
+public class HttpsRule {
+
+    public String apply(final HttpRequest request) {
+        return "";
+    }
+
+}
