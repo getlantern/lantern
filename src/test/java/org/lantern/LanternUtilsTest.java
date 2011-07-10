a78e8accb0601895ab61f36d35a2871cabec1aac
myleshorton <myles@getlantern.org>
2011-07-10 12:12:29 -0700

    Refactored censored lookup and tweaked win install config

--- src/test/java/org/lantern/LanternUtilsTest.java
+++ src/test/java/org/lantern/LanternUtilsTest.java
@@ -11,15 +11,15 @@ import org.junit.Test;
 public class LanternUtilsTest {
 
     @Test public void testCensored() throws Exception {
-        final boolean censored = LanternUtils.isCensored();
+        final boolean censored = CensoredUtils.isCensored();
         assertFalse("Censored?", censored);
-        assertTrue(LanternUtils.isExportRestricted("78.110.96.7")); // Syria
+        assertTrue(CensoredUtils.isExportRestricted("78.110.96.7")); // Syria
         
         
-        assertTrue(LanternUtils.isCensored("78.110.96.7")); // Syria
-        assertFalse(LanternUtils.isCensored("151.38.39.114")); // Italy
-        assertFalse(LanternUtils.isCensored("12.25.205.51")); // USA
-        assertFalse(LanternUtils.isCensored("200.21.225.82")); // Columbia
-        assertTrue(LanternUtils.isCensored("212.95.136.18")); // Iran
+        assertTrue(CensoredUtils.isCensored("78.110.96.7")); // Syria
+        assertFalse(CensoredUtils.isCensored("151.38.39.114")); // Italy
+        assertFalse(CensoredUtils.isCensored("12.25.205.51")); // USA
+        assertFalse(CensoredUtils.isCensored("200.21.225.82")); // Columbia
+        assertTrue(CensoredUtils.isCensored("212.95.136.18")); // Iran
     }
 }
