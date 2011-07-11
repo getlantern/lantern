41449a6cde9235e5c18e657d9bf969132b59bcb0
myleshorton <myles@getlantern.org>
2011-07-10 19:31:33 -0700

    Fix for not knowing the network service names in advance on OSX

--- src/main/java/org/lantern/Configurator.java
+++ src/main/java/org/lantern/Configurator.java
@@ -2,6 +2,11 @@ package org.lantern;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Enumeration;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.SystemUtils;
@@ -19,6 +24,9 @@ public class Configurator {
     private static final Logger LOG = 
         LoggerFactory.getLogger(Configurator.class);
     private volatile static boolean configured = false;
+    
+    private static final MacProxyManager mpm = 
+        new MacProxyManager("testId", 4291);
     
     public static void configure() {
         if (configured) {
@@ -54,8 +62,59 @@ public class Configurator {
             LOG.info("Not auto-configuring proxy in an uncensored country");
         }
     }
+    
+
+    public static void main(final String... args) throws SocketException {
+        final Collection<String> services = mpm.getNetworkServices();
+        System.out.println(services);
+        
+        Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+        
+        for (NetworkInterface netIf : Collections.list(nets)) {
+            System.out.printf("Display name: %s\n", netIf.getDisplayName());
+            System.out.printf("Name: %s\n", netIf.getName());
+            displaySubInterfaces(netIf);
+            System.out.printf("\n");
+        }
+    }
+
+    static void displaySubInterfaces(final NetworkInterface netIf) throws SocketException {
+        Enumeration<NetworkInterface> subIfs = netIf.getSubInterfaces();
+        
+        for (NetworkInterface subIf : Collections.list(subIfs)) {
+            System.out.printf("\tSub Interface Display name: %s\n", subIf.getDisplayName());
+            System.out.printf("\tSub Interface Name: %s\n", subIf.getName());
+        }
+     }
 
     private static void configureOsxProxy() {
+        final Collection<String> services = mpm.getNetworkServices();
+        for (final String service : services) {
+            LOG.info("Setting web proxy for {}", service);
+            final String web = 
+                "-setwebproxy '"+service.trim()+"' 127.0.0.1 " + 
+                LanternConstants.LANTERN_LOCALHOST_HTTP_PORT;
+            LOG.info("Running full command:\n{}", web);
+            //CommonUtils.nativeCall(web);
+            final String val1 = mpm.runNetworkSetup("-setwebproxy", service.trim(), 
+                "127.0.0.1", String.valueOf(LanternConstants.LANTERN_LOCALHOST_HTTP_PORT));
+            LOG.info("Got return val:\n"+val1);
+            
+            // Also set it for HTTPS!!
+            LOG.info("Setting secure web proxy for {}", service);
+            final String secure = 
+                "-setsecurewebproxy '"+service.trim()+"' 127.0.0.1 " + 
+                LanternConstants.LANTERN_LOCALHOST_HTTP_PORT;
+            LOG.info("Running full command:\n{}", secure);
+            
+            final String val2 = mpm.runNetworkSetup("-setsecurewebproxy", service.trim(), 
+                    "127.0.0.1", String.valueOf(LanternConstants.LANTERN_LOCALHOST_HTTP_PORT));
+            //final String val2 = mpm.runNetworkSetup(secure);
+            LOG.info("Got return val:\n"+val2);
+            //CommonUtils.nativeCall(secure);
+            
+        }
+        /*
         CommonUtils.nativeCall("networksetup -setwebproxy Airport 127.0.0.1 " + 
             LanternConstants.LANTERN_LOCALHOST_HTTP_PORT);
         CommonUtils.nativeCall("networksetup -setwebproxy Ethernet 127.0.0.1 " + 
@@ -66,24 +125,48 @@ public class Configurator {
             LanternConstants.LANTERN_LOCALHOST_HTTP_PORT);
         CommonUtils.nativeCall("networksetup -setsecurewebproxy Ethernet 127.0.0.1 " + 
             LanternConstants.LANTERN_LOCALHOST_HTTP_PORT);
+        */
         
         final Thread hook = new Thread(new Runnable() {
             @Override
             public void run() {
+                for (final String service : services) {
+                    
+                    LOG.info("Unsetting web proxy "+service);
+                    final String web = 
+                        "-setwebproxystate '"+service.trim()+"' off";
+                    LOG.info("Running full command:\n{}", web);
+                    final String val1 = mpm.runNetworkSetup("-setwebproxystate", service.trim(), "off");
+                    LOG.info("Got return val:\n"+val1);
+                    //CommonUtils.nativeCall(web);
+                    
+                    LOG.info("Unsetting web proxy secure "+service);
+                    final String secure = 
+                        "-setsecurewebproxystate '"+service.trim()+"' off";
+                    LOG.info("Running full command:\n{}", secure);
+                    final String val2 = mpm.runNetworkSetup("-setsecurewebproxystate", service.trim(), "off");
+                    LOG.info("Got return val:\n"+val2);
+                    //CommonUtils.nativeCall(secure);
+                }
+                try {
+                    Thread.sleep(400);
+                } catch (final InterruptedException e) {
+                    LOG.error("Interrupted?", e);
+                }
                 // Note that non-daemon hooks can exit prematurely with CTL-C,
                 // but not if System.exit is used as it should be in deployed
                 // versions.
+                /*
                 LOG.info("Unsetting web airport");
                 CommonUtils.nativeCall("networksetup -setwebproxystate Airport off");
-                
                 LOG.info("Unsetting web ethernet");
                 CommonUtils.nativeCall("networksetup -setwebproxystate Ethernet off");
-                LOG.info("Unsetting secure airport");
                 
+                LOG.info("Unsetting secure airport");
                 CommonUtils.nativeCall("networksetup -setsecurewebproxystate Airport off");
                 LOG.info("Unsetting secure ethernet");
                 CommonUtils.nativeCall("networksetup -setsecurewebproxystate Ethernet off");
-                
+                */
             }
             
         }, "Unset-Web-Proxy-OSX");
