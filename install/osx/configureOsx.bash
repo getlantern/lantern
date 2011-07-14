018a1693cff8aa1c2ed9a8b73ef112056661e9e1
myleshorton <myles@getlantern.org>
2011-07-14 16:01:59 -0700

    Fix for launchd

--- install/osx/configureOsx.bash
+++ install/osx/configureOsx.bash
@@ -30,6 +30,8 @@ PLIST_FILE=org.bns.lantern.plist
 PLIST_INSTALL_FULL=$APP_PATH/Contents/Resources/app/$PLIST_FILE
 LAUNCHD_PLIST=$PLIST_DIR/$PLIST_FILE
 
+test -f $PLIST_INSTALL_FULL || die "plist file does not exist at $PLIST_INSTALL_FULL?"
+
 log "Unloading launchd plist file just in case"
 # Attempt to unload in case an old one is there
 launchctl unload -F $LAUNCHD_PLIST 
@@ -42,14 +44,13 @@ log "Executing perl replace on Info.plist"
 # The following test is due to bizarre installer behavior where it installs to 
 # /Applications/Lantern.app sometimes and /Applications/Lantern/Lantern.app in others.
 perl -pi -e "s/<dict>/<dict><key>LSUIElement<\/key><string>1<\/string>/g" $APP_PATH/Contents/Info.plist || die "Could not fix Info.plist"
-perl -pi -e "s:/Applications/Lantern/Lantern.app:$APP_PATH:g" $APP_PATH/Contents/Info.plist || die "Could not fix Info.plist"
+perl -pi -e "s:/Applications/Lantern/Lantern.app:$APP_PATH:g" $PLIST_INSTALL_FULL || die "Could not fix Info.plist"
 
 # We also need to change the contents of the Info.plist file to reflect the correct path.
 log "Running in `pwd`"
 
 
 log "Copying launchd plist file"
-test -f $PLIST_INSTALL_FULL || die "plist file does not exist at $PLIST_INSTALL_FULL?"
 cp $PLIST_INSTALL_FULL $PLIST_DIR || die "Could not copy plist file from $PLIST_INSTALL_FULL to $PLIST_DIR"
 
 log "Changing permissions on launchd plist file"
