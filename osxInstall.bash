8465f4b989c52c2c7cb08bcdf2eee0f8ef4c8cea
myleshorton <myles@getlantern.org>
2011-07-23 16:11:10 -0700

    More tweaks for enabling slightly more subtle updates

--- osxInstall.bash
+++ osxInstall.bash
@@ -10,14 +10,20 @@ then
     die "$0: Received $# args... version required"
 fi
 
+VERSION_FILE=src/main/java/org/lantern/LanternConstants.java
+VERSION=$1
+perl -pi -e "s/lantern_version_tok/$VERSION/g" $VERSION_FILE
+
 #cd client
 mvn clean || die "Could not clean?"
 mvn install -Dmaven.test.skip=true || die "Could not build?"
+
+echo "Reverting version file"
+git checkout $VERSION_FILE || die "Could not revert version file?"
+
 #cd -
 cp target/lantern-*-jar-with-dependencies.jar install/common/lantern.jar || die "Could not copy jar?"
 
-
-VERSION=$1
 /Applications/install4j\ 5/bin/install4jc -m macos -r $VERSION ./install/lantern.install4j
 
 mv install/Lantern.dmg lantern-$VERSION.dmg
