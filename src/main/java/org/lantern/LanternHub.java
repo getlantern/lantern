9c0d0324974654470c26326e91db784aca1cf5df
myleshorton <myles@getlantern.org>
2011-07-23 00:56:17 -0700

    Added support for XMPP updates

--- src/main/java/org/lantern/LanternHub.java
+++ src/main/java/org/lantern/LanternHub.java
@@ -4,25 +4,25 @@ import org.eclipse.swt.widgets.Display;
 
 public class LanternHub {
 
-    private static TrustedContactsManager trustedContactsManager;
-    private static Display display;
-    private static SystemTray systemTray;
+    private volatile static TrustedContactsManager trustedContactsManager;
+    private volatile static Display display;
+    private volatile static SystemTray systemTray;
     
-    public static TrustedContactsManager getTrustedContactsManager() {
+    public synchronized static TrustedContactsManager getTrustedContactsManager() {
         if (trustedContactsManager == null) {
             trustedContactsManager = new DefaultTrustedContactsManager();
         } 
         return trustedContactsManager;
     }
 
-    public static Display display() {
+    public synchronized static Display display() {
         if (display == null) {
             display = new Display();
         }
         return display;
     }
 
-    public static SystemTray systemTray() {
+    public synchronized static SystemTray systemTray() {
         if (systemTray == null) {
             systemTray = new SystemTrayImpl(display());
             systemTray.createTray();
