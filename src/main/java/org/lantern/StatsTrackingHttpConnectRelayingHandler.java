f2f0b569ea4dab3211b237d0de726abf654ebffc
myleshorton <myles@getlantern.org>
2011-07-24 18:16:42 -0700

    Stats tracking fixes

---
+++ src/main/java/org/lantern/StatsTrackingHttpConnectRelayingHandler.java
@@ -0,0 +1,103 @@
+package org.lantern;
+
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.ChannelFutureListener;
+import org.jboss.netty.channel.ChannelHandler.Sharable;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelStateEvent;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
+import org.littleshoot.proxy.HttpConnectRelayingHandler;
+import org.littleshoot.proxy.ProxyUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Class that simply relays traffic the channel this is connected to to 
+ * another channel passed in to the constructor.
+ */
+@Sharable
+public class StatsTrackingHttpConnectRelayingHandler 
+    extends SimpleChannelUpstreamHandler {
+    
+    private static final Logger LOG = 
+        LoggerFactory.getLogger(StatsTrackingHttpConnectRelayingHandler.class);
+    
+    /**
+     * The channel to relay to. This could be a connection from the browser
+     * to the proxy or it could be a connection from the proxy to an external
+     * site.
+     */
+    private final Channel relayChannel;
+
+    private final BytesTracker bytesTracker;
+
+    /**
+     * Creates a new {@link HttpConnectRelayingHandler} with the specified 
+     * connection to relay to..
+     * 
+     * @param relayChannel The channel to relay messages to.
+     * @param bytesTracker Class for tracking bytes transferred.
+     */
+    public StatsTrackingHttpConnectRelayingHandler(final Channel relayChannel, 
+        final BytesTracker bytesTracker) {
+        this.relayChannel = relayChannel;
+        this.bytesTracker = bytesTracker;
+    }
+
+    @Override
+    public void messageReceived(final ChannelHandlerContext ctx, 
+        final MessageEvent e) throws Exception {
+        final ChannelBuffer msg = (ChannelBuffer) e.getMessage();
+        if (relayChannel.isConnected()) {
+            final ChannelFutureListener logListener = 
+                new ChannelFutureListener() {
+                @Override
+                public void operationComplete(final ChannelFuture future) 
+                    throws Exception {
+                    LOG.debug("Finished writing data on CONNECT channel");
+                }
+            };
+            final int bytes = msg.readableBytes();
+            
+            LOG.info("Recording proxied bytes through HTTP CONNECT: {}", bytes);
+            
+            if (this.bytesTracker != null) {
+                this.bytesTracker.addBytes(bytes);
+            }
+            relayChannel.write(msg).addListener(logListener);
+        }
+        else {
+            LOG.info("Channel not open. Connected? {}", 
+                relayChannel.isConnected());
+            // This will undoubtedly happen anyway, but just in case.
+            ProxyUtils.closeOnFlush(e.getChannel());
+        }
+    }
+    
+    @Override
+    public void channelOpen(final ChannelHandlerContext ctx, 
+        final ChannelStateEvent cse) throws Exception {
+        final Channel ch = cse.getChannel();
+        LOG.info("New CONNECT channel opened from proxy to web: {}", ch);
+    }
+
+    @Override
+    public void channelClosed(final ChannelHandlerContext ctx, 
+        final ChannelStateEvent e) throws Exception {
+        LOG.info("Got closed event on proxy -> web connection: {}", 
+            e.getChannel());
+        ProxyUtils.closeOnFlush(this.relayChannel);
+    }
+
+    @Override
+    public void exceptionCaught(final ChannelHandlerContext ctx, 
+        final ExceptionEvent e) throws Exception {
+        LOG.warn("Caught exception on proxy -> web connection: "+
+            e.getChannel(), e.getCause());
+        ProxyUtils.closeOnFlush(e.getChannel());
+    }
+}
