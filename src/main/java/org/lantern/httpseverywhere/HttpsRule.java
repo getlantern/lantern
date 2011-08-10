7b7289bcb487edbbeedc557153fdca0a0e3b6001
myleshorton <myles@getlantern.org>
2011-08-10 12:48:35 -0700

    Added better support for https everywhere rules

--- src/main/java/org/lantern/httpseverywhere/HttpsRule.java
+++ src/main/java/org/lantern/httpseverywhere/HttpsRule.java
@@ -4,8 +4,24 @@ import org.jboss.netty.handler.codec.http.HttpRequest;
 
 public class HttpsRule {
 
+    private final String from;
+    private final String to;
+
+    public HttpsRule(final String from, final String to) {
+        this.from = from;
+        this.to = to;
+    }
+    
     public String apply(final HttpRequest request) {
-        return "";
+        return apply(request.getUri());
     }
 
+    public String apply(final String uri) {
+        return uri.replaceAll(this.from, this.to);
+    }
+
+    @Override
+    public String toString() {
+        return "HttpsRule [from=" + from + ", to=" + to + "]";
+    }
 }
