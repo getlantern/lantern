8710f7df05be1807a100ca1a4d215c1d762cde02
myleshorton <myles@getlantern.org>
2011-08-12 12:20:17 -0700

    Cleanup of proxy settings, particularly during install

--- src/main/java/org/lantern/Configurator.java
+++ src/main/java/org/lantern/Configurator.java
@@ -2,11 +2,7 @@ package org.lantern;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.NetworkInterface;
-import java.net.SocketException;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Enumeration;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.SystemUtils;
@@ -27,6 +23,15 @@ public class Configurator {
     private static final MacProxyManager mpm = 
         new MacProxyManager("testId", 4291);
     
+    private static final String WINDOWS_REGISTRY_PROXY_KEY = 
+        "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings";
+    
+    private static final String ps = "ProxyServer";
+    private static final String pe = "ProxyEnable";
+    
+    private static final String LANTERN_PROXY_ADDRESS = "127.0.0.1:"+
+        LanternConstants.LANTERN_LOCALHOST_HTTP_PORT;
+    
     public static void configure() {
         if (configured) {
             LOG.error("Configure called twice?");
@@ -34,7 +39,6 @@ public class Configurator {
         }
         configured = true;
         reconfigure();
-        
     }
     
 
@@ -68,132 +72,44 @@ public class Configurator {
         }
     }
 
-
-    public static void main(final String... args) throws SocketException {
-        final Collection<String> services = mpm.getNetworkServices();
-        System.out.println(services);
-        
-        Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
-        
-        for (NetworkInterface netIf : Collections.list(nets)) {
-            System.out.printf("Display name: %s\n", netIf.getDisplayName());
-            System.out.printf("Name: %s\n", netIf.getName());
-            displaySubInterfaces(netIf);
-            System.out.printf("\n");
-        }
-    }
-
-    static void displaySubInterfaces(final NetworkInterface netIf) {
-        Enumeration<NetworkInterface> subIfs = netIf.getSubInterfaces();
-        
-        for (NetworkInterface subIf : Collections.list(subIfs)) {
-            System.out.printf("\tSub Interface Display name: %s\n", subIf.getDisplayName());
-            System.out.printf("\tSub Interface Name: %s\n", subIf.getName());
-        }
-     }
-
     private static void configureOsxProxy() {
         final Collection<String> services = mpm.getNetworkServices();
         for (final String service : services) {
             LOG.info("Setting web proxy for {}", service);
-            final String web = 
-                "-setwebproxy '"+service.trim()+"' 127.0.0.1 " + 
-                LanternConstants.LANTERN_LOCALHOST_HTTP_PORT;
-            LOG.info("Running full command:\n{}", web);
-            //CommonUtils.nativeCall(web);
             final String val1 = mpm.runNetworkSetup("-setwebproxy", service.trim(), 
                 "127.0.0.1", String.valueOf(LanternConstants.LANTERN_LOCALHOST_HTTP_PORT));
             LOG.info("Got return val:\n"+val1);
             
             // Also set it for HTTPS!!
             LOG.info("Setting secure web proxy for {}", service);
-            final String secure = 
-                "-setsecurewebproxy '"+service.trim()+"' 127.0.0.1 " + 
-                LanternConstants.LANTERN_LOCALHOST_HTTP_PORT;
-            LOG.info("Running full command:\n{}", secure);
-            
             final String val2 = mpm.runNetworkSetup("-setsecurewebproxy", service.trim(), 
-                    "127.0.0.1", String.valueOf(LanternConstants.LANTERN_LOCALHOST_HTTP_PORT));
-            //final String val2 = mpm.runNetworkSetup(secure);
+                "127.0.0.1", String.valueOf(LanternConstants.LANTERN_LOCALHOST_HTTP_PORT));
             LOG.info("Got return val:\n"+val2);
-            //CommonUtils.nativeCall(secure);
-            
         }
-        /*
-        CommonUtils.nativeCall("networksetup -setwebproxy Airport 127.0.0.1 " + 
-            LanternConstants.LANTERN_LOCALHOST_HTTP_PORT);
-        CommonUtils.nativeCall("networksetup -setwebproxy Ethernet 127.0.0.1 " + 
-            LanternConstants.LANTERN_LOCALHOST_HTTP_PORT);
         
-        // Also set it for HTTPS!!
-        CommonUtils.nativeCall("networksetup -setsecurewebproxy Airport 127.0.0.1 " + 
-            LanternConstants.LANTERN_LOCALHOST_HTTP_PORT);
-        CommonUtils.nativeCall("networksetup -setsecurewebproxy Ethernet 127.0.0.1 " + 
-            LanternConstants.LANTERN_LOCALHOST_HTTP_PORT);
-        */
-        
+        // Note that non-daemon hooks can exit prematurely with CTL-C,
+        // but not if System.exit is used as it should be in deployed
+        // versions.
         final Thread hook = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (final String service : services) {
-                    
-                    LOG.info("Unsetting web proxy "+service);
-                    final String web = 
-                        "-setwebproxystate '"+service.trim()+"' off";
-                    LOG.info("Running full command:\n{}", web);
-                    final String val1 = mpm.runNetworkSetup("-setwebproxystate", service.trim(), "off");
-                    LOG.info("Got return val:\n"+val1);
-                    //CommonUtils.nativeCall(web);
-                    
-                    LOG.info("Unsetting web proxy secure "+service);
-                    final String secure = 
-                        "-setsecurewebproxystate '"+service.trim()+"' off";
-                    LOG.info("Running full command:\n{}", secure);
-                    final String val2 = mpm.runNetworkSetup("-setsecurewebproxystate", service.trim(), "off");
-                    LOG.info("Got return val:\n"+val2);
-                    //CommonUtils.nativeCall(secure);
-                }
-                try {
-                    Thread.sleep(400);
-                } catch (final InterruptedException e) {
-                    LOG.error("Interrupted?", e);
-                }
-                // Note that non-daemon hooks can exit prematurely with CTL-C,
-                // but not if System.exit is used as it should be in deployed
-                // versions.
-                /*
-                LOG.info("Unsetting web airport");
-                CommonUtils.nativeCall("networksetup -setwebproxystate Airport off");
-                LOG.info("Unsetting web ethernet");
-                CommonUtils.nativeCall("networksetup -setwebproxystate Ethernet off");
-                
-                LOG.info("Unsetting secure airport");
-                CommonUtils.nativeCall("networksetup -setsecurewebproxystate Airport off");
-                LOG.info("Unsetting secure ethernet");
-                CommonUtils.nativeCall("networksetup -setsecurewebproxystate Ethernet off");
-                */
+                unproxyOsx();
             }
-            
         }, "Unset-Web-Proxy-OSX");
         Runtime.getRuntime().addShutdownHook(hook);
     }
+
 
     private static void configureWindowsProxy() {
         if (!SystemUtils.IS_OS_WINDOWS) {
             LOG.info("Not running on Windows");
             return;
         }
-        final String key = 
-            "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\" +
-            "Internet Settings";
-        
-        final String ps = "ProxyServer";
-        final String pe = "ProxyEnable";
         
         // We first want to read the start values so we can return the
         // registry to the original state when we shut down.
-        final String proxyServerOriginal = WindowsRegistry.read(key, ps);
-        final String proxyEnableOriginal = WindowsRegistry.read(key, pe);
+        final String proxyServerOriginal = WindowsRegistry.read(WINDOWS_REGISTRY_PROXY_KEY, ps);
+        final String proxyEnableOriginal = WindowsRegistry.read(WINDOWS_REGISTRY_PROXY_KEY, pe);
         
         final String proxyServerUs = "127.0.0.1:"+
             LanternConstants.LANTERN_LOCALHOST_HTTP_PORT;
@@ -201,9 +117,9 @@ public class Configurator {
 
         LOG.info("Setting registry to use Lantern as a proxy...");
         final int enableResult = 
-            WindowsRegistry.writeREG_SZ(key, ps, proxyServerUs);
+            WindowsRegistry.writeREG_SZ(WINDOWS_REGISTRY_PROXY_KEY, ps, proxyServerUs);
         final int serverResult = 
-            WindowsRegistry.writeREG_DWORD(key, pe, proxyEnableUs);
+            WindowsRegistry.writeREG_DWORD(WINDOWS_REGISTRY_PROXY_KEY, pe, proxyEnableUs);
         
         if (enableResult != 0) {
             LOG.error("Error enabling the proxy server? Result: "+enableResult);
@@ -217,33 +133,7 @@ public class Configurator {
         final Runnable runner = new Runnable() {
             @Override
             public void run() {
-                LOG.info("Resetting Windows registry settings to " +
-                    "original values.");
-                
-                // On shutdown, we need to check if the user has modified the
-                // registry since we originally set it. If they have, we want
-                // to keep their setting. If not, we want to revert back to 
-                // before MG started.
-                final String proxyServer = WindowsRegistry.read(key, ps);
-                final String proxyEnable = WindowsRegistry.read(key, pe);
-                
-                //LOG.info("Proxy enable original: '{}'", proxyEnableUs);
-                //LOG.info("Proxy enable now: '{}'", proxyEnable);
-                
-                if (proxyEnable.equals(proxyEnableUs)) {
-                    LOG.info("Setting proxy enable back to: {}", 
-                        proxyEnableOriginal);
-                    WindowsRegistry.writeREG_DWORD(key, pe,proxyEnableOriginal);
-                    LOG.info("Successfully reset proxy enable");
-                }
-                
-                if (proxyServer.equals(proxyServerUs)) {
-                    LOG.info("Setting proxy server back to: {}", 
-                        proxyServerOriginal);
-                    WindowsRegistry.writeREG_SZ(key, ps, proxyServerOriginal);
-                    LOG.info("Successfully reset proxy server");
-                }
-                LOG.info("Done resetting the Windows registry");
+                unproxyWindows(proxyServerOriginal, proxyEnableOriginal);
             }
         };
         
@@ -276,6 +166,72 @@ public class Configurator {
             } catch (final IOException e) {
                 LOG.error("Could not copy config file?", e);
             }
+        }
+    }
+
+    public static void unproxy() {
+        if (SystemUtils.IS_OS_WINDOWS) {
+            // We first want to read the start values so we can return the
+            // registry to the original state when we shut down.
+            final String proxyServerOriginal = 
+                WindowsRegistry.read(WINDOWS_REGISTRY_PROXY_KEY, ps);
+            if (proxyServerOriginal.equals(LANTERN_PROXY_ADDRESS)) {
+                unproxyWindows(proxyServerOriginal, "0");
+            }
+        } else if (SystemUtils.IS_OS_MAC_OSX) {
+            unproxyOsx();
+        } else {
+            LOG.warn("We don't yet support proxy configuration on other OSes");
+        }
+    }
+    
+    protected static void unproxyWindows(final String proxyServerOriginal, 
+        final String proxyEnableOriginal) {
+        LOG.info("Resetting Windows registry settings to original values.");
+        final String proxyEnableUs = "1";
+        
+        // On shutdown, we need to check if the user has modified the
+        // registry since we originally set it. If they have, we want
+        // to keep their setting. If not, we want to revert back to 
+        // before Lantern started.
+        final String proxyServer = 
+            WindowsRegistry.read(WINDOWS_REGISTRY_PROXY_KEY, ps);
+        final String proxyEnable = 
+            WindowsRegistry.read(WINDOWS_REGISTRY_PROXY_KEY, pe);
+        
+        if (proxyServer.equals(LANTERN_PROXY_ADDRESS)) {
+            LOG.info("Setting proxy server back to: {}", 
+                proxyServerOriginal);
+            WindowsRegistry.writeREG_SZ(WINDOWS_REGISTRY_PROXY_KEY, ps, 
+                proxyServerOriginal);
+            LOG.info("Successfully reset proxy server");
+        }
+        
+        if (proxyEnable.equals(proxyEnableUs)) {
+            LOG.info("Setting proxy enable back to: {}", 
+                proxyEnableOriginal);
+            WindowsRegistry.writeREG_DWORD(WINDOWS_REGISTRY_PROXY_KEY, pe, 
+                proxyEnableOriginal);
+            LOG.info("Successfully reset proxy enable");
+        }
+        
+        LOG.info("Done resetting the Windows registry");
+    }
+
+
+    protected static void unproxyOsx() {
+        final Collection<String> services = mpm.getNetworkServices();
+        for (final String service : services) {
+            
+            LOG.info("Unsetting web proxy "+service);
+            final String val1 = mpm.runNetworkSetup(
+                "-setwebproxystate", service.trim(), "off");
+            LOG.info("Got return val:\n"+val1);
+            
+            LOG.info("Unsetting web proxy secure "+service);
+            final String val2 = mpm.runNetworkSetup(
+                "-setsecurewebproxystate", service.trim(), "off");
+            LOG.info("Got return val:\n"+val2);
         }
     }
 
