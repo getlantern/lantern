ad262b4202a0853d02c23979ebb2b008881d036c
myleshorton <myles@getlantern.org>
2011-08-11 21:26:08 -0700

    Updates to HTTPS everywhere code to support trailing wildcards and clenaups

--- src/test/java/org/lantern/HttpsEverywhereTest.java
+++ src/test/java/org/lantern/HttpsEverywhereTest.java
@@ -1,31 +1,21 @@
 package org.lantern;
 
-import static org.junit.Assert.*;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
-import javax.xml.xpath.XPathExpressionException;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.lantern.httpseverywhere.HttpsEverywhere;
-import org.littleshoot.util.xml.XPathUtils;
-import org.littleshoot.util.xml.XmlUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
-
 
 public class HttpsEverywhereTest {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
     
     @Test public void testHttpsEverywhereRegex() throws Exception {
-        //HttpsEverywhere.toHttps("http://www.balatarin.com/test");
         final String[] urls = new String[] {
+            "http://www.gmail.com/test",
+            "http://news.google.com/news",
+            "http://www.google.com.testing/",
             "http://www.balatarin.com/test",
             "http://www.facebook.com/testing?query=test",
             "http://www.flickr.com/newPicture.jpg",
@@ -34,6 +24,11 @@ public class HttpsEverywhereTest {
         };
         
         final String[] expecteds = new String[] {
+            "https://mail.google.com/test",
+            "https://www.google.com/news",
+             // This should be the same -- it should match the *target* for 
+             // http://www.google.com.* but there is no relevant rule.
+            "http://www.google.com.testing/", 
             "https://balatarin.com/test",
             "https://www.facebook.com/testing?query=test",
             "https://secure.flickr.com/newPicture.jpg",
@@ -43,7 +38,7 @@ public class HttpsEverywhereTest {
         for (int i = 0; i < urls.length; i++) {
             final String request = urls[i];
             final String expected = expecteds[i];
-            final String converted = HttpsEverywhere.toHttps(request);//HttpsEverywhere.toHttps("Balatarin.xml", urls[i]);
+            final String converted = HttpsEverywhere.toHttps(request);
             log.info("Got converted: "+converted);
             assertEquals(expected, converted);
         }
@@ -55,59 +50,9 @@ public class HttpsEverywhereTest {
         
         for (int i = 0; i < excluded.length; i++) {
             final String request = excluded[i];
-            final String converted = HttpsEverywhere.toHttps(request);//HttpsEverywhere.toHttps("Balatarin.xml", urls[i]);
+            final String converted = HttpsEverywhere.toHttps(request);
             log.info("Got converted: "+converted);
             assertEquals(request, converted);
         }
-        
-        
-        /*
-        final String request = "http://images.google.com/testing?testing=testing";
-        final String converted = HttpsEverywhere.toHttps("GoogleImages.xml", request);
-        System.out.println("Converted: "+converted);
-        assertTrue(converted.startsWith("https://encrypted.google.com"));
-        */
-        
-        /*
-        final File gi = new File("GoogleImages.xml");
-        final InputStream is = new FileInputStream(gi);
-        final Document doc = XmlUtils.toDoc(is);
-        final XPathUtils utils = XPathUtils.newXPath(doc);
-        final NodeList nameNodes = utils.getNodes("/ruleset/rule/@from");
-        System.out.println(nameNodes.getLength());
-        for (int i = 0; i < nameNodes.getLength(); i++) {
-            final Node node = nameNodes.item(i);
-            //final String name = node.getTextContent();
-            final String from = node.getNodeValue();
-            System.out.println(from);
-            final String request = "http://images.google.com/testing?testing=testing";
-            final String converted = HttpsEverywhere.toHttps("GoogleImages.xml", request);
-        }
-        */
-    }
-    
-    private static String toHttps(final String file, final String request) 
-        throws XPathExpressionException, IOException, SAXException {
-        final File gi = new File(file);
-        final InputStream is = new FileInputStream(gi);
-        final Document doc = XmlUtils.toDoc(is);
-        final XPathUtils utils = XPathUtils.newXPath(doc);
-        final String from = utils.getString("/ruleset/rule/@from");
-        //final String from = "^http://(images|www|encrypted)\\.google\\.com/(.*)";
-        final String to = utils.getString("/ruleset/rule/@to");
-        System.out.println(from);
-        System.out.println(to);
-        return request.replaceAll(from, to);
-        /*
-        System.out.println(nameNodes.getLength());
-        for (int i = 0; i < nameNodes.getLength(); i++) {
-            final Node node = nameNodes.item(i);
-            //final String name = node.getTextContent();
-            final String from = node.getNodeValue();
-            System.out.println(from);
-            final String request = "http://images.google.com/testing?testing=testing";
-            final String converted = HttpsEverywhere.toHttps("GoogleImages.xml", request);
-        }
-        */
     }
 }
