73cd09bb2a12f7cb2c69a50192446a178efb8a4c
myleshorton <myles@getlantern.org>
2011-09-10 16:47:03 -0700

    Updated localization script

--- bin/localize.bash
+++ bin/localize.bash
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
+function die() {
+    echo $*
+    exit 1
+}
+
+
 cd /home/lantern/lantern
 xgettext -ktrc -ktr -kmarktr -ktrn:1,2 -o po/keys.pot $(find . -name "*.java")
 
-locales=("en" "zh_CN") 
+locales=("en" "zh_CN")
 for l in ${locales[@]}
-do 
+do
   echo "Processing locale: $l"
   touch po/$l.po
-  msgmerge -U po/$l.po po/keys.pot
-  msgfmt --java2 -d src/main/resources -r app.i18n.Messages -l $l po/$l.po
+  msgmerge -U po/$l.po po/keys.pot || die "Could not merge $l"
+  echo "Done merging $l"
+  msgfmt --java2 -d src/main/resources -r app.i18n.Messages -l $l po/$l.po || die "Could not format $l"
+  echo "Done processing $l"
 done
 
