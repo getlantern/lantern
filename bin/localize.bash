fbc6426177e0ce70dffddc22b972286bc20c0d48
myleshorton <myles@getlantern.org>
2011-09-09 17:35:03 -0700

    More cleanups for translations

--- bin/localize.bash
+++ bin/localize.bash
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 cd /home/lantern/lantern
 xgettext -ktrc -ktr -kmarktr -ktrn:1,2 -o po/keys.pot $(find . -name "*.java")
-
+touch po/en.po
+msgmerge -U po/en.po po/keys.pot
+msgfmt --java2 -d src/main/resources -r org.lantern.LanternBrowser -l en po/en.po
