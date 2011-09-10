08351aceb387a29fa598c2c314c04edccfac7673
myleshorton <myles@getlantern.org>
2011-09-10 16:35:12 -0700

    localization tweaks

--- bin/localize.bash
+++ bin/localize.bash
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 cd /home/lantern/lantern
 xgettext -ktrc -ktr -kmarktr -ktrn:1,2 -o po/keys.pot $(find . -name "*.java")
-touch po/en.po
-msgmerge -U po/en.po po/keys.pot
-msgfmt --java2 -d src/main/resources -r app.i18n.Messages -l en po/en.po
+
+locales=("en" "zh_CN") 
+for l in ${locales[@]}
+do 
+  echo "Processing locale: $l"
+  touch po/$l.po
+  msgmerge -U po/$l.po po/keys.pot
+  msgfmt --java2 -d src/main/resources -r app.i18n.Messages -l $l po/$l.po
+done
+
