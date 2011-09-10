2222fef2feb0f5ff09a4ba5e1691229bb84efa60
myleshorton <myles@getlantern.org>
2011-09-09 20:32:53 -0700

    More fixes for i18n scripts

--- bin/remoteLocalize.bash
+++ bin/remoteLocalize.bash
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 ./remoteScript.bash localize.bash
-scp -i ~/.ec2/id_rsa-gsg-keypair root@issues.littleshoot.org:/home/lantern/lantern/src/main/resources/*.class ../src/main/resources
+scp -i ~/.ec2/id_rsa-gsg-keypair root@issues.littleshoot.org:/home/lantern/lantern/src/main/resources/app/i18n/*.class ../src/main/resources/app/i18n/
+scp -i ~/.ec2/id_rsa-gsg-keypair root@issues.littleshoot.org:/home/lantern/lantern/po/*.po ../po/
+scp -i ~/.ec2/id_rsa-gsg-keypair root@issues.littleshoot.org:/home/lantern/lantern/po/keys.pot ../po/
