b1315f1d42dddd7b38e383c2cbda0a13a02b4157
myleshorton <myles@getlantern.org>
2011-09-09 17:35:03 -0700

    More cleanups for translations

--- bin/remoteLocalize.bash
+++ bin/remoteLocalize.bash
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 ./remoteScript.bash localize.bash
+scp -i ~/.ec2/id_rsa-gsg-keypair root@issues.littleshoot.org:/home/lantern/lantern/src/main/resources/*.class ../src/main/resources
