39c9e85f71d1f31c6b279f0e348606aa3040d43c
myleshorton <myles@getlantern.org>
2011-09-13 17:15:56 -0700

    Initial pass at getting Lantern working on Linux

--- run.bash
+++ run.bash
@@ -8,6 +8,7 @@ function die() {
 test -d target || mvn install:install-file -DgroupId=org.eclipse.swt.cocoa -DartifactId=x86 -Dversion=3.7 -Dpackaging=jar -Dfile=lib/swt-3.7-cocoa-macosx.jar
 test -d target || mvn install:install-file -DgroupId=org.eclipse.swt.cocoa -DartifactId=x86_64 -Dversion=3.7 -Dpackaging=jar -Dfile=lib/swt-3.7-cocoa-macosx-x86_64.jar
 test -d target || mvn install:install-file -DgroupId=org.eclipse.swt.win32.win32 -DartifactId=x86 -Dversion=3.7 -Dpackaging=jar -Dfile=lib/swt-3.7-win32-win32-x86.jar
+test -d target || mvn install:install-file -DgroupId=org.eclipse.swt.gtk.linux -DartifactId=x86 -Dversion=3.7 -Dpackaging=jar -Dfile=lib/swt-3.7-gtk-linux-x86.jar
 mvn package -Dmaven.test.skip=true || die "Could not package"
 #popd
 
