971532ac546f6c5f12790745cd784b084f5da73f
myleshorton <myles@getlantern.org>
2011-09-29 01:16:04 -0700

    Switched to use OTR mode for messaging with the lantern controller and added locale tests

--- src/test/java/org/lantern/LanternUtilsTest.java
+++ src/test/java/org/lantern/LanternUtilsTest.java
@@ -9,15 +9,45 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.SystemUtils;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.provider.ProviderManager;
 import org.junit.Test;
+import org.lantern.xmpp.GenericIQProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Test for Lantern utilities.
  */
 public class LanternUtilsTest {
     
-    @Test public void testOsFamily() throws Exception {
+    private static Logger LOG = LoggerFactory.getLogger(LanternUtilsTest.class);
+    
+    @Test public void testOtrMode() throws Exception {
+        ProviderManager.getInstance().addIQProvider(
+                "query", "google:shared-status", new GenericIQProvider());
+        ProviderManager.getInstance().addIQProvider(
+                "query", "google:nosave", new GenericIQProvider());
+        ProviderManager.getInstance().addIQProvider(
+                "query", "http://jabber.org/protocol/disco#info", new GenericIQProvider());
+
+        String email = LanternUtils.getStringProperty("google.user");
+        String pwd = LanternUtils.getStringProperty("google.pwd");
+        final XMPPConnection conn = LanternUtils.persistentXmppConnection(
+            email, pwd, "jqiq", 2);
+        final String activateResponse = LanternUtils.activateOtr(conn).toXML();
+        LOG.info("Got response: {}", activateResponse);
+        
+        final String allOtr = LanternUtils.getOtr(conn).toXML();
+        LOG.info("All OTR: {}", allOtr);
+        
+        assertTrue(
+            allOtr.contains("lantern-controller@appspot.com\" value=\"enabled"));
+    }
+    
+    
+    @Test 
+    public void testI18n() throws Exception {
         final ResourceBundle rb = 
             Utf8ResourceBundle.getBundle("LanternResourceBundle", Locale.CHINESE);
         
@@ -29,7 +59,8 @@ public class LanternUtilsTest {
         assertTrue(StringUtils.isNotBlank(val));
     }
 
-    @Test public void testToHttpsCandidates() throws Exception {
+    @Test 
+    public void testToHttpsCandidates() throws Exception {
         Collection<String> candidates = 
             LanternUtils.toHttpsCandidates("http://www.google.com");
         assertTrue(candidates.contains("www.google.com"));
@@ -51,11 +82,11 @@ public class LanternUtilsTest {
         //assertTrue(candidates.contains("*"));
     }
     
-    @Test public void testCensored() throws Exception {
+    @Test 
+    public void testCensored() throws Exception {
         final boolean censored = CensoredUtils.isCensored();
         assertFalse("Censored?", censored);
         assertTrue(CensoredUtils.isExportRestricted("78.110.96.7")); // Syria
-        
         
         assertTrue(CensoredUtils.isCensored("78.110.96.7")); // Syria
         assertFalse(CensoredUtils.isCensored("151.38.39.114")); // Italy
