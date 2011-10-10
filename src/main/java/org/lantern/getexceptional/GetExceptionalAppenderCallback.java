5e57b47be63624ca0ee5b98dc27ee2e618d29708
myleshorton <myles@getlantern.org>
2011-10-09 22:19:55 -0700

    added initial getexceptional support

--- src/main/java/org/lantern/getexceptional/GetExceptionalAppenderCallback.java
+++ src/main/java/org/lantern/getexceptional/GetExceptionalAppenderCallback.java
@@ -1,8 +1,6 @@
 package org.lantern.getexceptional;
 
-import java.util.Collection;
-
-import org.apache.commons.httpclient.NameValuePair;
+import org.json.simple.JSONObject;
 
 /**
  * Interface for making callbacks prior to sending data to GetExceptional.
@@ -13,7 +11,7 @@ public interface GetExceptionalAppenderCallback {
      * Allows the creator of a GetExceptional log4j appender to add arbitrary
      * data or edit existing data prior to the exception being reported.
      * 
-     * @param dataList The data for submission.
+     * @param json The data for submission.
      */
-    void addData(Collection<NameValuePair> dataList);
+    void addData(JSONObject json);
 }
