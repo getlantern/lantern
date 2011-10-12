19f1806b15d0174210d7e6c1786fc4a26185db40
myleshorton <myles@getlantern.org>
2011-10-12 15:24:18 -0700

    Added support for swapping in the getexceptional api key on build

--- installerBuild.bash
+++ installerBuild.bash
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+CONSTANTS_FILE=src/main/java/org/lantern/LanternConstants.java
 function die() {
   echo $*
   echo "Reverting version file"
-  git checkout $VERSION_FILE || die "Could not revert version file?"
+  git checkout $CONSTANTS_FILE || die "Could not revert version file?"
   exit 1
 }
 
@@ -12,18 +13,20 @@ then
     die "$0: Received $# args... version required"
 fi
 
-VERSION_FILE=src/main/java/org/lantern/LanternConstants.java
 VERSION=$1
 MVN_ARGS=$2
 echo "*******MAVEN ARGS*******: $MVN_ARGS"
-perl -pi -e "s/lantern_version_tok/$VERSION/g" $VERSION_FILE
+perl -pi -e "s/lantern_version_tok/$VERSION/g" $CONSTANTS_FILE
+GE_API_KEY=`cat ~/.lantern/lantern_getexceptional.txt`
+
+perl -pi -e "s/GetExceptionalUtils.NO_OP_KEY/\"$GE_API_KEY\"/g" $CONSTANTS_FILE
 
 #cd client
 mvn clean || die "Could not clean?"
 mvn $MVN_ARGS install -Dmaven.test.skip=true || die "Could not build?"
 
 echo "Reverting version file"
-git checkout $VERSION_FILE || die "Could not revert version file?"
+git checkout $CONSTANTS_FILE || die "Could not revert version file?"
 
 cp target/lantern-*-jar-with-dependencies.jar install/common/lantern.jar || die "Could not copy jar?"
 
