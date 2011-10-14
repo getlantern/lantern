ed53b69c8fb98c6f227979493f827af3b7957e8a
myleshorton <myles@getlantern.org>
2011-10-13 22:27:52 -0700

    New script for getting lines committed and updated FireFox extension a bit

---
+++ bin/linesCommitted.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import commands
+import os
+import sys
+
+if (len(sys.argv) != 3):
+    print "Usage: ./linesCommitted.py 2011-10-01 2012-01-01"
+    sys.exit(1)
+
+start = sys.argv[1]
+end = sys.argv[2]
+
+hist = commands.getoutput("git log --shortstat --reverse --pretty=oneline --after=\""+start+"\" --before=\""+end+"\" --no-merges")
+hist = hist.split("\n")
+totalins = 0
+
+for line in hist:
+    if line.startswith(' '):
+        ins =  line.split(",")[1]
+        totalins = totalins + int(ins.split(" ")[1])
+
+
+print "Between " + start + " and " + end + " the Lantern team wrote " + str(totalins) + " lines of code!"
