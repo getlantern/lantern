f615a75a2e5a41dfe1732cf0ce2a931f08400cf9
myleshorton <myles@getlantern.org>
2011-10-13 22:27:52 -0700

    New script for getting lines committed and updated FireFox extension a bit

--- install/firefox/lantern@getlantern.org/chrome/content/lantern.js
+++ install/firefox/lantern@getlantern.org/chrome/content/lantern.js
@@ -3,13 +3,26 @@ var Lantern = {
 
     prefs: null,
 
+    lanternRunning: function() {
+        var fileIn = FileIO.open('/test.txt');
+	return fileIn.exists();
+    },
+
     startup: function() {
+        dump("Starting Lantern extension...");
+
         this.prefs = Components.classes["@mozilla.org/preferences-service;1"]
             .getService(Components.interfaces.nsIPrefService)
             .getBranch("network.proxy.");
 
-        // Set FireFox to use the system proxy settings.
-        this.prefs.setIntPref("type", 5);
+        if (!this.lanternRunning()) {
+            dump("Lantern not running...");            
+            this.prefs.setIntPref("type", 1);
+        } else {
+            dump("Lantern running!!");            
+            // Set FireFox to use the system proxy settings.
+            this.prefs.setIntPref("type", 5);
+        }
     },
 }
 
