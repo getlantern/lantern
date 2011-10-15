a1ff19a8feff83601ff5e30b34b763be2b15eb3f
myleshorton <myles@getlantern.org>
2011-10-14 17:18:59 -0700

    more tweaks to detect if lantern is running from FireFox extension

--- install/firefox/lantern@getlantern.org/chrome/content/lantern.js
+++ install/firefox/lantern@getlantern.org/chrome/content/lantern.js
@@ -4,22 +4,30 @@ var Lantern = {
     prefs: null,
 
     lanternRunning: function() {
-        var fileIn = FileIO.open('/test.txt');
-	return fileIn.exists();
+        var home = DirIO.get('Home');
+        dump("Home: "+DirIO.path(home)+"\n");
+        //var fileIn = FileIO.open('/test.txt');
+        var fullPath = DirIO.path(home)+'/.lantern/lanternRunning';
+        dump("Full path: "+fullPath+"\n");
+        var normalized = fullPath.substring(7);
+        dump("Normalized: "+normalized+"\n");
+        var fileIn = FileIO.open(normalized);
+        dump("Opened file..."+fileIn+"\n");
+        return fileIn.exists();
     },
 
     startup: function() {
-        dump("Starting Lantern extension...");
+        dump("Starting Lantern extension...\n");
 
         this.prefs = Components.classes["@mozilla.org/preferences-service;1"]
             .getService(Components.interfaces.nsIPrefService)
             .getBranch("network.proxy.");
 
         if (!this.lanternRunning()) {
-            dump("Lantern not running...");            
+            dump("Lantern not running...\n");            
             this.prefs.setIntPref("type", 1);
         } else {
-            dump("Lantern running!!");            
+            dump("Lantern running!!\n");            
             // Set FireFox to use the system proxy settings.
             this.prefs.setIntPref("type", 5);
         }
