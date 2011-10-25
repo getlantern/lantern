c5f789460693e46ca1169918a65a5efcb5ddee6f
Myles Horton <myles@getlantern.org>
2011-10-24 18:07:26 -0700

    Changed peers connections to only activate when they've successfully connected

--- src/main/java/org/lantern/TrustedContactsManager.java
+++ src/main/java/org/lantern/TrustedContactsManager.java
@@ -2,11 +2,15 @@ package org.lantern;
 
 import java.util.Collection;
 
+import org.jivesoftware.smack.packet.Packet;
+
 public interface TrustedContactsManager {
 
     void addTrustedContact(String email);
 
     boolean isTrusted(String email);
+    
+    boolean isTrusted(Packet msg);
 
     boolean isJidTrusted(String from);
 
