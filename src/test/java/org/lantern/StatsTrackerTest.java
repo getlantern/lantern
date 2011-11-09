7b71ecd3a569d4ca4ae3b11327c58d031c32f8be
Myles Horton <myles@getlantern.org>
2011-11-09 06:28:05 +0100

    Added lots of google data and new api calls

--- src/test/java/org/lantern/StatsTrackerTest.java
+++ src/test/java/org/lantern/StatsTrackerTest.java
@@ -14,7 +14,51 @@ import org.junit.Test;
 
 public class StatsTrackerTest {
 
-    @Test public void testOni() throws Exception {
+    //@Test 
+    public void testGoogleContentRemovalRequests() throws Exception {
+        final StatsTracker st = new StatsTracker();
+        //final String goog = st.googleContentRemovalProductReason();
+        final String goog = st.googleContentRemovalRequests();
+        final JSONObject googJson = (JSONObject) JSONValue.parse(goog);
+        final JSONObject norwayJson = (JSONObject) googJson.get("NO");
+        final String trans = (String) norwayJson.get(
+            "Percentage of removal requests fully or partially complied with");
+        assertEquals("100", trans);
+    }
+    
+    //@Test 
+    public void testGoogleContentRemovalProductReason() throws Exception {
+        final StatsTracker st = new StatsTracker();
+        final String goog = st.googleContentRemovalProductReason();
+        final JSONObject googJson = (JSONObject) JSONValue.parse(goog);
+        final JSONObject usJson = (JSONObject) googJson.get("US");
+        assertTrue(usJson != null);
+        final JSONObject yt = (JSONObject) usJson.get("YouTube");
+        assertTrue("No YouTube in "+usJson.toJSONString(), yt != null);
+    }
+    
+    @Test
+    public void testCountryData() throws Exception {
+        final StatsTracker st = new StatsTracker();
+        final String china = st.countryData("CN");
+        final JSONObject chinaJson2 = (JSONObject) JSONValue.parse(china);
+        
+        assertTrue(chinaJson2.containsKey("lantern"));
+        assertTrue(chinaJson2.containsKey("oni"));
+        
+        final String norway = st.countryData("NO");
+        final JSONObject noJson = (JSONObject) JSONValue.parse(norway);
+        
+        assertTrue(noJson.containsKey("lantern"));
+        assertTrue(noJson.containsKey("oni"));
+        if (!noJson.containsKey("google-content-removal-requests-by-product-and-reason.csv")) {
+            System.out.println("No JSON in "+noJson.toJSONString());
+        }
+        assertTrue("No JSON in ", noJson.containsKey("google-content-removal-requests-by-product-and-reason.csv"));
+    }
+    
+    //@Test 
+    public void testOni() throws Exception {
         final StatsTracker st = new StatsTracker();
         
         final String jsonString = st.toJson();
@@ -22,6 +66,7 @@ public class StatsTrackerTest {
         final JSONObject json = (JSONObject) JSONValue.parse(jsonString);
         final JSONArray countries = (JSONArray) json.get("countries");
         boolean foundChina = false;
+        boolean foundNorway = false;
         final Iterator<JSONObject> iter = countries.iterator();
         while (iter.hasNext()) {
             final JSONObject obj = iter.next();
@@ -32,7 +77,18 @@ public class StatsTrackerTest {
                 final JSONObject oniTest = (JSONObject) obj.get("oni");
                 assertTrue("no oni??", oniTest != null);
             }
+            if (code.equals("NO")) {
+                foundNorway = true;
+                final JSONObject oniTest = (JSONObject) obj.get("oni");
+                assertTrue("no oni??", oniTest != null);
+                final JSONObject goog1 = (JSONObject) obj.get("google-content-removal-requests-by-product-and-reason.csv");
+                assertTrue("no google in "+obj.toJSONString(), goog1 != null);
+                //final JSONObject goog2 =
+                //    (JSONObject) goog1.get("google-content-removal-requests-by-product-and-reason.csv");
+                //assertTrue("no google subdata?", goog2 != null);
+            }
         }
+        assertTrue(foundNorway);
         assertTrue(foundChina);
         
         final String oni = st.oniJson();
