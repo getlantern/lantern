435c87d77ef1ceb23a13fd64332cd863a21d423d
Willie Forkner <forkner@beercan.me>
2011-12-13 17:44:19 -0500

    remove unused imports

--- src/main/java/org/lantern/DefaultLocalCipherProvider.java
+++ src/main/java/org/lantern/DefaultLocalCipherProvider.java
@@ -19,18 +19,6 @@ import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.PBEParameterSpec;
 import org.apache.commons.io.FileUtils;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Dialog;
-import org.eclipse.swt.widgets.Display; 
-import org.eclipse.swt.widgets.Event; 
-import org.eclipse.swt.widgets.Label; 
-import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.Shell; 
-import org.eclipse.swt.widgets.Text;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
