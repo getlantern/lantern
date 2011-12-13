8625d7598aa97f9121e9a762866566b78aeb8c57
Myles Horton <myles@getlantern.org>
2011-12-12 18:48:22 -0800

    New whitelist entry file

---
+++ src/main/java/org/lantern/WhitelistEntry.java
@@ -0,0 +1,55 @@
+package org.lantern;
+
+public class WhitelistEntry {
+
+    private final String site;
+    private final boolean required;
+
+    public WhitelistEntry(final String site) {
+        this(site, false);
+    }
+
+    public WhitelistEntry(final String site, final boolean required) {
+        this.site = site;
+        this.required = required;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public boolean isRequired() {
+        return required;
+    }
+    
+    @Override 
+    public String toString() {
+        return site;
+    }
+    
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((site == null) ? 0 : site.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        WhitelistEntry other = (WhitelistEntry) obj;
+        if (site == null) {
+            if (other.site != null)
+                return false;
+        } else if (!site.equals(other.site))
+            return false;
+        return true;
+    }
+}
