659b617a702cef372719cede6969262caf0d1741
Willie Forkner <forkner@beercan.me>
2011-12-16 15:35:02 -0500

    tidy code, whitespace

--- src/main/java/org/lantern/SecretServiceLocalCipherProvider.java
+++ src/main/java/org/lantern/SecretServiceLocalCipherProvider.java
@@ -1,4 +1,4 @@
-package org.lantern; 
+package org.lantern;
 
 import com.mcdermottroe.apple.OSXKeychain;
 import com.mcdermottroe.apple.OSXKeychainException;
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Map; 
+import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
@@ -33,7 +33,6 @@ import org.freedesktop.Secret.Prompt;
 import org.freedesktop.Secret.Secret; 
 import org.freedesktop.Secret.Service;
 import org.freedesktop.Secret.Session;
-
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -60,7 +59,7 @@ public class SecretServiceLocalCipherProvider extends AbstractAESLocalCipherProv
     private static final String LANTERN_KEY_LABEL = "Lantern Local Privacy";
     private static final String SECRET_CONTENT_TYPE = "text/plain; charset=utf8";
 
-    private static final AtomicReference<Boolean> DBUS_INITIALIZED = 
+    private static final AtomicReference<Boolean> DBUS_INITIALIZED =
         new AtomicReference<Boolean>();
 
     private static final Logger LOG = LoggerFactory.getLogger(SecretServiceLocalCipherProvider.class);
@@ -73,32 +72,31 @@ public class SecretServiceLocalCipherProvider extends AbstractAESLocalCipherProv
         super(cipherParamsFile);
     }
 
-
     private static void initDBUS() throws IOException {
         synchronized(DBUS_INITIALIZED) {
-            Boolean initialized = DBUS_INITIALIZED.get();
+            final Boolean initialized = DBUS_INITIALIZED.get();
             if (initialized == null || initialized.booleanValue() == false) {
                 LanternUtils.loadJarLibrary(UnixIOException.class, "libunix-java.so");
                 DBUS_INITIALIZED.set(Boolean.TRUE);
             }
         }
     }
-    
+
     public static boolean secretServiceAvailable() {
         DBusConnection conn = null;
         try {
             LOG.debug("Checking for Secret Service API availability...");
             initDBUS();
             conn = DBusConnection.getConnection(DBusConnection.SESSION);
-            Service secretService = conn.getRemoteObject(
+            final Service secretService = conn.getRemoteObject(
                 BUS_NAME, SERVICE_PATH, Service.class);
-            Pair<Variant, Path> result = secretService.OpenSession(ALGORITHM_PLAIN, new Variant(""));
+            final Pair<Variant, Path> result = secretService.OpenSession(ALGORITHM_PLAIN, new Variant(""));
             if (NO_OBJECT.equals(result.b.getPath())) {
                 LOG.info("Failed to negotiate plain session with Secret Service API.");
                 return false;
             }
             else {
-                Session session = conn.getRemoteObject(
+                final Session session = conn.getRemoteObject(
                     BUS_NAME, result.b.getPath(), Session.class);
                 session.Close();
             }
@@ -107,14 +105,12 @@ public class SecretServiceLocalCipherProvider extends AbstractAESLocalCipherProv
         } catch (Exception e) {
             LOG.debug("Could not connect to Secret Service API at {}: {}", SERVICE_PATH, e);
             return false;
-        }
-
-        finally {
+        } finally {
             closeConnection(conn);
         }
     }
 
-    private static void closeConnection(DBusConnection conn) {
+    private static void closeConnection(final DBusConnection conn) {
         if (conn != null) {
             try {
                 conn.disconnect(); 
@@ -133,22 +129,22 @@ public class SecretServiceLocalCipherProvider extends AbstractAESLocalCipherProv
             initDBUS();
 
             conn = DBusConnection.getConnection(DBusConnection.SESSION);
-            Service secretService = conn.getRemoteObject(
+            final Service secretService = conn.getRemoteObject(
                 BUS_NAME, SERVICE_PATH, Service.class);
 
             // negotiate a "plain" session (no encryption)
-            Pair<Variant, Path> result = secretService.OpenSession(ALGORITHM_PLAIN, new Variant(""));
+            final Pair<Variant, Path> result = secretService.OpenSession(ALGORITHM_PLAIN, new Variant(""));
             if (NO_OBJECT.equals(result.b)) {
                 throw new IOException("Unable to negotiate DBus session!");
             }
-            Path sessionPath = result.b;
-            Session session = conn.getRemoteObject(BUS_NAME, sessionPath.getPath(), Session.class);
+            final Path sessionPath = result.b;
+            final Session session = conn.getRemoteObject(BUS_NAME, sessionPath.getPath(), Session.class);
 
             LOG.debug("Requesting Item....");
-            Map<String, String> secretAttrs = new HashMap<String, String>();
+            final Map<String, String> secretAttrs = new HashMap<String, String>();
             secretAttrs.put(SECRET_ATTR_NAME, 
                             SECRET_ATTR_VALUE);
-            Pair<List<Path>, List<Path>> items = secretService.SearchItems(secretAttrs);
+            final Pair<List<Path>, List<Path>> items = secretService.SearchItems(secretAttrs);
             LOG.debug("Got {} unlocked / {} locked secret items.", items.a.size(), items.b.size());
 
             Item secretItem = null;
@@ -159,7 +155,7 @@ public class SecretServiceLocalCipherProvider extends AbstractAESLocalCipherProv
                 }
                 else {
                     LOG.debug("Secret is locked! Attempting to unlock...");
-                    Path secretItemPath = items.b.get(0);
+                    final Path secretItemPath = items.b.get(0);
                     if (unlockPath(secretItemPath, secretService, conn)) {
                         LOG.debug("Assuming all is well using path {}...", secretItemPath.getPath());
                         secretItem = conn.getRemoteObject(BUS_NAME, secretItemPath.getPath(), Item.class);
@@ -172,18 +168,18 @@ public class SecretServiceLocalCipherProvider extends AbstractAESLocalCipherProv
             }
             else {
                 LOG.debug("Secret is unlocked...");
-                Path itemPath = items.a.get(0);
-                secretItem = conn.getRemoteObject(BUS_NAME, itemPath.getPath(), Item.class);    
+                final Path itemPath = items.a.get(0);
+                secretItem = conn.getRemoteObject(BUS_NAME, itemPath.getPath(), Item.class);
             }
-            Secret secret = secretItem.GetSecret(sessionPath);
+            final Secret secret = secretItem.GetSecret(sessionPath);
             encodedKey = new byte[secret.value.size()];
             int i = 0; 
-            for (Byte b : secret.value) {
+            for (final Byte b : secret.value) {
                 encodedKey[i++] = b; 
             }
             session.Close();
 
-            Base64 base64 = new Base64();
+            final Base64 base64 = new Base64();
             return base64.decode(encodedKey);
         } catch (DBusException e) {
             throw new IOException(e);
@@ -198,7 +194,7 @@ public class SecretServiceLocalCipherProvider extends AbstractAESLocalCipherProv
     }
 
     @Override
-    void storeKeyData(byte[] key) throws IOException, GeneralSecurityException {
+    void storeKeyData(final byte[] key) throws IOException, GeneralSecurityException {
         byte [] encodedKey = null;
         List<Byte> secretValue = null;
          
@@ -207,69 +203,53 @@ public class SecretServiceLocalCipherProvider extends AbstractAESLocalCipherProv
             LOG.debug("Storing key data via Secret Service API...");
             initDBUS();
             conn = DBusConnection.getConnection(DBusConnection.SESSION);
-            Service secretService = conn.getRemoteObject(
+            final Service secretService = conn.getRemoteObject(
                 BUS_NAME, SERVICE_PATH, Service.class);
 
-            // List<Collection> collections = secretService.Get(Service.INTERFACE_NAME, Service.PROPERTY_COLLECTIONS);
-            Collection collection = conn.getRemoteObject(
+            final Collection collection = conn.getRemoteObject(
                 BUS_NAME, COLLECTION_PATH, Collection.class);
 
-            // Collection properties do not seem to be implemented. 
-            // 
-            // // If the Collection is locked, ask the user to unlock it...
-            // Boolean isLocked = collection.Get(Collection.INTERFACE_NAME, 
-            //                                   Collection.PROPERTY_LOCKED);
-            // if (isLocked) {
-            //     LOG.debug("Secret collection is locked, attempting to unlock....");
-            //     if (unlockPath(new Path(COLLECTION_PATH), secretService, conn)) {
-            //         LOG.debug("Unlock worked, proceeding...");
-            //     }
-            //     else {
-            //         throw new GeneralSecurityException("Unable to unlock secret collection.");   
-            //     }
-            // }
-
-            // instead, just request an unlock in all cases 
+            // Collection properties do not seem to be implemented...
+            // So instead of checking, just request an unlock in all cases 
             if (!unlockPath(new Path(COLLECTION_PATH), secretService, conn)) {
                 throw new GeneralSecurityException("Unable to unlock secret collection.");   
             }
 
             // negotiate a "plain" session (no encryption)
-            Pair<Variant, Path> result = secretService.OpenSession(ALGORITHM_PLAIN, new Variant(""));
+            final Pair<Variant, Path> result = secretService.OpenSession(ALGORITHM_PLAIN, new Variant(""));
             if (NO_OBJECT.equals(result.b)) {
                 throw new IOException("Unable to negotiate DBus session!");
             }
-            Path sessionPath = result.b;
-            Session session = conn.getRemoteObject(
+            final Path sessionPath = result.b;
+            final Session session = conn.getRemoteObject(
                 BUS_NAME, sessionPath.getPath(), Session.class);
 
-
             // construct a "Secret"
-            Base64 base64 = new Base64();
+            final Base64 base64 = new Base64();
             encodedKey = base64.encode(key);
              // XXX eh can this be autoboxed or somthing?
             secretValue = new ArrayList<Byte>(encodedKey.length);
             for (byte b : encodedKey) {
                 secretValue.add(new Byte(b));
             }
-            Secret secret = new Secret(sessionPath, new ArrayList<Byte>(), secretValue, SECRET_CONTENT_TYPE);
-            Map<String, Variant> secretProps = new HashMap<String, Variant>();
-            secretProps.put(SECRET_LABEL_PROPERTY, 
+            final Secret secret = new Secret(sessionPath, new ArrayList<Byte>(), secretValue, SECRET_CONTENT_TYPE);
+            final Map<String, Variant> secretProps = new HashMap<String, Variant>();
+            secretProps.put(SECRET_LABEL_PROPERTY,
                             new Variant(LANTERN_KEY_LABEL));
 
             // these are the "attributes" of the item, not to be confused 
             // with properties... 
-            Map<String, String> secretAttrs = new HashMap<String, String>();
+            final Map<String, String> secretAttrs = new HashMap<String, String>();
             secretAttrs.put(SECRET_ATTR_NAME, SECRET_ATTR_VALUE);
             secretProps.put(SECRET_ATTRIBUTES_PROPERTY, new Variant(secretAttrs, "a{ss}"));
 
             // add the item to the collection...
             LOG.debug("Requesting CreateItem....");
-            Pair<Path, Path> createResult = collection.CreateItem(
+            final Pair<Path, Path> createResult = collection.CreateItem(
                 secretProps, secret, true);
 
-            Path itemPath = createResult.a;
-            Path promptPath = createResult.b;
+            final Path itemPath = createResult.a;
+            final Path promptPath = createResult.b;
 
             // user may need to be prompted... for what is unclear.
             if (!NO_OBJECT.equals(promptPath.getPath())) {
@@ -277,8 +257,8 @@ public class SecretServiceLocalCipherProvider extends AbstractAESLocalCipherProv
                 // there does not seem to be a specification of what this means in this case... ?
                 // possibly the path to the item or / on failure?
                 LOG.debug("Prompt completed with dismissed={} result={}", sig.dismissed, sig.result);
-                List<Path> createdItemPaths = (List<Path>) sig.result.getValue();
-                Path createdItemPath = null; 
+                final List<Path> createdItemPaths = (List<Path>) sig.result.getValue();
+                Path createdItemPath = null;
                 if (createdItemPaths.size() > 0 ) {
                     createdItemPath = createdItemPaths.get(0);
                 }
@@ -297,7 +277,7 @@ public class SecretServiceLocalCipherProvider extends AbstractAESLocalCipherProv
             throw new IOException(e);
         } catch (InterruptedException e) {
             throw new IOException(e);
-        }finally {
+        } finally {
             if (encodedKey != null) {
                 Arrays.fill(encodedKey, (byte) 0);
             }
@@ -305,12 +285,12 @@ public class SecretServiceLocalCipherProvider extends AbstractAESLocalCipherProv
         }
     }
 
-    private boolean unlockPath(final Path path, Service secretService, DBusConnection conn) 
+    private boolean unlockPath(final Path path, final Service secretService, final DBusConnection conn)
         throws DBusException, InterruptedException {
         LOG.debug("Requesting unlock of path {}", path.getPath());
-        List<Path> pathsToUnlock = new ArrayList<Path>(1);
+        final List<Path> pathsToUnlock = new ArrayList<Path>(1);
         pathsToUnlock.add(path);
-        Pair<List<Path>, Path> unlockResult = secretService.Unlock(pathsToUnlock);
+        final Pair<List<Path>, Path> unlockResult = secretService.Unlock(pathsToUnlock);
         if (unlockResult.a.size() > 0) {
             LOG.debug("Path unlocked without prompt...");
             return true;
@@ -320,14 +300,14 @@ public class SecretServiceLocalCipherProvider extends AbstractAESLocalCipherProv
             LOG.debug("Prompting user to unlock....");
             Prompt.Completed sig = prompt(unlockResult.b, conn);
             LOG.debug("Prompt completed with dismissed={} result={}", sig.dismissed, sig.result);
-            List<Path> unlockedPaths = (List<Path>) sig.result.getValue();
+            final List<Path> unlockedPaths = (List<Path>) sig.result.getValue();
             Path unlockedPath = null; 
             if (unlockedPaths.size() > 0) {
                 unlockedPath = unlockedPaths.get(0);
             }
             if (sig.dismissed == true || unlockedPath == null || NO_OBJECT.equals(unlockedPath.getPath())) {
                 LOG.error("Failed to unlock path {}", path.getPath());
-                return false; 
+                return false;
             }
             else {
                 return true;
@@ -337,14 +317,14 @@ public class SecretServiceLocalCipherProvider extends AbstractAESLocalCipherProv
             LOG.debug("Confused, no prompt and no unlocked objects, failing.");
             return false;
         }
-    } 
-
-    private Prompt.Completed prompt(final Path promptPath, DBusConnection conn) 
+    }
+    
+    private Prompt.Completed prompt(final Path promptPath, final DBusConnection conn)
         throws InterruptedException, DBusException {
         LOG.debug("Prompt required...");
         final Prompt prompt = conn.getRemoteObject(
             BUS_NAME, promptPath.getPath(), Prompt.class);
-        
+
         final PromptHandler handler = new PromptHandler();
         conn.addSigHandler(Prompt.Completed.class, prompt, handler);
         LOG.debug("added signal handler, calling prompt");
@@ -363,7 +343,7 @@ public class SecretServiceLocalCipherProvider extends AbstractAESLocalCipherProv
      * Condition variable. 
      */
     private class PromptHandler implements DBusSigHandler {
-       
+
         private final Lock lock; 
         private final Condition gotResult; 
         private DBusSignal sig = null;
@@ -372,9 +352,9 @@ public class SecretServiceLocalCipherProvider extends AbstractAESLocalCipherProv
             lock = new ReentrantLock();
             gotResult = lock.newCondition();
         }
-        
+
         @Override
-        public void handle(DBusSignal sig) {        
+        public void handle(final DBusSignal sig) {
             this.sig = sig;
             lock.lock();
             try {
