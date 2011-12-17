3a600957bf5638c3e2a2ded9058bc1f653f18d3b
Myles Horton <myles@getlantern.org>
2011-12-16 17:47:11 -0800

    Refactored some of the pub sub stuff and added notification to browser client of connectivity changes

--- src/main/java/org/lantern/PresenceListener.java
+++ src/main/java/org/lantern/PresenceListener.java
@@ -2,10 +2,15 @@ package org.lantern;
 
 import org.jivesoftware.smack.packet.Presence;
 
+/**
+ * Listener for changes to the presence of peers.
+ */
 public interface PresenceListener {
 
     void onPresence(String address, Presence presence);
 
     void removePresence(String address);
+
+    void presencesUpdated();
 
 }
