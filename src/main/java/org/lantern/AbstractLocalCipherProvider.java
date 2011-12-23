2390200b3e47c542b0881e422714467b696b1e3b
Willie Forkner <forkner@beercan.me>
2011-12-23 12:29:44 -0500

    update keychain library, use modification vs. delete/add for existing entries, cleanup, validate stored keys in all providers

--- src/main/java/org/lantern/AbstractLocalCipherProvider.java
+++ src/main/java/org/lantern/AbstractLocalCipherProvider.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 import java.security.AlgorithmParameters;
 import java.security.GeneralSecurityException;
 import java.security.Key;
-import java.security.spec.KeySpec;
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+import java.util.Arrays;
 import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,17 +25,25 @@ abstract class AbstractLocalCipherProvider implements LocalCipherProvider {
     public static File DEFAULT_CIPHER_PARAMS_FILE =
             new File(LanternUtils.configDir(), "cipher.params");
     
+    public static final File DEFAULT_VALIDATOR_FILE = 
+        new File(LanternUtils.configDir(), "cipher.validator");
+    
+    private static final int VALIDATOR_SALT_BYTES = 8;
+    
     private final Logger log = LoggerFactory.getLogger(getClass());
     private final File paramsFile;
+    private final File validatorFile;
     private Key localKey = null;
     
     AbstractLocalCipherProvider() {
-        this(DEFAULT_CIPHER_PARAMS_FILE);
-    }
-    AbstractLocalCipherProvider(File cipherParamsFile) {
-        this.paramsFile = cipherParamsFile;
+        this(DEFAULT_VALIDATOR_FILE, DEFAULT_CIPHER_PARAMS_FILE);
     }
     
+    AbstractLocalCipherProvider(File validatorFile, File cipherParamsFile) {
+        this.validatorFile = validatorFile;
+        this.paramsFile = cipherParamsFile;
+    }
+        
     /** 
      * return the identifier for the algorithm that should be used
      * by the created Cipher and related classes -- eg "AES"
@@ -101,10 +109,60 @@ abstract class AbstractLocalCipherProvider implements LocalCipherProvider {
         params.init(encodedParams);
         return params;
     }
-    
+
     void saveParameters(final Cipher cipher) throws IOException {
         final AlgorithmParameters params = cipher.getParameters();
         final byte[] encodedParams = params.getEncoded();
         FileUtils.writeByteArrayToFile(paramsFile, encodedParams);
     }
+
+    boolean checkKeyValid(byte[] key, byte[] validator) throws IOException, GeneralSecurityException {
+        final byte[] salt = Arrays.copyOf(validator, VALIDATOR_SALT_BYTES);
+        return Arrays.equals(validator, createValidator(key, salt));
+    }
+
+    byte[] createValidator(byte []key) throws GeneralSecurityException, IOException {
+        // generate new salt bytes
+        final byte[] salt = new byte[VALIDATOR_SALT_BYTES];
+        final SecureRandom secureRandom = LanternHub.secureRandom();
+        secureRandom.nextBytes(salt);
+        return createValidator(key, salt);
+    }
+
+    byte[] createValidator(byte [] key, byte[] salt) throws IOException, GeneralSecurityException {
+        // the validator is the concatenation of the salt value 
+        // and the sha256 digest of the salt and the key
+        final MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(salt);
+        md.update(key);
+        final byte[] hash = md.digest();
+        final byte[] validator = new byte[salt.length + hash.length];
+        System.arraycopy(salt, 0, validator, 0, salt.length);
+        System.arraycopy(hash, 0, validator, salt.length, hash.length);
+        return validator; 
+    }
+
+    byte[] loadValidator() throws IOException {
+        return FileUtils.readFileToByteArray(validatorFile);
+    }
+
+    /** 
+     * create and store a new validator for the key given.
+     */
+    void storeValidator(byte []validator) throws IOException, GeneralSecurityException {
+        FileUtils.writeByteArrayToFile(validatorFile, validator);
+    }
+    
+    void zeroFill(char[] array) {
+        if (array != null) {
+            Arrays.fill(array, '\0');
+        }
+    }
+
+    void zeroFill(byte[] array) {
+        if (array != null) {
+            Arrays.fill(array, (byte) 0);
+        }
+    }
+    
 }
