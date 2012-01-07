a24e77696bac2421b29d158d8f7f3feb55948259
Myles Horton <myles@getlantern.org>
2012-01-07 01:24:56 -0800

    Added support for modifying trusted peers #70

--- src/main/java/org/lantern/DefaultTrustedContactsManager.java
+++ src/main/java/org/lantern/DefaultTrustedContactsManager.java
@@ -2,11 +2,9 @@ package org.lantern;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.security.GeneralSecurityException;
