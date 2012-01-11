ef83ec4c152c9bbad53446d96964da1aca26f588
Myles Horton <myles@getlantern.org>
2012-01-10 17:09:44 -0800

    Randomized localhost relay port

--- src/main/java/org/lantern/LanternConstants.java
+++ src/main/java/org/lantern/LanternConstants.java
@@ -26,7 +26,9 @@ public class LanternConstants {
      * This is the local proxy port data is relayed to on the "server" side
      * of P2P connections.
      */
-    public static final int PLAINTEXT_LOCALHOST_PROXY_PORT = 7777;
+    public static final int PLAINTEXT_LOCALHOST_PROXY_PORT = 
+        LanternUtils.randomPort();
+    
     public static final int LANTERN_LOCALHOST_HTTP_PORT = 8787;
     
     public static final String USER_NAME = "un";
