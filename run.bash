26b78d8dcf9bb5066dfefb2ff1cf5426274ef136
Willie Forkner <forkner@beercan.me>
2012-01-18 15:17:32 -0500

    Add helper script to run lantern with a port available for a java debugger to attach to

--- run.bash
+++ run.bash
@@ -29,6 +29,12 @@ fullPath=`dirname $0`
 jar=`find $fullPath/target/*with-dependencies.jar`
 cp=`echo $jar | sed 's,./,'$fullPath'/,'`
 javaArgs="-jar "$cp" $*"
+
+if [ "$RUN_LANTERN_DEBUG_PORT" ]
+    then
+    javaArgs="-Xdebug -Xrunjdwp:transport=dt_socket,address=$RUN_LANTERN_DEBUG_PORT,server=y,suspend=y "+$javaArgs
+fi
+
 uname -a | grep Darwin && extras="-XstartOnFirstThread"
 
 echo "Running using Java on path at `which java` with args $javaArgs"
