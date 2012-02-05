26f09ba69dd10be29ffc187f1d9bf925e518cd78
Myles Horton <myles@getlantern.org>
2012-02-05 11:18:43 -0800

    Added fix for invalid pom issue based on forkner's examples and add a quickRun script to run when lantern's already built

--- run.bash
+++ run.bash
@@ -7,12 +7,12 @@ function die() {
 mvn --version || die "Please install maven from http://maven.apache.org" 
 
 #pushd ..
-test -d target || mvn install:install-file -DgroupId=org.eclipse.swt.cocoa -DartifactId=x86 -Dversion=3.7 -Dpackaging=jar -Dfile=lib/swt-3.7-cocoa-macosx.jar
-test -d target || mvn install:install-file -DgroupId=org.eclipse.swt.cocoa -DartifactId=x86_64 -Dversion=3.7 -Dpackaging=jar -Dfile=lib/swt-3.7-cocoa-macosx-x86_64.jar
-test -d target || mvn install:install-file -DgroupId=org.eclipse.swt.win32.win32 -DartifactId=x86 -Dversion=3.7 -Dpackaging=jar -Dfile=lib/swt-3.7-win32-win32-x86.jar
-test -d target || mvn install:install-file -DgroupId=org.eclipse.swt.gtk.linux -DartifactId=x86 -Dversion=3.7 -Dpackaging=jar -Dfile=lib/swt-3.7-gtk-linux-x86.jar
-test -d target || mvn install:install-file -DgroupId=org.eclipse.swt.gtk.linux -DartifactId=x86_64 -Dversion=3.7 -Dpackaging=jar -Dfile=lib/swt-3.7-gtk-linux-x86_64.jar
-test -d target || mvn install:install-file -DgroupId=net.sourceforge.jdpapi -DartifactId=jdpapi-java -Dversion=1.0.1 -Dpackaging=jar -Dfile=lib/jdpapi-java-1.0.1.jar
+test -d target || mvn install:install-file -DgroupId=org.eclipse.swt.cocoa -DartifactId=x86 -Dversion=3.7 -Dpackaging=jar -Dfile=lib/swt-3.7-cocoa-macosx.jar -DgeneratePom=true
+test -d target || mvn install:install-file -DgroupId=org.eclipse.swt.cocoa -DartifactId=x86_64 -Dversion=3.7 -Dpackaging=jar -Dfile=lib/swt-3.7-cocoa-macosx-x86_64.jar -DgeneratePom=true
+test -d target || mvn install:install-file -DgroupId=org.eclipse.swt.win32.win32 -DartifactId=x86 -Dversion=3.7 -Dpackaging=jar -Dfile=lib/swt-3.7-win32-win32-x86.jar -DgeneratePom=true
+test -d target || mvn install:install-file -DgroupId=org.eclipse.swt.gtk.linux -DartifactId=x86 -Dversion=3.7 -Dpackaging=jar -Dfile=lib/swt-3.7-gtk-linux-x86.jar -DgeneratePom=true
+test -d target || mvn install:install-file -DgroupId=org.eclipse.swt.gtk.linux -DartifactId=x86_64 -Dversion=3.7 -Dpackaging=jar -Dfile=lib/swt-3.7-gtk-linux-x86_64.jar -DgeneratePom=true
+test -d target || mvn install:install-file -DgroupId=net.sourceforge.jdpapi -DartifactId=jdpapi-java -Dversion=1.0.1 -Dpackaging=jar -Dfile=lib/jdpapi-java-1.0.1.jar -DgeneratePom=true
 test -d target || mvn install:install-file -DgroupId=com.mcdermottroe.apple -DartifactId=osx-keychain -Dversion=0.1.3 -Dpackaging=jar -Dfile=lib/osxkeychain-0.1.3.jar -DgeneratePom=true
 test -d target || mvn install:install-file -DgroupId=cx.ath.matthew -DartifactId=hexdump -Dversion=0.2 -Dpackaging=jar -Dfile=lib/hexdump-0.2.jar -DgeneratePom=true
 test -d target || mvn install:install-file -DgroupId=cx.ath.matthew -DartifactId=unix-java-x86 -Dversion=0.5 -Dpackaging=jar -Dfile=lib/unix-0.5-x86.jar -DgeneratePom=true
@@ -20,23 +20,6 @@ test -d target || mvn install:install-file -DgroupId=cx.ath.matthew -DartifactId
 test -d target || mvn install:install-file -DgroupId=cx.ath.matthew -DartifactId=debug -Dversion=1.1 -Dpackaging=jar -Dfile=lib/debug-disable-1.1.jar -DgeneratePom=true
 test -d target || mvn install:install-file -DgroupId=org.freedesktop.dbus -DartifactId=dbus-java -Dversion=2.7 -Dpackaging=jar -Dfile=lib/libdbus-java-2.7.jar -DgeneratePom=true
 
-
-
 mvn package -Dmaven.test.skip=true || die "Could not package"
-#popd
 
-fullPath=`dirname $0`
-jar=`find $fullPath/target/*with-dependencies.jar`
-cp=`echo $jar | sed 's,./,'$fullPath'/,'`
-javaArgs="-jar "$cp" $*"
-
-if [ "$RUN_LANTERN_DEBUG_PORT" ]
-    then
-    javaArgs="-Xdebug -Xrunjdwp:transport=dt_socket,address=$RUN_LANTERN_DEBUG_PORT,server=y,suspend=y $javaArgs"
-fi
-
-uname -a | grep Darwin && extras="-XstartOnFirstThread"
-
-echo "Running using Java on path at `which java` with args $javaArgs"
-java $extras $javaArgs || die "Java process exited abnormally"
-#java $javaArgs org.lantern.Launcher || die "Java process exited abnormally"
+./quickRun.bash $*
