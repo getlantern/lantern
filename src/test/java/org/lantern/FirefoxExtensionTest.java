9e44d9012c4d2a4e239505e7fd96f28c7e309a1c
Myles Horton <myles@getlantern.org>
2012-02-14 18:31:57 -0800

    Change to ensure FF extension is always installed regardless of startup state #168

--- src/test/java/org/lantern/FirefoxExtensionTest.java
+++ src/test/java/org/lantern/FirefoxExtensionTest.java
@@ -4,13 +4,19 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 
 public class FirefoxExtensionTest {
 
     @Test public void testCopy() throws Exception {
-        final File dest = Configurator.copyFireFoxExtension();
+        final String extName = "lantern@getlantern.org";
+        final File dest = new File(Configurator.getExtensionDir(), extName);
+
+        FileUtils.deleteDirectory(dest);
+        
+        Configurator.copyFireFoxExtension();
         assertTrue("Did not create directory!", dest.isDirectory());
         dest.delete();
     }
