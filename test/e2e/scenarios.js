559e62649fb746869f89a8a542ad23efbdbd622b
Igor Minar <igor@angularjs.org>
2012-05-15 00:20:22 -0700

    fix typo in sample e2e test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -36,7 +36,7 @@ describe('my app', function() {
     });
 
 
-    it('should render view1 when user navigates to /view2', function() {
+    it('should render view2 when user navigates to /view2', function() {
       expect(element('[ng-view] p:first').text()).
         toMatch(/partial for view 2/);
     });
