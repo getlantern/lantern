65cb69135ee76996f59cedce3054f18016a32a86
Igor Minar <igor@angularjs.org>
2012-06-11 00:56:16 -0700

    migrate code to 1.0.0rc11

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {template: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {template: 'partials/partial2.html', controller: MyCtrl2});
+    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
+    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
     $routeProvider.otherwise({redirectTo: '/view1'});
   }]);
