3f74369f2e5ab09c1793827b831d8ecffc26110d
Myles Horton <myles@getlantern.org>
2012-07-06 19:14:15 -0700

    Scripting upload of meta refresh file, deploy refactorings #153

---
+++ installMetaRefresh.bash
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+function die() {
+  echo $*
+  exit 1
+}
+
+if [ $# -ne "3" ]
+then
+    die "$0: Received $# args... dir, name, and name of latest file (latest.dmg) required"
+fi
+dir=$1
+name=$2
+latestName=$3
+
+bucket=lantern
+url=http://$bucket.s3.amazonaws.com/$name
+echo "Uploading to http://cdn.getlantern.org/$name..."
+aws -putp $bucket $name
+echo "Uploaded lantern to http://cdn.getlantern.org/$name"
+echo "Also available at $url"
+
+
+pushd install/$dir || die "Could not change directories"
+perl -pi -e "s;url_token;$url;g" $latestName || die "Could not replace URL token"
+
+# Here's the trick -- send a custom mime type that's html instead of the mime type for the file extension
+aws -putpm $bucket $latestName text/html || die "Could not upload latest?"
+git checkout $latestName || die "Could not checkout"
+popd
+
