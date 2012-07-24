b2f872ec285fe7ada21b3af4a2993be147f9bcf7
Myles Horton <myles@getlantern.org>
2012-07-24 12:03:21 -0700

    Added password handling

--- winInstall.bash
+++ winInstall.bash
@@ -5,15 +5,18 @@ function die() {
   exit 1
 }
 
-if [ $# -ne "1" ]
+if [ $# -lt "1" ]
 then
     die "$0: Received $# args... version required"
 fi
 
 VERSION=$1
+
+# This may or may not be there -- will just prompt if not.
+INSTALL4J_PASS=$2
 ./installerBuild.bash $VERSION "-Dsun.arch.data.model=32 -Pwindows" || die "Could not build?"
 
-/Applications/install4j\ 5/bin/install4jc -m windows -r $VERSION ./install/lantern.install4j || die "Could not build installer"
+/Applications/install4j\ 5/bin/install4jc --win-keystore-password=$INSTALL4J_PASS -m windows -r $VERSION ./install/lantern.install4j || die "Could not build installer"
 
 name=lantern-$VERSION.exe
 mv install/Lantern.exe $name
