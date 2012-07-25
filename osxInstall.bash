ba8418e2bb10e03143534aac4b89ce1c8c327956
Myles Horton <myles@getlantern.org>
2012-07-25 12:36:09 -0700

    fixed cert password handling

--- osxInstall.bash
+++ osxInstall.bash
@@ -9,12 +9,15 @@ if [ $# -ne "2" ]
 then
     die "$0: Received $# args... version and cert password required"
 fi
-./installerBuild.bash $* || die "Could not build!!"
 
 VERSION=$1
 INSTALL4J_PASS=$2
 
+./installerBuild.bash $VERSION || die "Could not build!!"
+
+
 /Applications/install4j\ 5/bin/install4jc --mac-keystore-password=$INSTALL4J_PASS -m macos -r $VERSION ./install/lantern.install4j
+#/Applications/install4j\ 5/bin/install4jc -m macos -r $VERSION ./install/lantern.install4j
 
 name=lantern-$VERSION.dmg
 mv install/Lantern.dmg $name
--- winInstall.bash
+++ winInstall.bash
@@ -12,11 +12,11 @@ fi
 
 VERSION=$1
 
-# This may or may not be there -- will just prompt if not.
 INSTALL4J_PASS=$2
 ./installerBuild.bash $VERSION "-Dsun.arch.data.model=32 -Pwindows" || die "Could not build?"
 
 /Applications/install4j\ 5/bin/install4jc --win-keystore-password=$INSTALL4J_PASS -m windows -r $VERSION ./install/lantern.install4j || die "Could not build installer"
+#/Applications/install4j\ 5/bin/install4jc -m windows -r $VERSION ./install/lantern.install4j || die "Could not build installer"
 
 name=lantern-$VERSION.exe
 mv install/Lantern.exe $name
