cd71dc353b99ce53b90c1f1c4be22c43ffac6c45
Myles Horton <myles@getlantern.org>
2012-08-08 13:24:13 -0700

    Switched to dummy credentials #338

--- src/test/java/org/lantern/DefaultXmppHandlerTest.java
+++ src/test/java/org/lantern/DefaultXmppHandlerTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertTrue;
 import java.util.Collection;
 import java.util.HashSet;
 
-import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,21 +20,18 @@ public class DefaultXmppHandlerTest {
     /**
      * Make sure we're getting messages back from the controller.
      * 
-     * @throws Exception If anything goes haywire.
+     * @throws Exception If anything goes wrong.
      */
     @Test public void testControllerMessages() throws Exception {
-        final String email = LanternHub.settings().getEmail();
-        final String pwd = LanternHub.settings().getPassword();
-        if (StringUtils.isBlank(email) || StringUtils.isBlank(pwd)) {
-            LOG.info("user name and password not configured");
-            return;
-        }
+        final String email = "lanternuser@gmail.com";
+        final String pwd = "aKD13DAWd82";
         
+        LanternHub.resetSettings(true);
         final Settings settings = LanternHub.settings();
         settings.setProxies(new HashSet<String>());
         final XmppHandler handler = new DefaultXmppHandler();
         
-        handler.connect();
+        handler.connect(email, pwd);
         
         Collection<String> proxies = new HashSet<String>();
         
