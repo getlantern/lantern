5710f2a3da6520f3a00d620d4d4eab1c3a3679fc
Myles Horton <myles@getlantern.org>
2012-09-10 17:07:08 -0700

    Fixed handling of new code signing #386

--- allInstallers.bash
+++ allInstallers.bash
@@ -5,15 +5,16 @@ function die() {
   exit 1
 }
 
-if [ $# -ne "2" ]
+if [ $# -ne "3" ]
 then
-    die "$0: Received $# args... version and cert password required"
+    die "$0: Received $# args... version osx-cert-password win-cert-passwork required"
 fi
 
 VERSION=$1
-INSTALL4J_PASS=$2
-./osxInstall.bash $VERSION $INSTALL4J_PASS || die "Could not build OSX"
-./winInstall.bash $VERSION $INSTALL4J_PASS || die "Could not build windows"
+INSTALL4J_KEY_OSX=$2
+INSTALL4J_KEY_WIN=$2
+./osxInstall.bash $VERSION $INSTALL4J_KEY_OSX || die "Could not build OSX"
+./winInstall.bash $VERSION $INSTALL4J_KEY_WIN || die "Could not build windows"
 ./debInstall32Bit.bash $VERSION || die "Could not build linux 32 bit"
 ./debInstall64Bit.bash $VERSION || die "Could not build linux 64 bit"
 
