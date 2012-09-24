e031ce37d0e1c5c9a481c94c792b7aec234ea6b5
Myles Horton <myles@getlantern.org>
2012-09-23 17:48:06 -0700

    Added new deb deploy scripts, updated README, etc

--- debDeploy.bash
+++ debDeploy.bash
@@ -11,13 +11,13 @@ then
 fi
 VERSION=$1
 ARCH=$2
-./installerBuild.bash $VERSION "-Dsun.arch.data.model=$ARCH -Plinux" || die "Could not build!!"
+#./installerBuild.bash $VERSION "-Dsun.arch.data.model=$ARCH -Plinux" || die "Could not build!!"
 
 #/Applications/install4j\ 5/bin/install4jc -m linuxDeb -r $VERSION ./install/lantern.install4j
 
-#name=lantern-$VERSION-$ARCH-bit.deb
+name=lantern-$VERSION-$ARCH-bit.deb
 #mv install/lantern*$ARCH*.deb $name || die "Could not find built installer?"
 
-#./installMetaRefresh.bash linux $name latest-$ARCH.deb
+./installMetaRefresh.bash linux $name latest-$ARCH.deb
 
-#cp $name ~/Desktop/virtual-machine-files/
+cp $name ~/Desktop/virtual-machine-files/
