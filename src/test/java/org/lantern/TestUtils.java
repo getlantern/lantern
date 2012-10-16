1ff6bbb7313e11c043cea90852b9334048588352
Myles Horton <myles@getlantern.org>
2012-10-16 12:50:34 -0700

    better google talk tests

---
+++ src/test/java/org/lantern/TestUtils.java
@@ -0,0 +1,37 @@
+package org.lantern;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.commons.io.IOUtils;
+
+public class TestUtils {
+
+    private static final File propsFile = 
+        new File("src/main/resources/test.properties");
+    
+    private static final Properties props = new Properties();
+    
+    static {
+        InputStream is = null;
+        try {
+            is = new FileInputStream(propsFile);
+            props.load(is);
+        } catch (final IOException e) {
+            e.printStackTrace();
+        } finally {
+            IOUtils.closeQuietly(is);
+        }
+        
+    }
+    public static String loadTestEmail() {
+        return props.getProperty("email");
+    }
+    
+    public static String loadTestPassword() {
+        return props.getProperty("pass");
+    }
+}
