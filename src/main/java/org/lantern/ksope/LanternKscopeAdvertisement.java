b7fe986f825c821b03c2231494345746d3b3d824
Myles Horton <myles@getlantern.org>
2012-10-24 14:12:16 -0700

    Neglected to add lantern kscope advertisment class for #448

---
+++ src/main/java/org/lantern/ksope/LanternKscopeAdvertisement.java
@@ -0,0 +1,54 @@
+package org.lantern.ksope;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Advertisement for a Lantern node to be distributed using the Kaleidoscope
+ * limited advertisement protocol.
+ */
+public class LanternKscopeAdvertisement {
+
+    private final String jid;
+    
+    private final String address;
+    
+    private final int port;
+    
+    public LanternKscopeAdvertisement(final String jid) {
+        this(jid, "", 0);
+    }
+
+    public LanternKscopeAdvertisement(final String jid, final InetAddress addr, 
+        final int port) {
+        this(jid, addr.getHostAddress(), port);
+    }
+    
+    public LanternKscopeAdvertisement(final String jid, final String addr, 
+        final int port) {
+        this.jid = jid;
+        this.address = addr;
+        this.port = port;
+    }
+
+    public String getJid() {
+        return jid;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getPort() {
+        return port;
+    }
+    
+    public boolean hasMappedEndpoint() {
+        try {
+            InetAddress.getAllByName(address);
+            return this.port > 1;
+        } catch (final UnknownHostException e) {
+            return false;
+        }
+    }
+}
