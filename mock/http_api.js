2f65c2f04ac30f9a60e605ab5219952ebaea8b62
_pants <_pants@anotherwebsite.org>
2012-11-19 22:32:42 -0500

    "this._bayeuxBackend.publishSync" -> "publishSync"

--- mock/http_api.js
+++ mock/http_api.js
@@ -201,6 +201,7 @@ var MODALSEQ_GIVE = [MODAL.welcome, MODAL.authorize, MODAL.inviteFriends, MODAL.
  * */
 ApiServlet._advanceModal = function(backToIfNone) {
   var model = this._bayeuxBackend.model
+    , publishSync = this._bayeuxBackend.publishSync.bind(this._bayeuxBackend)
     , modalSeq = inGiveMode(model) ? MODALSEQ_GIVE : MODALSEQ_GET
     , next;
   for (var i=0; this._internalState.modalsCompleted[next=modalSeq[i++]];);
@@ -209,38 +210,40 @@ ApiServlet._advanceModal = function(backToIfNone) {
   model.modal = next;
   util.puts('modalsCompleted: ' + util.inspect(this._internalState.modalsCompleted));
   util.puts('next modal: ' + next);
-  this._bayeuxBackend.publishSync('modal');
+  publishSync('modal');
 };
 
 ApiServlet._tryConnect = function(model) {
-  var userid = model.settings.userid;
+  var userid = model.settings.userid
+    , publishSync = this._bayeuxBackend.publishSync.bind(this._bayeuxBackend)
+    ;
 
   // connect to google talk
   model.connectivity.gtalk = CONNECTIVITY.connecting;
-  this._bayeuxBackend.publishSync('connectivity.gtalk');
+  publishSync('connectivity.gtalk');
   model.modal = MODAL.gtalkConnecting;
-  this._bayeuxBackend.publishSync('modal');
+  publishSync('modal');
   sleep.usleep(3000000);
   if (userid ==  'user_cant_reach_gtalk@example.com') {
     model.connectivity.gtalk = CONNECTIVITY.notConnected;
-    this._bayeuxBackend.publishSync('connectivity.gtalk');
+    publishSync('connectivity.gtalk');
     model.modal = MODAL.gtalkUnreachable;
-    this._bayeuxBackend.publishSync('modal');
+    publishSync('modal');
     util.puts("user can't reach google talk, set modal to "+MODAL.gtalkUnreachable);
     return;
   }
   model.connectivity.gtalk = CONNECTIVITY.connected;
-  this._bayeuxBackend.publishSync('connectivity.gtalk');
+  publishSync('connectivity.gtalk');
 
   // refresh roster
   model.roster = roster;
-  this._bayeuxBackend.publishSync('roster');
+  publishSync('roster');
   sleep.usleep(250000);
 
   // check for lantern access
   if (userid != 'user_invited@example.com') {
     model.modal = MODAL.notInvited;
-    this._bayeuxBackend.publishSync('modal');
+    publishSync('modal');
     util.puts("user does not have Lantern access, set modal to "+MODAL.notInvited);
     return;
   }
@@ -249,7 +252,7 @@ ApiServlet._tryConnect = function(model) {
   // (advertised by online Lantern friends or remembered from previous connection)
   model.connectivity.peers.current = [peer1.peerid, peer2.peerid, peer3.peerid, peer4.peerid, peer5.peerid];
   model.connectivity.peers.lifetime = [peer1, peer2, peer3, peer4, peer5];
-  this._bayeuxBackend.publishSync('connectivity.peers');
+  publishSync('connectivity.peers');
   util.puts("user has access; connected to google talk, fetched roster:\n"+util.inspect(roster)+"\ndiscovered and connected to peers:\n"+util.inspect(model.connectivity.peers.current));
   ApiServlet._advanceModal.call(this);
 };
@@ -263,19 +266,20 @@ ApiServlet.HandlerMap = {
       } else {
         model.modal = MODAL.authorize;
         this._internalState.modalsCompleted[MODAL.passwordCreate] = true;
-        this._bayeuxBackend.publishSync('modal');
+        publishSync('modal');
       }
     },
   'settings/unlock': function(req, res) {
       var qs = url.parse(req.url, true).query 
         , model = this._bayeuxBackend.model
+        , publishSync = this._bayeuxBackend.publishSync.bind(this._bayeuxBackend)
         , password = qs.password
         ;
       if (!qs.password) {
         res.writeHead(400);
       } else if (qs.password == 'password') {
         model.modal = model.setupComplete ? MODAL.none : MODAL.welcome;
-        this._bayeuxBackend.publishSync('modal');
+        publishSync('modal');
       } else {
         res.writeHead(403);
       }
@@ -283,6 +287,7 @@ ApiServlet.HandlerMap = {
   interaction: function(req, res) {
       var qs = url.parse(req.url, true).query 
         , model = this._bayeuxBackend.model
+        , publishSync = this._bayeuxBackend.publishSync.bind(this._bayeuxBackend)
         , interaction = qs.interaction
         ;
       switch (model.modal) {
@@ -293,29 +298,29 @@ ApiServlet.HandlerMap = {
           }
           if (interaction == MODE.give && inCensoringCountry(model)) {
             model.modal = MODAL.giveModeForbidden;
-            this._bayeuxBackend.publishSync('modal');
+            publishSync('modal');
             res.writeHead(400);
             return;
           }
           model.settings.mode = interaction;
           model.modal = passwordCreateRequired(model) ?
                           MODAL.passwordCreate : MODAL.authorize;
-          this._bayeuxBackend.publishSync('settings.mode');
-          this._bayeuxBackend.publishSync('modal');
+          publishSync('settings.mode');
+          publishSync('modal');
           this._internalState.modalsCompleted[MODAL.welcome] = true;
           return;
 
         case MODAL.giveModeForbidden:
           if (interaction == INTERACTION.continue) {
             model.settings.mode = MODE.get;
-            this._bayeuxBackend.publishSync('settings.mode');
+            publishSync('settings.mode');
             this._internalState.modalsCompleted[MODAL.welcome] = true;
             ApiServlet._advanceModal.call(this, MODAL.settings);
             return;
           }
           if (interaction == INTERACTION.cancel && !this._internalState.modalsCompleted[MODAL.welcome]) {
             model.modal = MODAL.welcome;
-            this._bayeuxBackend.publishSync('modal');
+            publishSync('modal');
             return;
           }
           res.writeHead(400);
@@ -329,7 +334,7 @@ ApiServlet.HandlerMap = {
           }
           if (interaction == INTERACTION.reset) {
             model.settings.proxiedSites = this._DEFAULT_PROXIED_SITES.slice(0);
-            this._bayeuxBackend.publishSync('settings.proxiedSites');
+            publishSync('settings.proxiedSites');
             return;
           }
           res.writeHead(400);
@@ -363,7 +368,7 @@ ApiServlet.HandlerMap = {
             ApiServlet._tryConnect.call(this, model);
           } else if (interaction == INTERACTION.retryLater) {
             model.modal = MODAL.authorizeLater;
-            this._bayeuxBackend.publishSync('modal');
+            publishSync('modal');
           } else {
             res.writeHead(400);
             return;
@@ -376,9 +381,9 @@ ApiServlet.HandlerMap = {
             return;
           }
           model.modal = MODAL.none;
-          this._bayeuxBackend.publishSync('modal');
+          publishSync('modal');
           model.showVis = true;
-          this._bayeuxBackend.publishSync('showVis');
+          publishSync('showVis');
           return;
 
         case MODAL.notInvited:
@@ -387,7 +392,7 @@ ApiServlet.HandlerMap = {
             return;
           }
           model.modal = MODAL.requestInvite;
-          this._bayeuxBackend.publishSync('modal');
+          publishSync('modal');
           return;
 
         case MODAL.requestSent:
@@ -396,9 +401,9 @@ ApiServlet.HandlerMap = {
             return;
           }
           model.modal = MODAL.none;
-          this._bayeuxBackend.publishSync('modal');
+          publishSync('modal');
           model.showVis = true;
-          this._bayeuxBackend.publishSync('showVis');
+          publishSync('showVis');
           return;
 
         case MODAL.firstInviteReceived:
@@ -417,9 +422,9 @@ ApiServlet.HandlerMap = {
           this._internalState.modalsCompleted[MODAL.finished] = true;
           ApiServlet._advanceModal.call(this);
           model.setupComplete = true;
-          this._bayeuxBackend.publishSync('setupComplete');
+          publishSync('setupComplete');
           model.showVis = true;
-          this._bayeuxBackend.publishSync('showVis');
+          publishSync('showVis');
           return;
 
         case MODAL.none:
@@ -429,7 +434,7 @@ ApiServlet.HandlerMap = {
               // sign-in required
               if (model.connectivity.gtalk != CONNECTIVITY.connected) {
                 model.modal = MODAL.authorize;
-                this._bayeuxBackend.publishSync('modal');
+                publishSync('modal');
                 return;
               }
               // otherwise fall through to no-sign-in-required cases:
@@ -438,7 +443,7 @@ ApiServlet.HandlerMap = {
             case INTERACTION.updateAvailable:
             case INTERACTION.settings: // XXX check if signed in on clientside and only allow configuring settings accordingly
               model.modal = interaction;
-              this._bayeuxBackend.publishSync('modal');
+              publishSync('modal');
               return;
 
             default:
@@ -452,14 +457,14 @@ ApiServlet.HandlerMap = {
             return;
           }
           model.modal = MODAL.none;
-          this._bayeuxBackend.publishSync('modal');
+          publishSync('modal');
           return;
 
         case MODAL.settings:
           if (interaction == MODE.give || interaction == MODE.get) {
             if (interaction == MODE.give && inCensoringCountry(model)) {
               model.modal = MODAL.giveModeForbidden;
-              this._bayeuxBackend.publishSync('modal');
+              publishSync('modal');
               res.writeHead(400);
               return;
             }
@@ -467,17 +472,17 @@ ApiServlet.HandlerMap = {
             if (wasInGiveMode && model.settings.systemProxy)
               sleep.usleep(750000);
             model.settings.mode = interaction;
-            this._bayeuxBackend.publishSync('settings.mode');
+            publishSync('settings.mode');
             ApiServlet._advanceModal.call(this, MODAL.settings);
           } else if (interaction == INTERACTION.proxiedSites) {
             model.modal = MODAL.proxiedSites;
-            this._bayeuxBackend.publishSync('modal');
+            publishSync('modal');
           } else if (interaction == INTERACTION.close) {
             model.modal = MODAL.none;
-            this._bayeuxBackend.publishSync('modal');
+            publishSync('modal');
           } else if (interaction == INTERACTION.reset) {
             model.modal = MODAL.confirmReset;
-            this._bayeuxBackend.publishSync('modal');
+            publishSync('modal');
           } else {
             res.writeHead(400);
             return;
@@ -489,12 +494,12 @@ ApiServlet.HandlerMap = {
         case MODAL.confirmReset:
           if (interaction == INTERACTION.close) {
             model.modal = MODAL.none;
-            this._bayeuxBackend.publishSync('modal');
+            publishSync('modal');
             return;
           } else if (interaction == INTERACTION.reset) {
             ApiServlet._resetInternalState.call(this);
             this._bayeuxBackend.resetModel();
-            this._bayeuxBackend.publishSync();
+            publishSync();
             return;
           }
           res.writeHead(400);
@@ -507,6 +512,7 @@ ApiServlet.HandlerMap = {
     },
   'settings/': function(req, res) {
       var model = this._bayeuxBackend.model
+        , publishSync = this._bayeuxBackend.publishSync.bind(this._bayeuxBackend)
         , qs = url.parse(req.url, true).query
         , badRequest = false
         , mode = qs.mode
@@ -536,7 +542,7 @@ ApiServlet.HandlerMap = {
             if (inGiveMode(model) && mode == MODE.get && model.settings.systemProxy)
               sleep.usleep(750000);
             model.settings.mode = mode;
-            this._bayeuxBackend.publishSync('settings.mode');
+            publishSync('settings.mode');
           }
         }
         if (systemProxy) {
@@ -547,7 +553,7 @@ ApiServlet.HandlerMap = {
             systemProxy = systemProxy == 'true';
             if (systemProxy) sleep.usleep(750000);
             model.settings.systemProxy = systemProxy;
-            this._bayeuxBackend.publishSync('settings.systemProxy');
+            publishSync('settings.systemProxy');
           }
         }
         if (lang) {
@@ -557,7 +563,7 @@ ApiServlet.HandlerMap = {
             util.puts('invalid value of lang: ' + lang);
           } else {
             model.settings.lang = lang;
-            this._bayeuxBackend.publishSync('settings.lang');
+            publishSync('settings.lang');
           }
         }
         if (autoStart) {
@@ -567,7 +573,7 @@ ApiServlet.HandlerMap = {
           } else {
             autoStart = autoStart == 'true';
             model.settings.autoStart = autoStart;
-            this._bayeuxBackend.publishSync('settings.autoStart');
+            publishSync('settings.autoStart');
           }
         }
         if (autoReport) {
@@ -577,7 +583,7 @@ ApiServlet.HandlerMap = {
           } else {
             autoReport = autoReport == 'true';
             model.settings.autoReport = autoReport;
-            this._bayeuxBackend.publishSync('settings.autoReport');
+            publishSync('settings.autoReport');
           }
         }
         if (proxyAllSites) {
@@ -587,7 +593,7 @@ ApiServlet.HandlerMap = {
           } else {
             proxyAllSites = proxyAllSites == 'true';
             model.settings.proxyAllSites = proxyAllSites;
-            this._bayeuxBackend.publishSync('settings.proxyAllSites');
+            publishSync('settings.proxyAllSites');
           }
         }
         if (proxiedSites) {
@@ -598,7 +604,7 @@ ApiServlet.HandlerMap = {
             util.puts('invalid value of proxiedSites: ' + proxiedSites);
           } else {
             model.settings.proxiedSites = proxiedSites;
-            this._bayeuxBackend.publishSync('settings.proxiedSites');
+            publishSync('settings.proxiedSites');
           }
         }
       }
@@ -608,13 +614,14 @@ ApiServlet.HandlerMap = {
     },
   oauthAuthorized: function(req, res) {
       var model = this._bayeuxBackend.model
+        , publishSync = this._bayeuxBackend.publishSync.bind(this._bayeuxBackend)
         , qs = url.parse(req.url, true).query 
         , userid = qs.userid
         ;
       model.settings.userid = userid;
-      this._bayeuxBackend.publishSync('settings.userid');
+      publishSync('settings.userid');
       model.connectivity.gtalkAuthorized = true;
-      this._bayeuxBackend.publishSync('connectivity.gtalkAuthorized');
+      publishSync('connectivity.gtalkAuthorized');
       this._internalState.modalsCompleted[MODAL.authorize] = true;
       ApiServlet._tryConnect.call(this, model);
   },
@@ -630,7 +637,7 @@ ApiServlet.HandlerMap = {
       }
       sleep.usleep(750000);
       model.modal = 'requestSent';
-      this._bayeuxBackend.publishSync('modal');
+      publishSync('modal');
     }
 };
 
