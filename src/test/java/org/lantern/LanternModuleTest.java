cc0c5600ef8d4b99e8c8323a9e16da83af388ca8
Myles Horton <myles@getlantern.org>
2012-11-30 15:45:16 -0800

    Tweaked to delay loading of Display for easier testing #463

---
+++ src/test/java/org/lantern/LanternModuleTest.java
@@ -0,0 +1,25 @@
+package org.lantern;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.lantern.state.Model;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+public class LanternModuleTest {
+
+    @Test
+    public void test() {
+        final Injector injector = Guice.createInjector(new LanternModule());
+        final LanternService xmpp = 
+            injector.getInstance(DefaultXmppHandler.class);
+        
+        final Model model = injector.getInstance(Model.class);
+        assertNotNull(xmpp);
+        assertNotNull(model);
+        assertTrue(model == injector.getInstance(Model.class));
+    }
+
+}
