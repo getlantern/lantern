837372b3e8d19fce6d25ea818deaca2fc0ff3899
Myles Horton <myles@getlantern.org>
2012-12-12 12:48:50 -0800

    Fixed deb installer builds

--- debInstall32Bit.bash
+++ debInstall32Bit.bash
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
+function die() {
+  echo $*
+  exit 1
+}
+
+if [ $# -ne "2" ]
+then
+    die "$0: Received $# args... version and whether or not this is a release required"
+fi
+
 ./debInstall.bash $* 32 
--- debInstall64Bit.bash
+++ debInstall64Bit.bash
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
+function die() {
+  echo $*
+  exit 1
+}
+
+if [ $# -ne "2" ]
+then
+    die "$0: Received $# args... version and whether or not this is a release required"
+fi
+#RELEASE=$2
+
 ./debInstall.bash $* 64 
