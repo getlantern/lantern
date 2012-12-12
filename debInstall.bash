17726d4da39bbd8b00fa3a6c50e7f40f228b490b
Myles Horton <myles@getlantern.org>
2012-12-12 12:48:50 -0800

    Fixed deb installer builds

--- debInstall.bash
+++ debInstall.bash
@@ -5,12 +5,14 @@ function die() {
   exit 1
 }
 
-if [ $# -ne "2" ]
+if [ $# -ne "3" ]
 then
-    die "$0: Received $# args... version and architecture required"
+    die "$0: Received $# args... version, architecture, and whether or not this is a release required"
 fi
 VERSION=$1
-ARCH=$2
+RELEASE=$2
+ARCH=$3
+
 ./installerBuild.bash $VERSION "-Dsun.arch.data.model=$ARCH -Plinux" || die "Could not build!!"
 
 /Applications/install4j\ 5/bin/install4jc -m linuxDeb -r $VERSION ./install/lantern.install4j || die "Could not build Linux installer?"
@@ -18,6 +20,6 @@ ARCH=$2
 name=lantern-$VERSION-$ARCH-bit.deb
 mv install/lantern*$ARCH*.deb $name || die "Could not find built installer?"
 
-./installMetaRefresh.bash linux $name latest-$ARCH.deb false
+./installMetaRefresh.bash linux $name latest-$ARCH.deb $RELEASE
 
 #cp $name ~/Desktop/virtual-machine-files/
