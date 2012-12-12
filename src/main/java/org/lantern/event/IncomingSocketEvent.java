2f1d5e8bce7220d77aaae8c2d2114387a8f23f39
Myles Horton <myles@getlantern.org>
2012-12-12 11:59:12 -0800

    Adding incoming socket events for #463

--- src/main/java/org/lantern/event/IncomingSocketEvent.java
+++ src/main/java/org/lantern/event/IncomingSocketEvent.java
@@ -1,17 +1,31 @@
 package org.lantern.event;
 
 import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelStateEvent;
 
 public class IncomingSocketEvent {
 
     private final Channel channel;
+    private final boolean open;
+    private final ChannelStateEvent cse;
 
-    public IncomingSocketEvent(final Channel channel) {
+    public IncomingSocketEvent(final Channel channel, 
+        final ChannelStateEvent cse, final boolean open) {
         this.channel = channel;
+        this.cse = cse;
+        this.open = open;
     }
 
     public Channel getChannel() {
         return channel;
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
+
+    public ChannelStateEvent getCse() {
+        return cse;
     }
 
 }
