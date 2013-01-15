f6c596ff26cd0b0a811e5e7f238a5b6addc69b40
Myles Horton <myles@getlantern.org>
2013-01-14 16:59:06 -0800

    Tweaked connection params

--- src/main/java/org/lantern/util/LanternHttpClient.java
+++ src/main/java/org/lantern/util/LanternHttpClient.java
@@ -3,6 +3,7 @@ package org.lantern.util;
 import org.apache.http.HttpHost;
 import org.apache.http.conn.params.ConnRoutePNames;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.CoreConnectionPNames;
 import org.lantern.LanternConstants;
 
 public class LanternHttpClient extends DefaultHttpClient {
@@ -13,5 +14,7 @@ public class LanternHttpClient extends DefaultHttpClient {
             new HttpHost(LanternConstants.FALLBACK_SERVER_HOST, 
                 Integer.valueOf(LanternConstants.FALLBACK_SERVER_PORT));
         getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY,proxy);
+        getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 50000);
+        getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, 120000);
     }
 }
