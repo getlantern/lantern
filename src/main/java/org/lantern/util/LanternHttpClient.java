388d1ce2f08888a702210f187b80d63103311d5b
Myles Horton <myles@getlantern.org>
2013-01-31 14:49:42 -0800

    Switching to https #473

--- src/main/java/org/lantern/util/LanternHttpClient.java
+++ src/main/java/org/lantern/util/LanternHttpClient.java
@@ -12,7 +12,7 @@ public class LanternHttpClient extends DefaultHttpClient {
         super();
         final HttpHost proxy = 
             new HttpHost(LanternConstants.FALLBACK_SERVER_HOST, 
-                Integer.valueOf(LanternConstants.FALLBACK_SERVER_PORT));
+                Integer.valueOf(LanternConstants.FALLBACK_SERVER_PORT), "https");
         getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY,proxy);
         getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 50000);
         getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, 120000);
