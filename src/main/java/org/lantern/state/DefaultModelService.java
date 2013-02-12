5db5d6470b8bbaed3ab6e421fea39264e78e6150
Leah X Schmidt <leahxschmidt@gmail.com>
2013-02-11 22:15:07 -0500

    formatting

--- src/main/java/org/lantern/state/DefaultModelService.java
+++ src/main/java/org/lantern/state/DefaultModelService.java
@@ -26,16 +26,16 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 /**
- * Class that does the dirty work of executing changes to the various settings 
+ * Class that does the dirty work of executing changes to the various settings
  * users can configure.
  */
 @Singleton
 public class DefaultModelService implements ModelService {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
-    
+
     private final File launchdPlist;
-    
+
     private final Executor proxyQueue = Executors.newSingleThreadExecutor(
         new ThreadFactoryBuilder().setDaemon(true).setNameFormat(
             "System-Proxy-Thread-%d").build());
@@ -54,11 +54,11 @@ public class DefaultModelService implements ModelService {
     public DefaultModelService(final Model model,
         final Proxifier proxifier, final ModelUtils modelUtils,
         final XmppHandler xmppHandler) {
-        this(LanternConstants.LAUNCHD_PLIST, LanternConstants.GNOME_AUTOSTART, 
+        this(LanternConstants.LAUNCHD_PLIST, LanternConstants.GNOME_AUTOSTART,
                 model, proxifier, modelUtils, xmppHandler);
     }
-    
-    public DefaultModelService(final File launchdPlist, 
+
+    public DefaultModelService(final File launchdPlist,
         final File gnomeAutostart, final Model model,
         final Proxifier proxifier, final ModelUtils modelUtils,
         final XmppHandler xmppHandler) {
@@ -69,11 +69,11 @@ public class DefaultModelService implements ModelService {
         this.modelUtils = modelUtils;
         this.xmppHandler = xmppHandler;
     }
-    
+
     @Override
     public void setRunAtSystemStart(final boolean runOnSystemStartup) {
         log.debug("Setting start at login to "+runOnSystemStartup);
-        
+
         this.model.getSettings().setRunAtSystemStart(runOnSystemStartup);
         Events.sync(SyncPath.START_AT_LOGIN, runOnSystemStartup);
         if (SystemUtils.IS_OS_MAC_OSX && this.launchdPlist.isFile()) {
@@ -89,26 +89,26 @@ public class DefaultModelService implements ModelService {
     }
 
     public void setStartAtLoginOsx(final boolean start) {
-        LanternUtils.replaceInFile(this.launchdPlist, 
+        LanternUtils.replaceInFile(this.launchdPlist,
                 "<"+!start+"/>", "<"+start+"/>");
     }
 
     public void setStartAtLoginLinux(final boolean start) {
-        LanternUtils.replaceInFile(this.gnomeAutostart, 
+        LanternUtils.replaceInFile(this.gnomeAutostart,
             "X-GNOME-Autostart-enabled="+!start, "X-GNOME-Autostart-enabled="+start);
     }
 
     public void setStartAtLoginWindows(final boolean start) {
-        final String key = 
+        final String key =
             "Software\\Microsoft\\Windows\\CurrentVersion\\Run";
         int result = 0;
         if (start) {
             try {
-                final String path = 
+                final String path =
                     "\""+new File("Lantern.exe").getCanonicalPath()+"\"" + " --launchd";
                     //"\"\\\""+new File("Lantern.exe").getCanonicalPath()+"\\\"\"" + " --launchd";
-                
-                
+
+
                 Registry.write(key, "Lantern", path);
             } catch (final IOException e) {
                 log.error("Could not get canonical path", e);
@@ -116,7 +116,7 @@ public class DefaultModelService implements ModelService {
         } else {
             Registry.write(key, "Lantern", "");
         }
-        
+
         if (result != 0) {
             log.error("Error changing startAtLogin? Result: "+result);
         }
@@ -153,16 +153,16 @@ public class DefaultModelService implements ModelService {
             return;
         }
         this.model.getSettings().setSystemProxy(isSystemProxy);
-        
+
         log.info("Setting system proxy");
-        
-        // Go ahead and change the setting so that it will affect. It will 
+
+        // Go ahead and change the setting so that it will affect. It will
         // be set again by the api, but that doesn't matter.
         this.model.getSettings().setSystemProxy(isSystemProxy);
         if (!this.model.isSetupComplete()) {
             return;
         }
-        
+
         final Runnable proxyRunner = new Runnable() {
             @Override
             public void run() {
@@ -183,9 +183,9 @@ public class DefaultModelService implements ModelService {
     @Override
     public void setMode(final Mode mode) {
         log.debug("Calling set get mode. Get is: "+mode);
-        // When we move to give mode, we want to start advertising our 
+        // When we move to give mode, we want to start advertising our
         // ID and to start accepting incoming connections.
-        
+
         // We we move to get mode, we want to stop advertising our ID and to
         // stop accepting incoming connections.
         final Settings set = this.model.getSettings();
@@ -193,21 +193,21 @@ public class DefaultModelService implements ModelService {
             log.info("Mode is unchanged.");
             return;
         }
-        
+
         // Go ahead and set the setting although it will also be
         // updated by the api as well. We want to make sure the
         // state seen by the following calls is consistent with
         // this flag being aspirational vs. representational
         set.setMode(mode);
-        
-        // We disconnect and reconnect to create a new Jabber ID that will 
+
+        // We disconnect and reconnect to create a new Jabber ID that will
         // not advertise us as a connection point.
         if (!model.isSetupComplete()) {
             log.debug("Not disconnecting and reconnecting before setup is " +
                 "complete");
             return;
         }
-        
+
         // We dont' want to force the frontend to wait for all of this, so we
         // thread it.
         final Runnable runner = new Runnable() {
@@ -218,7 +218,7 @@ public class DefaultModelService implements ModelService {
                 try {
                     try {
                         xmppHandler.connect();
-                        
+
                         // TODO: This isn't quite right. We don't necessarily have
                         // proxies to connect to at this point, and we shouldn't set
                         // the OS proxy until we do.
@@ -244,7 +244,7 @@ public class DefaultModelService implements ModelService {
                 }
             }
         };
-        
+
         final Thread t = new Thread(runner, "Mode-Shift-Thread");
         t.setDaemon(true);
         t.start();
@@ -256,6 +256,7 @@ public class DefaultModelService implements ModelService {
         Events.sync(SyncPath.AUTO_REPORT, autoReport);
     }
 
+    @Override
     public void invite(List<String> emails) {
         for (String email : emails) {
             xmppHandler.sendInvite(email);
