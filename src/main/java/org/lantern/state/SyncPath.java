b77f563428f0931cc6768dfbb5492a7a839019ab
Leah X Schmidt <leahxschmidt@gmail.com>
2013-02-12 16:27:33 -0500

    fix paths to be json-pointer paths

--- src/main/java/org/lantern/state/SyncPath.java
+++ src/main/java/org/lantern/state/SyncPath.java
@@ -5,25 +5,25 @@ package org.lantern.state;
  * The available channels to sync on.
  */
 public enum SyncPath {
-    SETTINGS("settings"), 
-    VERSION_UPDATED("version.latest"), 
-    ROSTER("roster"), 
-    MODAL("modal"), 
-    ALL(""), 
-    PROFILE("profile"), 
-    MODE("settings.mode"),
-    CONNECTIVITY_GTALK("connectivity.gtalk"), 
-    PEERS("connectivity.peers"), 
-    INVITED("connectivity.invited"), 
-    NINVITES("ninvites"), 
-    START_AT_LOGIN("settings.startAtLogin"),
-    AUTO_CONNECT("settings.autoConnect"),
-    AUTO_REPORT("settings.autoReport"),
-    PROXY_ALL_SITES("settings.proxyAllSites"), 
-    SUBSCRIPTION_REQUESTS("friends.pending");
-    
+    SETTINGS("settings"),
+    VERSION_UPDATED("version/latest"),
+    ROSTER("roster"),
+    MODAL("modal"),
+    ALL(""),
+    PROFILE("profile"),
+    MODE("settings/mode"),
+    CONNECTIVITY_GTALK("connectivity/gtalk"),
+    PEERS("connectivity/peers"),
+    INVITED("connectivity/invited"),
+    NINVITES("ninvites"),
+    START_AT_LOGIN("settings/startAtLogin"),
+    AUTO_CONNECT("settings/autoConnect"),
+    AUTO_REPORT("settings/autoReport"),
+    PROXY_ALL_SITES("settings/proxyAllSites"),
+    SUBSCRIPTION_REQUESTS("friends/pending");
+
     private final String path;
-    
+
     private SyncPath(final String path) {
         this.path = path;
         // We do a dummy check here to make sure to catch any bogus paths.
