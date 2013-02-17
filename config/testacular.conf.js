50015e5da7dcbaa47582d1b81df28683573508bb
_pants <_pants@anotherwebsite.org>
2013-02-17 16:46:01 -0500

    update to jquery 1.9.1, fix tests

--- config/testacular.conf.js
+++ config/testacular.conf.js
@@ -3,14 +3,16 @@ basePath = '../';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
+
   'app/lib/jquery.js',
   'app/lib/angular/angular.js',
-//'app/lib/angular/angular-*.js',
-  'app/lib/angular-ui/angular-ui.js',
-  'app/lib/bootstrap/js/bootstrap.js',
-  'app/lib/select2/select2.js',
-  'app/lib/*.js',
+//'app/lib/angular-ui/angular-ui.js',
+//'app/lib/select2/select2.js',
+  'app/lib/lodash.js',
+  'app/lib/jsonpatch.js',
+
   'app/js/*.js',
+
   'test/lib/angular/angular-mocks.js',
   'test/unit/**/*.js'
 ];
