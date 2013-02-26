f094d745f77c53a4ab39c189afa06dbbd3bdf520
Leah X Schmidt <leahxschmidt@gmail.com>
2013-02-26 16:02:20 -0500

    remove obsolete script

--- install.bash
+++ install.bash
@@ -6,6 +6,4 @@ function die() {
 
 mvn --version || die "Please install maven from http://maven.apache.org" 
 
-#pushd ..
-test -d target || ./installDeps.bash
 mvn package -Dmaven.artifact.threads=1 -Dmaven.test.skip=true || die "Could not package"
