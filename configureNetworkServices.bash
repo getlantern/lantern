f80475d37ba5a890cd0a241c4c60046622993a44
Myles Horton <myles@getlantern.org>
2013-02-27 23:09:19 -0800

    More peer details for #302

--- configureNetworkServices.bash
+++ configureNetworkServices.bash
@@ -11,11 +11,11 @@ onOff=$1
 url=$2
 #port=$3
 log "Setting to on or off: $onOff"
-#while read s; 
-#do
-    log "Configuring network:"
-    networksetup -setautoproxyurl "Wi-Fi" $url || log "Could not set auto proxy URL for $s"
-    networksetup -setautoproxystate "Wi-Fi" "$onOff" || log "Could not turn auto proxy on for $s"
-    log "Configured network: "
-#done < <(networksetup -listallnetworkservices | tail +2)
+while read s; 
+do
+    log "Configuring network: $s"
+    networksetup -setautoproxyurl "$s" $url || log "Could not set auto proxy URL for $s"
+    networksetup -setautoproxystate "$s" "$onOff" || log "Could not turn auto proxy on for $s"
+    log "Configured network: $s"
+done < <(networksetup -listallnetworkservices | tail +2)
 log "Done configuring network services!!"
