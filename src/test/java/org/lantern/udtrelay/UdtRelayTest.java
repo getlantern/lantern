27618e92ccc5c9ee11e05061242dc50c6e64b452
Myles Horton <myles@getlantern.org>
2013-03-09 17:05:04 -0800

    change test to work offline

--- src/test/java/org/lantern/udtrelay/UdtRelayTest.java
+++ src/test/java/org/lantern/udtrelay/UdtRelayTest.java
@@ -2,9 +2,14 @@ package org.lantern.udtrelay;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.concurrent.Future;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
@@ -14,10 +19,16 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
 import org.apache.http.params.CoreConnectionPNames;
 import org.apache.http.util.EntityUtils;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.handler.codec.http.HttpRequest;
 import org.junit.Test;
 import org.lantern.LanternUtils;
 import org.littleshoot.proxy.DefaultHttpProxyServer;
 import org.littleshoot.proxy.HttpProxyServer;
+import org.littleshoot.proxy.ProxyCacheManager;
+import org.littleshoot.proxy.ProxyUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -37,22 +48,49 @@ public class UdtRelayTest {
         final InetSocketAddress localRelayAddress = 
             new InetSocketAddress("127.0.0.1", relayPort);
         final UdtRelayProxy relay = 
-            new UdtRelayProxy(localRelayAddress.getPort(), "127.0.0.1", proxyPort);
+            new UdtRelayProxy(localRelayAddress.getPort(), "127.0.0.1", 
+                proxyPort);
         startRelay(relay, localRelayAddress.getPort());
         
         // We do this a few times to make sure there are no issues with 
         // subsequent runs.
         for (int i = 0; i < 3; i++) {
-            hitRelay(relayPort);
+            hitRelayRaw(relayPort);
         }
     }
+    
+    private static final String RESPONSE = 
+            "HTTP 200 OK\r\n" +
+            "Server: Gnutella\r\n"+
+            "Content-type: application/binary\r\n"+
+            "Content-Length: 0\r\n" +
+            "\r\n";
     
     private void startProxyServer(final int port) throws Exception {
         final Thread t = new Thread(new Runnable() {
 
             @Override
             public void run() {
-                final HttpProxyServer server = new DefaultHttpProxyServer(port);
+                final HttpProxyServer server = new DefaultHttpProxyServer(port, 
+                    new ProxyCacheManager() {
+                    
+                    @Override
+                    public boolean returnCacheHit(final HttpRequest request, 
+                            final Channel channel) {
+                        System.err.println("RETURNING CACHE HIT");
+                        channel.write(ChannelBuffers.wrappedBuffer(RESPONSE.getBytes()));
+                        ProxyUtils.closeOnFlush(channel);
+                        return true;
+                    }
+                    
+                    @Override
+                    public Future<String> cache(final HttpRequest originalRequest,
+                        final org.jboss.netty.handler.codec.http.HttpResponse httpResponse,
+                        final Object response, ChannelBuffer encoded) {
+                        return null;
+                    }
+                });
+                
                 System.out.println("About to start...");
                 server.start();
             }
@@ -62,6 +100,31 @@ public class UdtRelayTest {
         LanternUtils.waitForServer(port, 6000);
     }
 
+    private void hitRelayRaw(final int relayPort) throws Exception {
+        final Socket sock = new Socket();
+        final String request = 
+            "GET http://www.google.com HTTP/1.1\r\n"+
+            "Host: www.google.com\r\n"+
+            "User-Agent: Apache-HttpClient/4.2.2 (java 1.5)\r\n"+
+            "Connection: Keep-Alive\r\n\r\n";
+        sock.connect(new InetSocketAddress("127.0.0.1", relayPort));
+        
+        sock.getOutputStream().write(request.getBytes());
+        final BufferedReader br = 
+            new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        final StringBuilder sb = new StringBuilder();
+        String cur = br.readLine();
+        sb.append(cur);
+        while(StringUtils.isNotBlank(cur)) {
+            System.err.println(cur);
+            cur = br.readLine();
+            sb.append(cur);
+        }
+        assertTrue("Unexpected response "+sb.toString(), sb.toString().startsWith("HTTP 200 OK"));
+        System.out.println("");
+        sock.close();
+    }
+    
     private void hitRelay(final int relayPort) throws Exception {
         // We create new clients each time here to ensure that we're always
         // using a new client-side port.
@@ -73,7 +136,8 @@ public class UdtRelayTest {
         httpClient.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 50000);
         httpClient.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, 120000);
         
-        final HttpGet get = new HttpGet("http://www.google.com");
+        //final HttpGet get = new HttpGet("http://www.google.com");
+        final HttpGet get = new HttpGet("http://127.0.0.1");
         final HttpResponse response = httpClient.execute(get);
         final HttpEntity entity = response.getEntity();
         final String body = 
