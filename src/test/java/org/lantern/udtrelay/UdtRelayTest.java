92f9f9ad35ca7314fc64f79c7e03cc3740f23d66
Myles Horton <myles@getlantern.org>
2013-03-11 18:08:58 -0700

    Added netty3 to netty4 conversion #302

--- src/test/java/org/lantern/udtrelay/UdtRelayTest.java
+++ src/test/java/org/lantern/udtrelay/UdtRelayTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.BufUtil;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFuture;
@@ -11,9 +12,11 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundByteHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.udt.UdtChannel;
 import io.netty.channel.udt.nio.NioUdtProvider;
+import io.netty.handler.codec.http.DefaultHttpRequest;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -22,6 +25,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicReference;
@@ -38,8 +42,20 @@ import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
 import org.apache.http.params.CoreConnectionPNames;
 import org.apache.http.util.EntityUtils;
 import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.AbstractChannel;
+import org.jboss.netty.channel.AbstractChannelSink;
 import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelConfig;
+import org.jboss.netty.channel.ChannelEvent;
+import org.jboss.netty.channel.ChannelPipelineException;
+import org.jboss.netty.channel.ChannelSink;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.DefaultChannelConfig;
+import org.jboss.netty.handler.codec.http.HttpMethod;
 import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.jboss.netty.handler.codec.http.HttpRequestEncoder;
+import org.jboss.netty.handler.codec.http.HttpVersion;
 import org.junit.Test;
 import org.lantern.LanternClientConstants;
 import org.lantern.LanternConstants;
@@ -78,11 +94,21 @@ public class UdtRelayTest {
         // same thing (except a few specific HTTP headers) from the relay.
         //final String expected = hitProxyDirect(proxyPort);
         
+        
+
         // We do this a few times to make sure there are no issues with 
         // subsequent runs.
         for (int i = 0; i < 3; i++) {
+            final String uri = "http://lantern.s3.amazonaws.com/windows-x86-1.7.0_03.tar.gz";
+            final HttpRequest request = 
+                new org.jboss.netty.handler.codec.http.DefaultHttpRequest(
+                    HttpVersion.HTTP_1_1, HttpMethod.HEAD, uri);
+            
+            request.addHeader("Host", "lantern.s3.amazonaws.com");
+            request.addHeader("Proxy-Connection", "Keep-Alive");
             if (udt) {
-                hitRelayUdtNetty(relayPort, "");
+                //hitRelayUdtNetty(relayPort, "");
+                hitRelayUdtNetty(createDummyChannel(), request, relayPort);
             } else {
                 hitRelayRaw(relayPort);
             }
@@ -200,6 +226,7 @@ public class UdtRelayTest {
 
     }
     
+    /*
     private void hitRelayUdtNetty(final int relayPort, final String expected) 
         throws Exception {
         
@@ -211,6 +238,9 @@ public class UdtRelayTest {
         
         final AtomicReference<String> responseRef = new AtomicReference<String>("");
         
+        //final Channel browserToProxyChannel = new AbstractCh
+        final org.jboss.netty.channel.ChannelPipeline pipeline = Channels.pipeline();
+        final DummyChannel chan = createDummyChannel(pipeline); 
         try {
             boot.group(connectGroup)
                 .channelFactory(NioUdtProvider.BYTE_CONNECTOR)
@@ -218,24 +248,129 @@ public class UdtRelayTest {
                     @Override
                     public void initChannel(final UdtChannel ch)
                             throws Exception {
-                        ch.pipeline().addLast(
+                        final ChannelPipeline p = ch.pipeline();
+                        p.addLast(
                             //new LoggingHandler(LogLevel.INFO),
-                            new HttpResponseClientHandler(responseRef));
+                            new HttpResponseClientHandler(responseRef, chan));
                     }
                 });
             // Start the client.
             final ChannelFuture f = 
                 boot.connect(LanternClientConstants.LOCALHOST, relayPort).sync();
             
-            synchronized(responseRef) {
-                if (responseRef.get().length() == 0) {
-                    responseRef.wait(2000);
+            synchronized(chan) {
+                if (chan.message.length() == 0) {
+                    chan.wait(2000);
                 }
             }
-            // Wait until the connection is closed.
+            
+            //System.err.println(chan.message);
             assertTrue("Unexpected response "+responseRef.get(), 
-                    responseRef.get().startsWith("HTTP/1.1 200 OK"));
+                    chan.message.startsWith("HTTP/1.1 200 OK"));
+            
+            //assertTrue("Unexpected response "+responseRef.get(), 
+            //        responseRef.get().startsWith("HTTP/1.1 200 OK"));
             f.channel().close();
+        } finally {
+            // Shut down the event loop to terminate all threads.
+            boot.shutdown();
+        }
+    }
+    */
+    
+    public static class DummyChannel extends AbstractChannel {
+        
+        ChannelConfig config;
+        private String message = "";
+        
+        DummyChannel(final org.jboss.netty.channel.ChannelPipeline p, 
+            final ChannelSink sink) {
+            super(null, null, p, sink);
+            config = new DefaultChannelConfig();
+        }
+        
+        @Override public ChannelConfig getConfig() {return config;}
+        
+        @Override public SocketAddress getLocalAddress() { return new InetSocketAddress("127.0.0.1", 55512); }
+        @Override public SocketAddress getRemoteAddress() { return new InetSocketAddress("127.0.0.1", 55513); }
+        
+        @Override public boolean isBound() {return true;}
+        @Override public boolean isConnected() {return true;}
+        
+        @Override public org.jboss.netty.channel.ChannelFuture write(final Object message) {
+            final ChannelBuffer cb = (ChannelBuffer) message;
+            this.message = cb.toString(LanternConstants.UTF8);
+            final org.jboss.netty.channel.ChannelFuture cf = super.write(message);
+            //cf.setSuccess();
+            return cf;
+            //return Channels.write(this, message);
+            //return null;
+        }
+    }
+
+
+    public static DummyChannel createDummyChannel() {
+        final org.jboss.netty.channel.ChannelPipeline pipeline = Channels.pipeline();
+        final ChannelSink sink = new AbstractChannelSink() {
+            @Override public void eventSunk(org.jboss.netty.channel.ChannelPipeline p, ChannelEvent e) {}
+            @Override public void exceptionCaught(org.jboss.netty.channel.ChannelPipeline pipeline,
+                                     ChannelEvent e,
+                                     ChannelPipelineException cause)
+                                     throws Exception {
+                                         
+                Throwable t = cause.getCause();
+                if (t != null) {
+                    t.printStackTrace();
+                }
+                super.exceptionCaught(pipeline,e,cause);
+            }
+            
+        };
+
+        return new DummyChannel(pipeline, sink);
+    }
+    
+    private void hitRelayUdtNetty(
+        final DummyChannel browserToProxyChannel, final HttpRequest request,
+        final int relayPort) throws Exception {
+        browserToProxyChannel.setReadable(false);
+        
+        final Bootstrap boot = new Bootstrap();
+        final ThreadFactory connectFactory = Threads.newThreadFactory("connect");
+        final NioEventLoopGroup connectGroup = new NioEventLoopGroup(1,
+                connectFactory, NioUdtProvider.BYTE_PROVIDER);
+
+        try {
+            boot.group(connectGroup)
+                .channelFactory(NioUdtProvider.BYTE_CONNECTOR)
+                .handler(new ChannelInitializer<UdtChannel>() {
+                    @Override
+                    public void initChannel(final UdtChannel ch)
+                            throws Exception {
+                        final ChannelPipeline p = ch.pipeline();
+                        p.addLast(
+                            //new LoggingHandler(LogLevel.INFO),
+                            new HttpResponseClientHandler(
+                                browserToProxyChannel, request));
+                    }
+                });
+            // Start the client.
+            final ChannelFuture f = 
+                boot.connect(LanternClientConstants.LOCALHOST, relayPort).sync();
+            
+            
+            synchronized(browserToProxyChannel) {
+                if (browserToProxyChannel.message.length() == 0) {
+                    browserToProxyChannel.wait(2000);
+                }
+            }
+            
+            //System.err.println(chan.message);
+            assertTrue("Unexpected response: "+browserToProxyChannel.message, 
+                    browserToProxyChannel.message.startsWith("HTTP/1.1 200 OK"));
+            // Wait until the connection is closed.
+            f.channel().close();
+            
         } finally {
             // Shut down the event loop to terminate all threads.
             boot.shutdown();
@@ -247,18 +382,34 @@ public class UdtRelayTest {
         private static final Logger log = 
                 LoggerFactory.getLogger(HttpResponseClientHandler.class);
 
-        private final ByteBuf message = Unpooled.wrappedBuffer(REQUEST.getBytes());
+        //private final ByteBuf message = Unpooled.wrappedBuffer(REQUEST.getBytes());
 
-        private final AtomicReference<String> responseRef;
+        private final Channel browserToProxyChannel;
 
-        private HttpResponseClientHandler(final AtomicReference<String> response) {
-            responseRef = response;
+        private HttpRequest httpRequest;
+        
+        private static final class HttpRequestConverter extends HttpRequestEncoder {
+            private Channel basicChannel = new ChannelAdapter();
+
+            public ByteBuf encode(final HttpRequest request) throws Exception {
+                final ChannelBuffer cb = (ChannelBuffer) super.encode(null, basicChannel, request);
+                return Unpooled.wrappedBuffer(cb.toByteBuffer());
+            }
+        };
+        
+        private static final HttpRequestConverter encoder = new HttpRequestConverter();
+
+        private HttpResponseClientHandler(
+            final Channel browserToProxyChannel, final HttpRequest request) {
+            this.browserToProxyChannel = browserToProxyChannel;
+            this.httpRequest = request;
         }
 
         @Override
         public void channelActive(final ChannelHandlerContext ctx) throws Exception {
             log.info("Channel active " + NioUdtProvider.socketUDT(ctx.channel()).toStringOptions());
-            ctx.write(message);
+            
+            ctx.write(encoder.encode(httpRequest));
         }
 
         @Override
@@ -266,9 +417,12 @@ public class UdtRelayTest {
                 final ByteBuf in) {
             final String response = in.toString(LanternConstants.UTF8);
             log.info("INBOUND UPDATED!!\n"+response);
-            synchronized (responseRef) {
-                responseRef.set(response);
-                responseRef.notifyAll();
+            
+            
+            synchronized (browserToProxyChannel) {
+                final ChannelBuffer wrapped = ChannelBuffers.wrappedBuffer(response.getBytes());
+                this.browserToProxyChannel.write(wrapped);
+                this.browserToProxyChannel.notifyAll();
             }
         }
 
