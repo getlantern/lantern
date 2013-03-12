aa9e106591d39f4cee3ee0a336dd24c9d9d760c6
Myles Horton <myles@getlantern.org>
2013-03-12 10:50:54 -0700

    Linking incoming FiveTuples with UDT relay server #302

--- src/main/java/org/lantern/udtrelay/UdtRelayServerFiveTupleListener.java
+++ src/main/java/org/lantern/udtrelay/UdtRelayServerFiveTupleListener.java
@@ -1,12 +1,19 @@
 package org.lantern.udtrelay;
 
+import java.net.InetSocketAddress;
+
+import org.lantern.LanternUtils;
 import org.lastbamboo.common.offer.answer.OfferAnswer;
 import org.lastbamboo.common.offer.answer.OfferAnswerListener;
 import org.littleshoot.util.FiveTuple;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UdtRelayServerFiveTupleListener 
     implements OfferAnswerListener<FiveTuple>{
-
+    
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    
     @Override
     public void onOfferAnswerFailed(final OfferAnswer offerAnswer) {
         
@@ -19,7 +26,15 @@ public class UdtRelayServerFiveTupleListener
 
     @Override
     public void onUdpSocket(final FiveTuple sock) {
+        final InetSocketAddress local = sock.getLocal();
+        final UdtRelayProxy proxy = 
+            new UdtRelayProxy(local, 
+                LanternUtils.PLAINTEXT_LOCALHOST_PROXY_PORT);
         
+        try {
+            proxy.run();
+        } catch (final Exception e) {
+            log.warn("Exception running UDT proxy?", e);
+        }
     }
-
 }
