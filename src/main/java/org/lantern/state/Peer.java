e42a2bb4df6d0e14b4d93e1434fa3f0ef6a37f4a
Leah X Schmidt <leahxschmidt@gmail.com>
2013-03-18 15:03:26 -0400

    use peer lastconnected

--- src/main/java/org/lantern/state/Peer.java
+++ src/main/java/org/lantern/state/Peer.java
@@ -9,6 +9,7 @@ import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
 import org.codehaus.jackson.map.annotate.JsonView;
 import org.lantern.LanternClientConstants;
 import org.lantern.LanternRosterEntry;
+import org.lantern.event.Events;
 import org.lantern.state.Model.Persistent;
 import org.lantern.state.Model.Run;
 import org.lantern.util.LanternTrafficCounter;
@@ -265,6 +266,7 @@ public class Peer {
     
     public void setLastConnectedLong(final long lastConnectedLong) {
         this.lastConnectedLong = lastConnectedLong;
+        Events.eventBus().post(new PeerLastConnectedChangedEvent(this));
     }
 
     public String getPeerid() {
