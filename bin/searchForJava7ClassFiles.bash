dbf34119768b7b291be432d684cd20f71db56991
Myles Horton <myles@getlantern.org>
2013-03-20 15:31:31 -0700

    Lots of fixes for flagging java 7 files at build time and for configuring linux libraries for installer/keyring  #357

--- bin/searchForJava7ClassFiles.bash
+++ bin/searchForJava7ClassFiles.bash
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-function die()
-{
+function die() {
   echo $*
   exit 1
 }
 
-JARS=`find ../lib/repository -name "*.jar"`
+if [ $# -eq "1" ] 
+then
+    JARS=( $1 )
+    #die "$0: Received $# args... version, whether or not this is a release, architecture, and build ID required"
+else 
+    JARS=`find ../lib/repository -name "*.jar"`
+fi
+
+#JARS=`find ../lib/repository -name "*.jar"`
 for x in $JARS
 do
 #   echo "Searching for java 7 class files in jar $x"
@@ -17,7 +24,7 @@ do
    #echo "jarName $jarName"
    jar xf $jarName
 
-   find . -name "*.class" | xargs file | grep 51 && echo "FOUND JAVA 7 CLASS FILE IN $x"
+   find . -name "*.class" | xargs file | grep 51 && echo "FOUND JAVA 7 CLASS FILE IN $x" && exit 1
    cd ..
    rm -rf temp
 done
