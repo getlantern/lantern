e54157371e4eaf1c17b1a8ab54249d39b91ad3cc
Leah X Schmidt <leahxschmidt@gmail.com>
2013-03-22 20:56:45 -0400

    global bps/bytesEver

--- src/main/java/org/lantern/state/Global.java
+++ src/main/java/org/lantern/state/Global.java
@@ -1,16 +1,35 @@
 package org.lantern.state;
 
-
 public class Global {
 
     private final NUsers nusers = new NUsers();
     private final NPeers npeers = new NPeers();
 
+    private int bytesEver;
+    private int bps;
+
     public NPeers getNpeers() {
         return npeers;
     }
+
     public NUsers getNusers() {
         return nusers;
+    }
+
+    public int getBytesEver() {
+        return bytesEver;
+    }
+
+    public void setBytesEver(int bytesEver) {
+        this.bytesEver = bytesEver;
+    }
+
+    public int getBps() {
+        return bps;
+    }
+
+    public void setBps(int bps) {
+        this.bps = bps;
     }
 
 }
