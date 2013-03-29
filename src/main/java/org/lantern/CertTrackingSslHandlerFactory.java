746d33efe615a8e091ab495193fb34b6153648c5
Myles Horton <myles@getlantern.org>
2013-03-29 08:41:08 -0700

    Wiring

--- src/main/java/org/lantern/CertTrackingSslHandlerFactory.java
+++ src/main/java/org/lantern/CertTrackingSslHandlerFactory.java
@@ -9,19 +9,32 @@ import org.littleshoot.proxy.KeyStoreManager;
 import org.littleshoot.proxy.SslContextFactory;
 import org.littleshoot.proxy.SslHandshakeHandler;
 
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+@Singleton
 public class CertTrackingSslHandlerFactory implements HandshakeHandlerFactory {
 
     private final KeyStoreManager ksm;
     
+    @Inject
     public CertTrackingSslHandlerFactory(final KeyStoreManager ksm) {
         this.ksm = ksm;
     }
-        
+
+    /**
+     * This method is called for every new pipeline that's created -- i.e.
+     * for every new incoming connection to the server. We need to reload
+     * the trust store each to make sure we take into account all the last
+     * certificates from peers.
+     */
     @Override
     public HandshakeHandler newHandshakeHandler() {
         final SslContextFactory scf = new SslContextFactory(ksm);
         final SSLEngine engine = scf.getServerContext().createSSLEngine();
         engine.setUseClientMode(false);
-        return new SslHandshakeHandler("ssl", new SslHandler(engine));
+        engine.setNeedClientAuth(true);
+        final SslHandler handler = new SslHandler(engine);
+        return new SslHandshakeHandler("ssl", handler);
     }
 }
