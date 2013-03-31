822a5a3be5e00b4b89916fc48b39b6de7ee70548
Myles Horton <myles@getlantern.org>
2013-03-30 23:14:53 -0700

    Cleanups

--- osxInstall.bash
+++ osxInstall.bash
@@ -10,20 +10,15 @@ then
     die "$0: Received $# args... version and release (true or false) required"
 fi
 
+VERSION=$1
 RELEASE=$2;
 
-VERSION=$1
-#INSTALL4J_MAC_PASS=$2
-
+echo "RELEASE flag is $RELEASE"
 ./installerBuild.bash $VERSION "" $RELEASE || die "Could not build!!"
-
-#install4jc -L $INSTALL4J_KEY || die "Could not update license information?"
 
 install4jc -v --mac-keystore-password=$INSTALL4J_MAC_PASS -m macos -r $VERSION ./install/lantern.install4j || die "Could not build installer?"
 
-#/Applications/install4j\ 5/bin/install4jc -m macos -r $VERSION ./install/lantern.install4j
-
 name=lantern-$VERSION.dmg
-mv install/Lantern.dmg $name
+mv install/Lantern.dmg $name || die "Could not move new installer -- failed to create somehow?"
 ./installMetaRefresh.bash osx $name latest.dmg $RELEASE || die "ERROR: Could not build meta-refresh redirect file"
 
--- winInstall.bash
+++ winInstall.bash
@@ -14,19 +14,10 @@ VERSION=$1
 RELEASE=$2
 
 echo "RELEASE flag is $RELEASE"
-
-#INSTALL4J_PASS=$2
 ./installerBuild.bash $VERSION "-Dsun.arch.data.model=32 -Pwindows" $RELEASE || die "Could not build?"
 
-install4jc --win-keystore-password=$INSTALL4J_WIN_PASS -m windows -r $VERSION ./install/lantern.install4j || die "Could not build installer"
-#install4jc -L $INSTALL4J_KEY --win-keystore-password=$INSTALL4J_WIN_PASS -m windows -r $VERSION ./install/lantern.install4j || die "Could not build installer"
-#/Applications/install4j\ 5/bin/install4jc -m windows -r $VERSION ./install/lantern.install4j || die "Could not build installer"
+install4jc -v --win-keystore-password=$INSTALL4J_WIN_PASS -m windows -r $VERSION ./install/lantern.install4j || die "Could not build installer"
 
 name=lantern-$VERSION.exe
 mv install/Lantern.exe $name || die "Could not move new installer -- failed to create somehow?"
-
-./installMetaRefresh.bash win $name latest.exe $RELEASE
-
-#cp $name ~/Desktop/virtual-machine-files/
-
-
+./installMetaRefresh.bash win $name latest.exe $RELEASE || die "ERROR: Could not build meta-refresh redirect file"
