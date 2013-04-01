5fef77837b0557c26641277f54d4dd072f6aec01
Myles Horton <myles@getlantern.org>
2013-04-01 15:24:42 -0700

    Removed dead code

--- src/main/java/org/lantern/DefaultPeerFactory.java
+++ src/main/java/org/lantern/DefaultPeerFactory.java
@@ -142,39 +142,6 @@ public class DefaultPeerFactory implements PeerFactory {
         this.exec.submit(runner);
     }
 
-
-    /**
-     * Adds an incoming peer. Note that this method purely uses the address
-     * of the incoming peer and not the JID. For the case of port-mapped peers,
-     * this will be accurate because the remote address is in fact the address
-     * of the peer. For p2p connections, however, there's an intermediary
-     * step where we typically copy data from a temporary local server to the
-     * local HTTP server, for the purposes of making ICE work more simply
-     * (i.e. that way the HTTP server doesn't have to worry about ICE but
-     * rather just about servicing incoming sockets). The problem is that if
-     * this method is used to add those peers, their IP address will always
-     * be the IP address of localhost, so they will not be mapped correctly.
-     * Their data will be tracked correctly, however.
-     *
-     * See:
-     *
-     * https://github.com/adamfisk/littleshoot-util/blob/master/src/main/java/org/littleshoot/util/RelayingSocketHandler.java
-     *
-     * @param address The address of the peer.
-     * @param trafficCounter The counter for keeping track of traffic to and
-     * from the peer.
-     */
-    /*
-    @Override
-    public void onIncomingConnection(final URI fullJid, final InetSocketAddress isa,
-        final LanternTrafficCounter trafficCounter) {
-        // There should always be an existing peer at this point,
-        // and we should add data to that peer. Incoming peers should always
-        // be of type pc.
-        updatePeer(fullJid, isa, Type.pc, trafficCounter);
-    }
-    */
-
     private void updatePeer(final URI fullJid, final InetSocketAddress isa,
             final Type type, final LanternTrafficCounter trafficCounter) {
         final Peer peer = peers.getPeer(fullJid);
