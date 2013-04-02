f29079de4a61f80ac88817721eea7b2962b2e6c6
Myles Horton <myles@getlantern.org>
2013-04-02 15:50:20 -0700

    Making test more cross-platform

--- src/test/java/org/lantern/LanternModuleTest.java
+++ src/test/java/org/lantern/LanternModuleTest.java
@@ -4,7 +4,13 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.lantern.http.JettyLauncher;
+import org.lantern.privacy.DefaultLocalCipherProvider;
+import org.lantern.privacy.UnencryptedFileService;
 import org.lantern.state.Model;
+import org.lastbamboo.common.portmapping.NatPmpService;
+import org.lastbamboo.common.portmapping.PortMapListener;
+import org.lastbamboo.common.portmapping.PortMappingProtocol;
+import org.lastbamboo.common.portmapping.UpnpService;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
@@ -13,7 +19,37 @@ public class LanternModuleTest {
 
     @Test
     public void test() {
-        final Injector injector = Guice.createInjector(new LanternModule());
+        final LanternModule lm = new LanternModule();
+        lm.setLocalCipherProvider(new DefaultLocalCipherProvider());
+        lm.setEncryptedFileService(new UnencryptedFileService());
+        lm.setUpnpService(new UpnpService() {
+            @Override
+            public void shutdown() {}
+            
+            @Override
+            public void removeUpnpMapping(int mappingIndex) {}
+            
+            @Override
+            public int addUpnpMapping(PortMappingProtocol protocol, int localPort,
+                    int externalPortRequested, PortMapListener portMapListener) {
+                return 0;
+            }
+        });
+        lm.setNatPmpService(new NatPmpService() {
+            
+            @Override
+            public void shutdown() {}
+            
+            @Override
+            public void removeNatPmpMapping(int mappingIndex) {}
+            
+            @Override
+            public int addNatPmpMapping(PortMappingProtocol protocol, int localPort,
+                    int externalPortRequested, PortMapListener portMapListener) {
+                return 0;
+            }
+        });
+        final Injector injector = Guice.createInjector(lm);
         
         final LanternService xmpp = 
             injector.getInstance(DefaultXmppHandler.class);
