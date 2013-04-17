2db2a016869d6fe56d17894e2617130f407db636
Leah X Schmidt <leahxschmidt@gmail.com>
2013-04-17 12:45:23 -0400

    minor test

--- src/test/java/org/lantern/VersionTest.java
+++ src/test/java/org/lantern/VersionTest.java
@@ -1,6 +1,7 @@
 package org.lantern;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
@@ -15,5 +16,9 @@ public class VersionTest {
         Version version = new Version();
         Installed installed = version.getInstalled();
         assertFalse(StringUtils.isBlank(installed.getGit()));
+
+        // we will never be running the tests from an installer-generated
+        // version
+        assertTrue(LanternClientConstants.isDevMode());
     }
 }
