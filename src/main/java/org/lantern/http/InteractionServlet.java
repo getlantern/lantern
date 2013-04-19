dd551b46eb705806d3bf81908735f97725c5e0ea
Leah X Schmidt <leahxschmidt@gmail.com>
2013-04-18 21:31:33 -0400

    move geoip checker out to its own class

--- src/main/java/org/lantern/http/InteractionServlet.java
+++ src/main/java/org/lantern/http/InteractionServlet.java
@@ -20,7 +20,6 @@ import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.lantern.Censored;
 import org.lantern.ConnectivityChangedEvent;
-import org.lantern.GeoData;
 import org.lantern.JsonUtils;
 import org.lantern.LanternClientConstants;
 import org.lantern.LanternFeedback;
@@ -32,7 +31,6 @@ import org.lantern.state.Connectivity;
 import org.lantern.state.InternalState;
 import org.lantern.state.InviteQueue;
 import org.lantern.state.JsonModelModifier;
-import org.lantern.state.Location;
 import org.lantern.state.LocationChangedEvent;
 import org.lantern.state.Modal;
 import org.lantern.state.Mode;
@@ -797,17 +795,6 @@ public class InteractionServlet extends HttpServlet {
         }
         if (ipChanged) {
             InetAddress ip = e.getNewIp();
-            String newIpString = ip.getHostAddress();
-            final Location loc = model.getLocation();
-            if (loc.getLat() == 0.0 && loc.getLon() == 0.0) {
-                final GeoData geo = modelUtils.getGeoData(newIpString);
-                if (geo.getLatitude() != 0.0 || geo.getLongitude() != 0.0) {
-                    loc.setCountry(geo.getCountrycode());
-                    loc.setLat(geo.getLatitude());
-                    loc.setLon(geo.getLongitude());
-                    Events.sync(SyncPath.LOCATION, loc);
-                }
-            }
             connectivity.setIp(ip.toString());
         }
         connectivity.setInternet(true);
