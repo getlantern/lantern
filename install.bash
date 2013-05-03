6e585272d5e0dea3a43350edc1d578395bbe9453
Myles Horton <myles@getlantern.org>
2013-05-03 15:29:48 -0700

    added permissions check closes #742

--- install.bash
+++ install.bash
@@ -6,4 +6,9 @@ function die() {
 
 mvn --version || die "Please install maven from http://maven.apache.org" 
 
+if [ $(uname) == "Darwin" ]
+then
+  ls -la configureNetworkServices | grep rwsr | grep wheel || ./setNetUidOsx.bash
+fi
+
 mvn package -Dmaven.artifact.threads=1 -Dmaven.test.skip=true || die "Could not package"
