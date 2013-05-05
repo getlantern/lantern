0837a9af24e4d14d59b0fe6456f3cc55ff1a9b1b
Myles Horton <myles@getlantern.org>
2013-05-04 22:59:56 -0700

    cleanup

--- bin/dep.bash
+++ bin/dep.bash
@@ -10,10 +10,5 @@ then
     die "$0: Received $# args... group, artifact, version, file, and sources required"
 fi
 
-#group=$1
-#artifact=$2
-#version=$3
-#file=$4
-#sources=$5
 mvn deploy:deploy-file -DgeneratePom=true -DrepositoryId=aws-release -Durl=s3://lantern-mvn-repo/release -Dpackaging=jar -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -Dfile=$4 -Dsources=$5
 #mvn deploy:deploy-file -DgeneratePom=true -DrepositoryId=aws-release -Durl=s3://lantern-mvn-repo/release -Dpackaging=jar -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -Dfile=$4 
