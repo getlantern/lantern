b4d3dd10452f73ab992ce36e21c3f4f75c657539
Myles Horton <myles@getlantern.org>
2013-05-14 00:11:34 -0700

    fully automated release script #712

--- bin/mvnrelease.bash
+++ bin/mvnrelease.bash
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
 function die() {
-  echo $*
+  echo "$@"
   exit 1
 }
 
+if [ $# -lt "3" ]
+then
+    die "$0: Received $# args... gpg password, git user name, and git password required"
+fi
+
+gpgpass=$1
+gituser=$2
+gitpass=$3
 mvn release:clean || die "Could not clean?"
-mvn release:prepare || die "Could not prepare?"
-mvn release:perform || die "Could not perform?"
+
+releasePrepare.exp $gpgpass $gituser $gitpass || die "Could not prepare release"
+releasePerform.exp $gpgpass || die "Could not perform release"
+#mvn release:prepare || die "Could not prepare?"
+#mvn release:perform || die "Could not perform?"
 
 pushd target/checkout/ || die "Could not cd?"
 mvn nexus-staging:release || die "Could not release?"
