81cf15a739f4582adeb53d58bbd670326e42ee08
_pants <_pants@getlantern.org>
2013-05-17 13:22:17 -0400

    `ln osascript Lantern` trick so password prompt gets title "Lantern" #762

--- install/osx/configureOsx.bash
+++ install/osx/configureOsx.bash
@@ -97,7 +97,8 @@ log "Loading launchd plist file"
 logFile $LAUNCHD_PLIST
 
 log "Changing owner on configureNetworkServices"
-osascript -e "do shell script \"chown root:admin $APP_PATH/Contents/Resources/app/configureNetworkServices\" with administrator privileges" || die "Could not change owner"
+ln /usr/bin/osascript ./Lantern
+./Lantern -e "do shell script \"chown root:admin $APP_PATH/Contents/Resources/app/configureNetworkServices\" with administrator privileges" || die "Could not change owner"
 
 log "Finished configuring Lantern!"
 exit 0
