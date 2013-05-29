4a2c0daa496b3e05f783dc3fcb47769a20e118d3
Myles Horton <myles@getlantern.org>
2013-05-29 00:13:58 -0700

    further fleshed out

--- release.bash
+++ release.bash
@@ -14,9 +14,15 @@ grep "<version>$1-SNAPSHOT</version>" pom.xml || die "$1 not found in pom.xml"
 
 mvn release:prepare || die "Could not prepare release?"
 
+echo "Creating branch $1"
+git checkout -b $1 lantern-$1 || die "Could not create a branch"
+git push origin $1 || die "Could not push new branch"
+
 # We don't care about actually releasing -- just running the tests, tagging, 
 # incrementing the version -- so cleanup
 mvn release:clean
 
 # Update to the latest commited code
 git pull || die "Could not pull?"
+
+echo "Finished. You can release from the branch $1"
