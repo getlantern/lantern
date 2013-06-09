41cfc69c3e07b565c7dafdad724677928c7c0b15
Myles Horton <myles@getlantern.org>
2013-06-09 00:56:25 -0700

    fixed path

--- buildInstallerWrappers.bash
+++ buildInstallerWrappers.bash
@@ -6,6 +6,6 @@ function die() {
 }
 
 install4jc -L $INSTALL4J_KEY || die "Could not update license information?"
-install4jc -v --win-keystore-password=$INSTALL4J_WIN_PASS --mac-keystore-password=$INSTALL4J_MAC_PASS ./install/wrapper.install4j || die "Could not build installer?"
+install4jc -v --win-keystore-password=$INSTALL4J_WIN_PASS --mac-keystore-password=$INSTALL4J_MAC_PASS ./install/wrapper/wrapper.install4j || die "Could not build installer?"
 
 
