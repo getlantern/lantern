7834d0e609f8e41f3c723f47abf3488d24064db6
Myles Horton <myles@getlantern.org>
2013-06-08 21:51:27 -0700

    added license update

--- buildInstallerWrappers.bash
+++ buildInstallerWrappers.bash
@@ -10,7 +10,7 @@ then
     die "$0: Received $# args... version required (without full git revision)"
 fi
 
-
+install4jc -L $INSTALL4J_KEY || die "Could not update license information?"
 install4jc -v --win-keystore-password=$INSTALL4J_WIN_PASS --mac-keystore-password=$INSTALL4J_MAC_PASS ./install/wrapper.install4j || die "Could not build installer?"
 
 
