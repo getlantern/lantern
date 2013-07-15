00ea22168bbfec8b47c91422368d1d18a0265325
Myles Horton <myles@getlantern.org>
2013-07-14 22:18:17 -0700

    tweaks

--- buildDiagnosticTool.bash
+++ buildDiagnosticTool.bash
@@ -4,8 +4,19 @@ function die() {
   echo $*
   exit 1
 }
-perl -pi -e "s/org.lantern.Launcher/org.lantern.Diagnostics" || die "Could not swap main class?"
+echo "Swapping main class"
+perl -pi -e "s/org.lantern.Launcher/org.lantern.Diagnostics/g" pom.xml || die "Could not swap main class?"
+
+echo "Copying cacerts"
 cp src/test/resources/cacerts install/common/ || die "Could not copy cacerts?"
+
+echo "Copying test.properties"
 cp src/test/resources/test.properties install/common/ || die "Could not copy test.properties"
 
+echo "Running installer build process..."
 ./winInstall.bash HEAD false || die "Could not build diagnostic tool?"
+
+echo "Cleaning up..."
+rm install/common/cacerts
+rm install/common/test.properties
+git checkout pom.xml
