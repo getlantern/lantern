1fb6ffaf6a23f8d59a1c09a592dee99123b4d626
Myles Horton <myles@getlantern.org>
2013-08-28 17:22:02 -0700

    added initial pass at uploading pack200 compressed jar for updates

--- installMetaRefresh.bash
+++ installMetaRefresh.bash
@@ -46,10 +46,15 @@ if $release ; then
   echo "Uploading SHA-1 `cat $latestName.sha1`"
   aws -putp $bucket $latestName.sha1
 
-  cp install/common/lantern.jar $latestName.jar || die "Could not copy latest jar?"
+#  md5 -q $name > $latestName.md5
+#  echo "Uploading MD5 `cat $latestName.md5`"
+#  aws -putp $bucket $latestName.md5
 
-  echo "Uploading latest jar: $latestName.jar"
-  aws -putp $bucket $latestName.jar
+  #cp install/common/lantern.jar $latestName.jar || die "Could not copy latest jar?"
+  #pack200 $latestName.pack.gz $latestName.jar || die "Could not pack jar?"
+
+  #echo "Uploading latest jar: $latestName.pack.gz"
+  #aws -putp $bucket $latestName.pack.gz
 else
   echo "NOT RELEASING!!!"
 fi
