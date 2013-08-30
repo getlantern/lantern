8607a342f47a3749c0383ae2fe5e3b3c622c0591
Myles Horton <myles@getlantern.org>
2013-08-29 17:48:13 -0700

    added check for java 7 compilation on installer verification class

--- buildInstallerWrappers.bash
+++ buildInstallerWrappers.bash
@@ -5,6 +5,9 @@ function die() {
   exit 1
 }
 
+test -f ./install/wrapper/InstallDownloader.class || die "Could not find InstallerDownloader class file?"
+file ./install/wrapper/InstallDownloader.class | grep 51 && die "InstallerDownloader.class was compiled with java7"
+
 install4jc -L $INSTALL4J_KEY || die "Could not update license information?"
 install4jc -v --win-keystore-password=$INSTALL4J_WIN_PASS --mac-keystore-password=$INSTALL4J_MAC_PASS ./install/wrapper/wrapper.install4j || die "Could not build installer?"
 
