66c8a9ea817c2f73b3ff5784b39d89f451ef54e3
Myles Horton <myles@getlantern.org>
2013-10-20 07:50:14 -0700

    Clear network tracking on reset

--- src/main/java/org/lantern/network/NetworkTracker.java
+++ src/main/java/org/lantern/network/NetworkTracker.java
@@ -9,9 +9,11 @@ import java.util.Map;
 import java.util.Set;
 
 import org.jboss.netty.util.internal.ConcurrentHashMap;
+import org.lantern.event.ResetEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.eventbus.Subscribe;
 import com.google.inject.Singleton;
 
 /**
@@ -206,5 +208,13 @@ public class NetworkTracker<U, I, D> {
         this.trustedOnlineInstances = currentTrustedOnlineInstances;
         LOG.debug("Number of trusted online instances: {}",
                 trustedOnlineInstances.size());
+    }
+    
+    @Subscribe
+    public void onReset(final ResetEvent event) {
+        this.onlineInstancesByAdvertisingUser.clear();
+        this.trustedCertificatesByInstance.clear();
+        this.trustedOnlineInstances.clear();
+        this.trustedUsers.clear();
     }
 }
