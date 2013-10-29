df78d5119a1bfd8e18cd6945e794bac5f1136267
Ox To A Cart <ox.to.a.cart@gmail.com>
2013-10-29 08:06:33 -0500

    #1073 Added comment to Cocoa code

--- LanternBrowser/LanternBrowser/AppDelegate.m
+++ LanternBrowser/LanternBrowser/AppDelegate.m
@@ -27,6 +27,10 @@
     return YES;
 }
 
+//
+// Implementing this (and registering it with [self.webView setPolicyDelegate:self]) allows us to find out about people clicking on links with
+// target = "_blank" and then open these.  We open them in Safari and always force a new instance (window) as opposed to a new tab.
+// Note - for whatever reason, when using window.open(), the request comes in as null, so only target = "_blank" actually works.
 - (void)webView:(WebView *)webView decidePolicyForNewWindowAction:(NSDictionary *)actionInformation request:(NSURLRequest *)request newFrameName:(NSString *)frameName decisionListener:(id < WebPolicyDecisionListener >)listener {
     NSURL* url = request.URL;
     NSLog(@"Got request to open url: %@", [url description]);
