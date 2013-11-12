5796ee77faaebbcb051768632238d7367fd4d812
Ox To A Cart <ox.to.a.cart@gmail.com>
2013-11-11 19:46:48 -0600

    #1129 Added osyan.net to whitelist

--- src/main/java/org/lantern/Whitelist.java
+++ src/main/java/org/lantern/Whitelist.java
@@ -102,6 +102,7 @@ public class Whitelist {
         "linkadeh.com",
         "manoto1.com",
         "mardomak.org",
+        "osyan.net",
         "radiofarda.com",
         "radiozamaneh.com",
         "roozonline.com",
