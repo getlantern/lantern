b74b40b1d11902d10b8aab203660c738e0090a36
Ox To A Cart <ox.to.a.cart@gmail.com>
2013-11-15 14:01:24 -0600

    #1151 Launching Lantern in system default browser

--- src/main/java/org/lantern/BrowserService.java
+++ src/main/java/org/lantern/BrowserService.java
@@ -1,15 +1,19 @@
 package org.lantern;
 
-public interface BrowserService extends LanternService {
+import java.net.URI;
+
+public interface BrowserService {
 
     void openBrowser();
 
     void openBrowser(int port, final String prefix);
 
+    void openBrowser(URI uri);
+
     void openBrowserWhenPortReady(int port, final String prefix);
 
     void openBrowserWhenPortReady();
-    
+
     void reopenBrowser();
 
 }
