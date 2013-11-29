4d35c4c68496e0bca0d34bf67cfb11c35c42b744
Adam Fisk <afisk@Leahs-MacBook-Air.local>
2013-11-28 23:10:27 -0800

    charset fix from @cnzzr closes #1183

--- src/main/java/org/lantern/oauth/OauthUtils.java
+++ src/main/java/org/lantern/oauth/OauthUtils.java
@@ -265,7 +265,7 @@ public class OauthUtils {
                 return "";
             }
             final HttpEntity entity = response.getEntity();
-            final String body = IOUtils.toString(entity.getContent());
+            final String body = IOUtils.toString(entity.getContent(), "UTF-8");
             EntityUtils.consume(entity);
             
             if (code < 200 || code > 299) {
