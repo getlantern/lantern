45d285abdcb12fac3f794c01d50b1c5d965b87c9
_pants <_pants@getlantern.org>
2014-01-13 17:15:19 -0500

    move system country and timezone into model.system

--- src/main/java/org/lantern/LanternFeedback.java
+++ src/main/java/org/lantern/LanternFeedback.java
@@ -1,24 +1,21 @@
 package org.lantern;
 
-import java.io.IOException;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.apache.commons.io.FileSystemUtils;
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.SystemUtils;
 import org.lantern.loggly.Loggly;
 import org.lantern.loggly.LogglyMessage;
 import org.lantern.state.Mode;
 import org.lantern.state.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 @Singleton
 public class LanternFeedback {
-    
+    private static final Logger LOG = LoggerFactory.getLogger(JsonUtils.class);
     private Model model;
     private Loggly loggly;
 
@@ -42,37 +39,5 @@ public class LanternFeedback {
                 new Date())
                 .setExtraFromJson(json);
         loggly.log(msg);
-        /*
-        final Map <String, String> feedback = new HashMap<String, String>(); 
-        feedback.putAll(systemInfo());
-        feedback.put("message", message);
-        feedback.put("replyto", replyTo == null ? "" : replyTo);
-        */
     }
-
-    // TODO merge this with model.system
-    protected Map<String, String> systemInfo() {
-        final Map<String, String> info = new HashMap<String,String>();
-
-        info.put("lanternVersion", LanternClientConstants.VERSION);        
-        info.put("javaVersion", SystemUtils.JAVA_VERSION);
-        info.put("osName", SystemUtils.OS_NAME);
-        info.put("osArch", SystemUtils.OS_ARCH);
-        info.put("osVersion", SystemUtils.OS_VERSION);
-        info.put("language", SystemUtils.USER_LANGUAGE);
-        info.put("country", SystemUtils.USER_COUNTRY);
-        info.put("timeZone", SystemUtils.USER_TIMEZONE);
-        
-        final String osRoot = SystemUtils.IS_OS_WINDOWS ? "c:" : "/";
-        long free = Long.MAX_VALUE;
-        try {
-            free = FileSystemUtils.freeSpaceKb(osRoot);
-            // Convert to megabytes for easy reading.
-            free = free / 1024L;
-        } catch (final IOException e) {
-        }
-        info.put("diskSpace", String.valueOf(free));
-        return info;
-    }
-    
 }
