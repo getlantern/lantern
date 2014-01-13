00edba736d1d5cf0c8e777f792f75365f5152dcc
Ox To A Cart <ox.to.a.cart@gmail.com>
2014-01-13 11:01:08 -0600

    #1281 Constraining random characters to within A-Z

--- src/main/java/org/lantern/util/RandomLengthString.java
+++ src/main/java/org/lantern/util/RandomLengthString.java
@@ -6,6 +6,8 @@ import java.security.SecureRandom;
  * Generates a random length string.
  */
 public class RandomLengthString {
+    private static final int LETTER_A = 65;
+    private static final int LETTER_Z = 90;
     private final int maxLength;
     private final String[] preGeneratedStrings;
     private final ThreadLocal<SecureRandom> random = new ThreadLocal<SecureRandom>() {
@@ -22,7 +24,10 @@ public class RandomLengthString {
         for (int i = 0; i < maxLength; i++) {
             char[] chars = new char[i];
             for (int j = 0; j < i; j++) {
-                chars[j] = (char) random.get().nextInt(255);
+                // Pick a random character from A-Z
+                chars[j] =
+                        (char)
+                        (LETTER_A + random.get().nextInt(LETTER_Z + 1 - LETTER_A));
             }
             preGeneratedStrings[i] = new String(chars);
         }
