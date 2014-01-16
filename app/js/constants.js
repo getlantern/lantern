54c1716d3cae2d00cc8f176e886515e2ad409895
_pants <_pants@getlantern.org>
2014-01-16 13:31:21 -0500

    allow domains > 4 chars in email addresses

--- app/js/constants.js
+++ app/js/constants.js
@@ -44,8 +44,8 @@ var DEFAULT_LANG = 'en_US',
     CONTACT_FORM_MAXLEN = 500000,
     INPUT_PAT = {
       // based on http://www.regular-expressions.info/email.html
-      EMAIL: /^[a-zA-Z0-9._%+-]+@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,4}$/,
-      EMAIL_INSIDE: /[a-zA-Z0-9._%+-]+@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,4}/,
+      EMAIL: /^[a-zA-Z0-9._%+-]+@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/,
+      EMAIL_INSIDE: /[a-zA-Z0-9._%+-]+@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}/,
       // from http://html5pattern.com/
       DOMAIN: /^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$/,
       IPV4: /((^|\.)((25[0-5])|(2[0-4]\d)|(1\d\d)|([1-9]?\d))){4}$/
