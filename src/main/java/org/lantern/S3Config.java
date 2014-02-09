f8d77cae4371f16ea7e21a168729589556177fef
Myles Horton <myles@getlantern.org>
2014-02-08 21:58:43 -0800

    Adding default poll times #1372

--- src/main/java/org/lantern/S3Config.java
+++ src/main/java/org/lantern/S3Config.java
@@ -14,8 +14,8 @@ public class S3Config {
     public static final String DEFAULT_CONTROLLER_ID = "lanternctrl1-2";
     
     private String controller = DEFAULT_CONTROLLER_ID;
-    private int minpoll;
-    private int maxpoll;
+    private int minpoll = 5;
+    private int maxpoll = 15;
     private Collection<FallbackProxy> fallbacks = Collections.emptyList();
 
     public S3Config() {}
