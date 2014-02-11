getlantern/lantern

commit 131e3d8faa06109d2b4527ebc0d0a6de038d6ca5

only show free-to-friend suggestions when out of quota

app/js/controllers.js
@@ -368,6 +368,9 @@ function LanternFriendsCtrl($scope, $timeout, logFactory, $filter, INPUT_PAT, FR
     }
     _.each($scope.displayedFriends, addConnectedStatus);
     $scope.displayedFriends = _.sortBy($scope.displayedFriends, prettyUserFltr);
+    if (!model.remainingFriendingQuota) {
+      $scope.displayedSuggestions = _.filter($scope.displayedSuggestions, 'freeToFriend');
+    }
     $scope.displayedSuggestions = _.sortBy($scope.displayedSuggestions, prettyUserFltr);
   }
   $scope.$watch('model.peers', updateDisplayedFriends, true);

commit 53570b37e48214cf8573541ae70a33dd7026a947

friend suggestion fixes
only show "n friend suggestions" header when we're displaying some suggestions.
broken in certain cases by 131e3d8 which hid suggestions that aren't
freeToFriend when out of quota.

also fix copy/paste bug which causes adding a friend to fail when you're out of
quota.

app/js/app.js
@@ -114,8 +114,10 @@ var app = angular.module('app', [
       for (var i=0, l=friends.length, ii=friends[i]; ii; ii=friends[++i]) {
         $rootScope.friendsByEmail[ii.email] = ii;
         if (ii.status === FRIEND_STATUS.pending) {
-          $rootScope.nfriendSuggestions++;
-          $rootScope.friendSuggestions.push(ii);
+          if (model.remainingFriendingQuota || ii.freeToFriend) {
+            $rootScope.nfriendSuggestions++;
+            $rootScope.friendSuggestions.push(ii);
+          }
         } else if (ii.status == FRIEND_STATUS.friend) {
           $rootScope.nfriends++;
         }

app/js/controllers.js
@@ -306,7 +306,7 @@ function LanternFriendsCtrl($scope, $timeout, logFactory, $filter, INPUT_PAT, FR
       return;
     }
     if (!model.remainingFriendingQuota) {
-      var friend = $scope.friendsByEmail[match];
+      var friend = $scope.friendsByEmail[email];
       if (!friend || !friend.freeToFriend) {
         return;
       }
@@ -368,9 +368,6 @@ function LanternFriendsCtrl($scope, $timeout, logFactory, $filter, INPUT_PAT, FR
     }
     _.each($scope.displayedFriends, addConnectedStatus);
     $scope.displayedFriends = _.sortBy($scope.displayedFriends, prettyUserFltr);
-    if (!model.remainingFriendingQuota) {
-      $scope.displayedSuggestions = _.filter($scope.displayedSuggestions, 'freeToFriend');
-    }
     $scope.displayedSuggestions = _.sortBy($scope.displayedSuggestions, prettyUserFltr);
   }
   $scope.$watch('model.peers', updateDisplayedFriends, true);