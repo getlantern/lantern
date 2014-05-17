225b18d9c6e4c8127ba5505374623c5e5a8875da
Spider Person <aranhoide@gmail.com>
2014-05-17 11:20:28 +0200

    fix: copys3file.py had its own out-of-sync installer bucket

--- copys3file.py
+++ copys3file.py
@@ -4,6 +4,9 @@ import sys
 import boto
 from boto.s3.key import Key
 
+# DRY: installMetaRefresh.bash
+BUCKET = 'lantern-installers'
+
 key = str(sys.argv[1])
 
 if key.endswith('dmg'):
@@ -16,20 +19,21 @@ elif key.endswith('64-bit.deb'):
     newest = 'newest-64.deb'
 else:
     print 'File name with full version required. .deb files should end in 32-bit.deb or 64-bit.deb'
-    sys.exit(1)   
+    sys.exit(1)
 
 conn = boto.connect_s3()
-b = conn.get_bucket('lantern')
+
+b = conn.get_bucket(BUCKET)
 
 k = Key(b)
 k.key = key
-k.copy('lantern', newest, preserve_acl=True)
+k.copy(BUCKET, newest, preserve_acl=True)
 
 # Since we've just updated the fixed name 'lantest.x' file in our bucket,
 # we need to make sure to invalidate it on cloudfront in case anyone's
 # using it.
 #print 'Invalidating newest installers on CloudFront...'
 #c = boto.connect_cloudfront()
-#paths = [newest] 
+#paths = [newest]
 #inval_req = c.create_invalidation_request(u'E1D7VOTZEUYRZT', paths)
 #status = c.invalidation_request_status(u'E1D7VOTZEUYRZT', inval_req.id)
