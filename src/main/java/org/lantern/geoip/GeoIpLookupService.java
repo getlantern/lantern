2da97eadebc7ba42fc796f528e9d788e1123a1ca
Ox Cart <ox.to.a.cart@gmail.com>
2014-06-04 15:59:41 -0500

    Removed unused LookupService import

--- src/main/java/org/lantern/geoip/GeoIpLookupService.java
+++ src/main/java/org/lantern/geoip/GeoIpLookupService.java
@@ -19,7 +19,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.inject.Singleton;
-import com.maxmind.geoip.LookupService;
 
 /**
  * Service for GeoLocating. Uses MaxMind's {@link LookupService} internally.
