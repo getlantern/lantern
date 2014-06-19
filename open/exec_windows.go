a09a2535ca5758662dfb0577fee1644826d8a850
skratchdot <git@skratchdot.com>
2014-06-19 16:05:56 -0400

    fixes #6 - (Windows) open.Start does not correctly handle URLs with ampersands

--- open/exec_windows.go
+++ open/exec_windows.go
@@ -4,12 +4,18 @@ package open
 
 import (
 	"os/exec"
+	"strings"
 )
 
+func cleaninput(input string) string {
+	r := strings.NewReplacer("&", "^&")
+	return r.Replace(input)
+}
+
 func open(input string) *exec.Cmd {
-	return exec.Command("cmd", "/C", "start", "", input)
+	return exec.Command("cmd", "/C", "start", "", cleaninput(input))
 }
 
 func openWith(input string, appName string) *exec.Cmd {
-	return exec.Command("cmd", "/C", "start", "", appName, input)
+	return exec.Command("cmd", "/C", "start", "", appName, cleaninput(input))
 }
