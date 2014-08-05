3319e5673b8778f017e62bafc1f54e60c683e2de
Myles Horton <myles@getlantern.org>
2014-08-05 13:39:49 -0700

    Added duplicate checking closes #1697

--- src/main/java/org/lantern/papertrail/PapertrailAppender.java
+++ src/main/java/org/lantern/papertrail/PapertrailAppender.java
@@ -3,6 +3,9 @@ package org.lantern.papertrail;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.Socket;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashSet;
 
 import javax.net.SocketFactory;
 
@@ -10,6 +13,7 @@ import org.apache.commons.lang3.SystemUtils;
 import org.apache.log4j.Appender;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Layout;
+import org.apache.log4j.spi.LocationInfo;
 import org.apache.log4j.spi.LoggingEvent;
 import org.lantern.Censored;
 import org.lantern.LanternClientConstants;
@@ -21,6 +25,10 @@ import org.lantern.state.Model;
  * An {@link Appender} that logs to {@link Papertrail}.
  */
 public class PapertrailAppender extends AppenderSkeleton {
+    
+    private final Collection<LogData> recentLogs = 
+            Collections.synchronizedSet(new LinkedHashSet<LogData>());
+    
     private static final String PAPERTRAIL_HOST = "logs2.papertrailapp.com";
     private static final int PAPERTRAIL_PORT = 22762;
 
@@ -54,6 +62,21 @@ public class PapertrailAppender extends AppenderSkeleton {
             // Don't report anything if the user doesn't have it turned on.
             return;
         }
+        final LocationInfo li = event.getLocationInformation();
+        final LogData logData = new LogData(li);
+        if (recentLogs.contains(logData)) {
+            // Don't send duplicates to avoid hammering the server.
+            return;
+        }
+        synchronized (this.recentLogs) {
+            // Remove the oldest bug if necessary.
+            if (this.recentLogs.size() >= 200) {
+                final LogData lastIn = this.recentLogs.iterator().next();
+                this.recentLogs.remove(lastIn);
+            }
+            recentLogs.add(logData);
+        }
+        
         StringWriter message = new StringWriter();
         // Start the message off with a prefix that gives some metadata
         message.append(String.format(
@@ -81,6 +104,59 @@ public class PapertrailAppender extends AppenderSkeleton {
     @Override
     public void close() {
         // do nothing
+    }
+    
+    private static final class LogData {
+
+        private final String className;
+        private final String methodName;
+        private final String lineNumber;
+
+        private LogData(final LocationInfo li) {
+            this.className = li.getClassName();
+            this.methodName = li.getMethodName();
+            this.lineNumber = li.getLineNumber();
+        }
+
+        @Override
+        public int hashCode() {
+            final int PRIME = 31;
+            int result = 1;
+            result = PRIME * result
+                    + ((className == null) ? 0 : className.hashCode());
+            result = PRIME * result
+                    + ((lineNumber == null) ? 0 : lineNumber.hashCode());
+            result = PRIME * result
+                    + ((methodName == null) ? 0 : methodName.hashCode());
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            final LogData other = (LogData) obj;
+            if (className == null) {
+                if (other.className != null)
+                    return false;
+            } else if (!className.equals(other.className))
+                return false;
+            if (lineNumber == null) {
+                if (other.lineNumber != null)
+                    return false;
+            } else if (!lineNumber.equals(other.lineNumber))
+                return false;
+            if (methodName == null) {
+                if (other.methodName != null)
+                    return false;
+            } else if (!methodName.equals(other.methodName))
+                return false;
+            return true;
+        }
     }
 
 }
