05ac2d7bf3e780d62a89b5129d6ecfc0dfd85a6d
Sebastian Thiel <byronimo@gmail.com>
2014-09-04 12:56:57 +0200

    Adjusted linux open-code to assume existence of xdg-open.
    The current implentation didn't work without a side-by-side file, which
    somewhat reduces the usefulness of this library.
    
    See https://github.com/skratchdot/open-golang/issues/2 .

--- open/exec.go
+++ open/exec.go
@@ -4,15 +4,10 @@ package open
 
 import (
 	"os/exec"
-	"path"
-	"runtime"
 )
 
 func open(input string) *exec.Cmd {
-	// http://andrewbrookins.com/tech/golang-get-directory-of-the-current-file/
-	_, file, _, _ := runtime.Caller(1)
-	app := path.Join(path.Dir(file), "..", "vendor", "xdg-open")
-	return exec.Command(app, input)
+	return exec.Command("xdg-open", input)
 }
 
 func openWith(input string, appName string) *exec.Cmd {
