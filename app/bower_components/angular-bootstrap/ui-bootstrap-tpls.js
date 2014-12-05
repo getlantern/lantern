07a2bfea1b87c69fcc0c616d583b5929145c3e22
Todd Blose <todd@lana.io>
2014-12-04 22:25:56 -0800

    fix styling issues

--- app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js
+++ app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js
@@ -3926,7 +3926,6 @@ angular.module("template/alert/alert.html", []).run(["$templateCache", function(
     "<div class=\"alert\" ng-class=\"['alert-' + (type || 'warning'), closeable ? 'alert-dismissable' : null]\" role=\"alert\">\n" +
     "    <button ng-show=\"closeable\" type=\"button\" class=\"close\" ng-click=\"close()\">\n" +
     "        <span aria-hidden=\"true\">&times;</span>\n" +
-    "        <span class=\"sr-only\">Close</span>\n" +
     "    </button>\n" +
     "    <div ng-transclude></div>\n" +
     "</div>\n" +
