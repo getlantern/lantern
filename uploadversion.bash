2dfba194cd14e16d5c91a4232d5bb90552ae5b71
Myles Horton <myles@getlantern.org>
2014-12-09 14:25:37 -0800

    minor fix

--- uploadversion.bash
+++ uploadversion.bash
@@ -16,5 +16,4 @@ BETA=$2
 perl -pi -e "s/STABLE/$STABLE/g" version.js
 perl -pi -e "s/BETA/$BETA/g" version.js
 s3cmd put -P version.js s3://lantern
-perl -pi -e "s/$STABLE/STABLE/g" version.js
-perl -pi -e "s/$BETA/BETA/g" version.js
+git checkout -- version.js
