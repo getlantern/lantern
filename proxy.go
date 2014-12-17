da0452973dbba53192c72ba1f012831775e48064
Ox Cart <ox.to.a.cart@gmail.com>
2014-12-17 15:40:28 -0600

    Squashed 'src/github.com/getlantern/proxy/' content from commit 8e430d7
    git-subtree-dir: src/github.com/getlantern/proxy
    git-subtree-split: 8e430d7273752df7f91f22896045a8e5a76e6aec

---
+++ proxy.go
@@ -0,0 +1,71 @@
+package proxy
+
+import (
+	"io"
+	"net"
+	"testing"
+
+	"github.com/getlantern/testify/assert"
+)
+
+var (
+	ping = []byte("ping")
+	pong = []byte("pong")
+)
+
+// Dialer is an interface for types that allowing connecting to some destination
+// via an intervening proxy (or proxies).
+type Dialer interface {
+	// Dial: function that dials a given destination using the proxy.
+	Dial(network, addr string) (net.Conn, error)
+
+	// Close: closes the proxy and any underlying resources
+	Close() error
+}
+
+// Test tests a Dialer.
+func Test(t *testing.T, dialer Dialer) {
+	// Set up listener for server endpoint
+	sl, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		t.Fatalf("Unable to listen: %s", err)
+	}
+
+	// Server that responds to ping
+	go func() {
+		conn, err := sl.Accept()
+		if err != nil {
+			t.Fatalf("Unable to accept connection: %s", err)
+			return
+		}
+		defer conn.Close()
+		b := make([]byte, 4)
+		_, err = io.ReadFull(conn, b)
+		if err != nil {
+			t.Fatalf("Unable to read from client: %s", err)
+		}
+		assert.Equal(t, ping, b, "Didn't receive correct ping message")
+		_, err = conn.Write(pong)
+		if err != nil {
+			t.Fatalf("Unable to write to client: %s", err)
+		}
+	}()
+
+	conn, err := dialer.Dial(sl.Addr().Network(), sl.Addr().String())
+	if err != nil {
+		t.Fatalf("Unable to dial via proxy: %s", err)
+	}
+	defer conn.Close()
+
+	_, err = conn.Write(ping)
+	if err != nil {
+		t.Fatalf("Unable to write to server via proxy: %s", err)
+	}
+
+	b := make([]byte, 4)
+	_, err = io.ReadFull(conn, b)
+	if err != nil {
+		t.Fatalf("Unable to read from server: %s", err)
+	}
+	assert.Equal(t, pong, b, "Didn't receive correct pong message")
+}
