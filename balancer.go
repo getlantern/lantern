e8cb7112f161ed103c5a80cd11ea651d691e450e
Ox Cart <ox.to.a.cart@gmail.com>
2014-12-17 15:40:18 -0600

    Squashed 'src/github.com/getlantern/balancer/' content from commit b8e5517
    git-subtree-dir: src/github.com/getlantern/balancer
    git-subtree-split: b8e5517c9f18186956e6328d21faa2bdb825f840

---
+++ balancer.go
@@ -0,0 +1,177 @@
+package balancer
+
+import (
+	"fmt"
+	"math/rand"
+	"net"
+	"sort"
+
+	"github.com/getlantern/golog"
+)
+
+var (
+	log = golog.LoggerFor("balancer")
+)
+
+var (
+	emptyDialers = []*dialer{}
+)
+
+// Balancer balances connections established by one or more Dialers.
+type Balancer struct {
+	dialers []*dialer
+}
+
+// New creates a new Balancer using the supplied Dialers.
+func New(dialers ...*Dialer) *Balancer {
+	dhs := make([]*dialer, 0, len(dialers))
+	for _, d := range dialers {
+		dl := &dialer{Dialer: d}
+		dl.start()
+		dhs = append(dhs, dl)
+	}
+	// Sort dialers by QOS for later selection
+	sort.Sort(byQOS(dhs))
+	return &Balancer{
+		dialers: dhs,
+	}
+}
+
+// DialQOS dials network, addr using one of the currently active configured
+// Dialers. It attempts to use a Dialer whose QOS is higher than targetQOS, but
+// will use the highest QOS Dialer(s) if none meet targetQOS. When multiple
+// Dialers meet the targetQOS, load is distributed amongst them randomly based
+// on their relative Weights.
+//
+// If a Dialer fails to connect, Dial will keep falling back through the
+// remaining Dialers until it either manages to connect, or runs out of dialers
+// in which case it returns an error.
+func (b *Balancer) DialQOS(network, addr string, targetQOS int) (net.Conn, error) {
+	dialers := b.getDialers()
+	for {
+		if len(dialers) == 0 {
+			return nil, fmt.Errorf("No dialers left to try")
+		}
+		var d *dialer
+		d, dialers = randomDialer(dialers, targetQOS)
+		if d == nil {
+			return nil, fmt.Errorf("No dialers left")
+		}
+		if d.Label != "" {
+			log.Debugf("Dialing %s://%s with %s", network, addr, d.Label)
+		}
+		conn, err := d.Dial(network, addr)
+		if err != nil {
+			log.Tracef("Unable to dial: %s", err)
+			d.onError(err)
+			continue
+		}
+		return conn, nil
+	}
+}
+
+// Dial is like DialQOS with a targetQOS of 0.
+func (b *Balancer) Dial(network, addr string) (net.Conn, error) {
+	return b.DialQOS(network, addr, 0)
+}
+
+// Close closes this Balancer, stopping all background processing. You must call
+// Close to avoid leaking goroutines.
+func (b *Balancer) Close() {
+	for _, d := range b.dialers {
+		d.stop()
+	}
+}
+
+func (b *Balancer) getDialers() []*dialer {
+	result := make([]*dialer, len(b.dialers))
+	copy(result, b.dialers)
+	return result
+}
+
+func randomDialer(dialers []*dialer, targetQOS int) (chosen *dialer, others []*dialer) {
+	// Weed out inactive dialers and those with too low QOS, preferring higher
+	// QOS
+	filtered := make([]*dialer, 0)
+	highestQOS := 0
+	for _, d := range dialers {
+		if !d.isActive() {
+			log.Trace("Excluding inactive dialer")
+			continue
+		}
+
+		highestQOS = d.QOS // don't need to compare since dialers are already sorted by QOS
+		if d.QOS >= targetQOS {
+			log.Tracef("Including dialer with QOS %d meeting targetQOS %d", d.QOS, targetQOS)
+			filtered = append(filtered, d)
+		}
+	}
+
+	if len(filtered) == 0 {
+		log.Trace("No dialers meet targetQOS, finding remaining with highest QOS")
+		for _, d := range dialers {
+			if !d.isActive() {
+				log.Trace("Excluding inactive dialer")
+				continue
+			}
+
+			if d.QOS == highestQOS {
+				filtered = append(filtered, d)
+			}
+		}
+	}
+
+	if len(filtered) == 0 {
+		log.Trace("Still no dialers!")
+		return nil, nil
+	}
+
+	totalWeights := 0
+	for _, d := range filtered {
+		totalWeights += d.Weight
+	}
+
+	// Pick a random server using a target value between 0 and the total weights
+	t := rand.Intn(totalWeights)
+	aw := 0
+	for _, d := range filtered {
+		aw += d.Weight
+		if aw > t {
+			log.Trace("Reached random target value, using this dialer")
+			return d, withoutDialer(dialers, d)
+		}
+	}
+
+	// We should never reach this
+	panic("No dialer found!")
+}
+
+func withoutDialer(dialers []*dialer, d *dialer) []*dialer {
+	for i, existing := range dialers {
+		if existing == d {
+			return without(dialers, i)
+		}
+	}
+	log.Tracef("Dialer not found for removal: %s", d)
+	return dialers
+}
+
+func without(dialers []*dialer, i int) []*dialer {
+	if len(dialers) == 1 {
+		return emptyDialers
+	} else if i == len(dialers)-1 {
+		return dialers[:i]
+	} else {
+		c := make([]*dialer, len(dialers)-1)
+		copy(c[:i], dialers[:i])
+		copy(c[i:], dialers[i+1:])
+		return c
+	}
+}
+
+// byQOS implements sort.Interface for []*dialer based on the QOS
+type byQOS []*dialer
+
+func (a byQOS) Len() int           { return len(a) }
+func (a byQOS) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byQOS) Less(i, j int) bool { return a[i].QOS < a[j].QOS }
---
+++ balancer_test.go
@@ -0,0 +1,310 @@
+package balancer
+
+import (
+	"fmt"
+	"io"
+	"math/rand"
+	"net"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/getlantern/testify/assert"
+)
+
+var (
+	msg = []byte("Hello world")
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func TestRandomDialer(t *testing.T) {
+	dialers := []*dialer{
+		&dialer{
+			Dialer: &Dialer{
+				Label:  "A",
+				Weight: 90,
+			},
+			active: 1,
+		}, &dialer{
+			Dialer: &Dialer{
+				Label:  "B",
+				Weight: 9,
+			},
+			active: 1,
+		}, &dialer{
+			Dialer: &Dialer{
+				Label:  "C",
+				Weight: 1,
+			},
+			active: 1,
+		},
+	}
+
+	trials := 1000000
+	counts := make(map[string]float64)
+	for i := 0; i < trials; i++ {
+		d, _ := randomDialer(dialers, 0)
+		counts[d.Label] = counts[d.Label] + 1
+	}
+	assertWithinRangeOf(t, counts["A"], .9*float64(trials), .1)
+	assertWithinRangeOf(t, counts["B"], .09*float64(trials), .1)
+	assertWithinRangeOf(t, counts["C"], .01*float64(trials), .1)
+}
+
+func assertWithinRangeOf(t *testing.T, actual float64, expected float64, margin float64) {
+	assert.True(t, actual >= expected*(1-margin) && actual <= expected*(1+margin), fmt.Sprintf("%v not within %v of %v", actual, margin, expected))
+}
+
+func TestAll(t *testing.T) {
+	// Start an echo server
+	l, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		log.Fatalf("Unable to listen: %s", err)
+	}
+	defer l.Close()
+	go func() {
+		for {
+			c, err := l.Accept()
+			if err == nil {
+				go func() {
+					_, err = io.Copy(c, c)
+					if err != nil {
+						log.Fatalf("Unable to echo: %s", err)
+					}
+				}()
+			}
+		}
+	}()
+	addr := l.Addr().String()
+
+	dialedBy := int32(0)
+
+	dialer1Closed := int32(0)
+	dialer1 := &Dialer{
+		Label:  "Dialer 1",
+		Weight: 1,
+		QOS:    10,
+		Dial: func(network, addr string) (net.Conn, error) {
+			atomic.StoreInt32(&dialedBy, 1)
+			return net.Dial(network, addr)
+		},
+		OnClose: func() {
+			atomic.StoreInt32(&dialer1Closed, 1)
+		},
+	}
+	dialer2 := &Dialer{
+		Weight: 10000000,
+		QOS:    1,
+		Dial: func(network, addr string) (net.Conn, error) {
+			atomic.StoreInt32(&dialedBy, 2)
+			return net.Dial(network, addr)
+		},
+	}
+	checkAttempts := int32(0)
+	dialer3 := &Dialer{
+		Weight: 1,
+		QOS:    15,
+		Dial: func(network, addr string) (net.Conn, error) {
+			atomic.StoreInt32(&dialedBy, 3)
+			if checkAttempts < 6 {
+				// Fail for a while
+				return nil, fmt.Errorf("Me no dialee")
+			} else {
+				// Eventually succeed
+				return net.Dial(network, addr)
+			}
+		},
+		Check: func() bool {
+			time.Sleep(100 * time.Millisecond)
+			n := atomic.AddInt32(&checkAttempts, 1)
+			return n > 5
+		},
+	}
+
+	d4attempts := int32(0)
+	dialer4 := &Dialer{
+		Weight: 1,
+		QOS:    15,
+		Dial: func(network, addr string) (net.Conn, error) {
+			atomic.StoreInt32(&dialedBy, 4)
+			defer atomic.AddInt32(&d4attempts, 1)
+			if d4attempts < 1 {
+				// Fail once
+				return nil, fmt.Errorf("Me no dialee")
+			} else {
+				// Eventually succeed
+				return net.Dial(network, addr)
+			}
+		},
+	}
+
+	// Test failure with no dialers
+	b := New()
+	_, err = b.Dial("tcp", addr)
+	assert.Error(t, err, "Dialing with no dialers should have failed")
+
+	// Test successful single dialer
+	b = New(dialer1)
+	defer func() {
+		b.Close()
+		time.Sleep(250 * time.Millisecond)
+		assert.Equal(t, int32(1), dialer1Closed, "Dialer 1 should have been closed")
+	}()
+	conn, err := b.Dial("tcp", addr)
+	assert.NoError(t, err, "Dialing should have succeeded")
+	assert.Equal(t, 1, dialedBy, "Wrong dialedBy")
+	if err == nil {
+		doTestConn(t, conn)
+	}
+
+	// Test QOS
+	dialedBy = 0
+	b = New(dialer1, dialer2)
+	defer b.Close()
+	conn, err = b.DialQOS("tcp", addr, 5)
+	assert.NoError(t, err, "Dialing should have succeeded")
+	assert.Equal(t, 1, dialedBy, "Wrong dialedBy")
+	if err == nil {
+		doTestConn(t, conn)
+	}
+
+	// Test random selection
+	dialedBy = 0
+	conn, err = b.Dial("tcp", addr)
+	assert.NoError(t, err, "Dialing should have succeeded")
+	assert.Equal(t, 2, dialedBy, "Wrong dialedBy (note this has a 1/%d chance of failing)", (dialer1.Weight + dialer2.Weight))
+	if err == nil {
+		doTestConn(t, conn)
+	}
+
+	// Test success with failing dialer
+	dialedBy = 0
+	b = New(dialer1, dialer2, dialer3)
+	defer b.Close()
+	conn, err = b.DialQOS("tcp", addr, 20)
+	assert.NoError(t, err, "Dialing should have succeeded")
+	assert.Equal(t, 1, dialedBy, "Wrong dialedBy")
+	if err == nil {
+		doTestConn(t, conn)
+	}
+
+	// Test failure
+	b = New(dialer3)
+	maxCheckTimeout = 100 * time.Millisecond
+	// Dial a bunch of times on multiple goroutines to hit different failure branches
+	var wg sync.WaitGroup
+	wg.Add(2)
+	for j := 0; j < 2; j++ {
+		bn := b
+		go func() {
+			for i := 0; i < 50; i++ {
+				_, err := bn.Dial("tcp", addr)
+				assert.Error(t, err, "Dialing should have failed")
+				time.Sleep(10 * time.Millisecond)
+			}
+			wg.Done()
+		}()
+	}
+
+	time.Sleep(1 * time.Second)
+	assert.Equal(t, 6, checkAttempts, "Wrong number of check attempts on failed dialer")
+	wg.Wait()
+
+	// Test success after successful recheck using custom check
+	conn, err = b.DialQOS("tcp", addr, 20)
+	assert.NoError(t, err, "Dialing should have succeeded")
+	assert.Equal(t, 3, dialedBy, "Wrong dialedBy")
+	if err == nil {
+		doTestConn(t, conn)
+	}
+
+	// Test failure
+	b = New(dialer4)
+	_, err = b.Dial("tcp", addr)
+	assert.Error(t, err, "Dialing should have failed")
+
+	time.Sleep(1 * time.Second)
+	assert.Equal(t, 2, d4attempts, "Wrong number of dial attempts on failed dialer")
+
+	// Test success after successful retest using default check
+	conn, err = b.DialQOS("tcp", addr, 20)
+	assert.NoError(t, err, "Dialing should have succeeded")
+	assert.Equal(t, 4, dialedBy, "Wrong dialedBy")
+	if err == nil {
+		doTestConn(t, conn)
+	}
+
+}
+
+func doTestConn(t *testing.T, conn net.Conn) {
+	defer conn.Close()
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		n, err := conn.Write(msg)
+		assert.NoError(t, err, "Writing should have succeeded")
+		assert.Equal(t, len(msg), n, "Should have written full message")
+		wg.Done()
+	}()
+	go func() {
+		b := make([]byte, len(msg))
+		n, err := io.ReadFull(conn, b)
+		assert.NoError(t, err, "Read should have succeeded")
+		assert.Equal(t, len(msg), n, "Should have read full message")
+		assert.Equal(t, msg, b[:n], "Read should have matched written")
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
+
+var (
+	failed = fmt.Errorf("I failed")
+)
+
+type failingConn struct {
+	conn         net.Conn
+	bytesRead    int
+	bytesWritten int
+}
+
+func (c *failingConn) Read(b []byte) (n int, err error) {
+	n, err = c.conn.Read(b[:c.bytesRead])
+	err = failed
+	return
+}
+
+func (c *failingConn) Write(b []byte) (n int, err error) {
+	n, err = c.conn.Write(b[:c.bytesWritten])
+	err = failed
+	return
+}
+
+func (c *failingConn) LocalAddr() net.Addr {
+	return c.conn.LocalAddr()
+}
+
+func (c *failingConn) RemoteAddr() net.Addr {
+	return c.conn.RemoteAddr()
+}
+
+func (c *failingConn) SetDeadline(t time.Time) error {
+	return c.conn.SetDeadline(t)
+}
+
+func (c *failingConn) SetReadDeadline(t time.Time) error {
+	return c.conn.SetReadDeadline(t)
+}
+
+func (c *failingConn) SetWriteDeadline(t time.Time) error {
+	return c.conn.SetWriteDeadline(t)
+}
+
+func (c *failingConn) Close() error {
+	return c.conn.Close()
+}
