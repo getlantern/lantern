6c2f2134caf4010f581953a9846f38cb7eac8d8f
Myles Horton <myles@getlantern.org>
2014-12-18 10:42:31 -0800

    removed chrome flag

--- src/main/java/org/lantern/browser/OsxBrowser.java
+++ src/main/java/org/lantern/browser/OsxBrowser.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
