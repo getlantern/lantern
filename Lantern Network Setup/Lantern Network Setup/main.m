c80a2b3ec960d3acb5a629a51d4fa37c1ffffedf
Myles Horton <myles@getlantern.org>
2015-01-09 14:39:24 -0800

    Changes from @fffw to fix issues with system proxy config on OSX closes #2151

--- Lantern Network Setup/Lantern Network Setup/main.m
+++ Lantern Network Setup/Lantern Network Setup/main.m
@@ -1,102 +1,134 @@
 /** Configures the Lantern network proxy on all interfaces.
-
+ 
  By Leah X Schmidt
  Copyright 2013, Brave New Software
-
+ 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
-
+ 
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
-
+ 
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+ 
  */
-#import <Foundation/NSObject.h>
-#import <Foundation/NSString.h>
-#import <Foundation/NSTask.h>
-#import <Foundation/NSData.h>
 #import <Foundation/NSArray.h>
 #import <Foundation/Foundation.h>
-#import <Foundation/NSProcessInfo.h>
-#import <Foundation/NSAutoreleasePool.h>
+#import <SystemConfiguration/SCPreferences.h>
+#import <SystemConfiguration/SCNetworkConfiguration.h>
 
-NSString* networkSetupPath = @"/usr/sbin/networksetup";
 
-void runNetworkSetup(NSString *command, NSString *arg1, NSString *arg2) {
-
-    NSTask *task;
-    task = [[NSTask alloc] init];
-    [task setLaunchPath: networkSetupPath];
-
-    NSPipe *pipe;
-    pipe = [NSPipe pipe];
-    [task setStandardOutput: pipe];
-
-    NSArray *arguments;
-    arguments = [NSArray arrayWithObjects: command, arg1, arg2, nil];
-    [task setArguments: arguments];
-
-    [task launch];
-
-    //ignore output
-    [[pipe fileHandleForReading] readDataToEndOfFile];
-
-}
-
-void configureNetworkServices(NSString *onOff, NSString *url) {
-
-    NSTask *task;
-    task = [[NSTask alloc] init];
-    [task setLaunchPath: networkSetupPath];
-
-    NSArray *arguments;
-    arguments = [NSArray arrayWithObjects: @"-listallnetworkservices", nil];
-    [task setArguments: arguments];
-
-    NSPipe *pipe;
-    pipe = [NSPipe pipe];
-    [task setStandardOutput: pipe];
-
-    NSFileHandle *file;
-    file = [pipe fileHandleForReading];
-
-    [task launch];
-
-    NSData *data;
-    data = [file readDataToEndOfFile];
-
-    NSString *string;
-    string = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
-
-    NSArray *lines = [string componentsSeparatedByString:@"\n"];
-    int line = 0;
-    for (NSString *s in lines) {
-        line += 1;
-        if (line == 1)
-            //skip first line
-            continue;
-        if ([s length] == 0)
-            continue;
-        runNetworkSetup(@"-setautoproxyurl", s, url);
-        runNetworkSetup(@"-setautoproxystate", s, onOff);
+void toggleAutoProxyConfigFile(NSString *onOff, NSString *autoProxyConfigFileUrl)
+{
+    
+    NSLog(@"Toggle %@ auto proxy configuration file to %@", onOff, autoProxyConfigFileUrl);
+    BOOL success = FALSE;
+    
+    SCNetworkSetRef networkSetRef;
+    CFArrayRef networkServicesArrayRef;
+    SCNetworkServiceRef networkServiceRef;
+    SCNetworkProtocolRef proxyProtocolRef;
+    NSDictionary *oldPreferences;
+    NSMutableDictionary *newPreferences;
+    NSString *wantedHost;
+    
+    
+    // Get System Preferences Lock
+    SCPreferencesRef prefsRef = SCPreferencesCreate(NULL, CFSTR("org.getlantern.lantern"), NULL);
+    
+    if(prefsRef==NULL) {
+        NSLog(@"Fail to obtain Preferences Ref!!");
+        goto freePrefsRef;
     }
-
+    
+    success = SCPreferencesLock(prefsRef, TRUE);
+    if (!success) {
+        NSLog(@"Fail to obtain PreferencesLock");
+        goto freePrefsRef;
+    }
+    
+    // Get available network services
+    networkSetRef = SCNetworkSetCopyCurrent(prefsRef);
+    if(networkSetRef == NULL) {
+        NSLog(@"Fail to get available network services");
+        goto freeNetworkSetRef;
+    }
+    
+    //Look up interface entry
+    networkServicesArrayRef = SCNetworkSetCopyServices(networkSetRef);
+    networkServiceRef = NULL;
+    for (long i = 0; i < CFArrayGetCount(networkServicesArrayRef); i++) {
+        networkServiceRef = CFArrayGetValueAtIndex(networkServicesArrayRef, i);
+        NSLog(@"Setting proxy for device %@", SCNetworkServiceGetName(networkServiceRef));
+        
+        // Get proxy protocol
+        proxyProtocolRef = SCNetworkServiceCopyProtocol(networkServiceRef, kSCNetworkProtocolTypeProxies);
+        if(proxyProtocolRef == NULL) {
+            NSLog(@"Couldn't acquire copy of proxyProtocol");
+            goto freeProxyProtocolRef;
+        }
+        
+        oldPreferences = (__bridge NSDictionary*)SCNetworkProtocolGetConfiguration(proxyProtocolRef);
+        newPreferences = [NSMutableDictionary dictionaryWithDictionary: oldPreferences];
+        wantedHost = @"localhost";
+        
+        if([onOff  isEqual: @"on"]) {//Turn proxy configuration ON
+            [newPreferences setValue: wantedHost forKey:(NSString*)kSCPropNetProxiesHTTPProxy];
+            [newPreferences setValue:[NSNumber numberWithInt:1] forKey:(NSString*)kSCPropNetProxiesProxyAutoConfigEnable];
+            [newPreferences setValue:autoProxyConfigFileUrl forKey:(NSString*)kSCPropNetProxiesProxyAutoConfigURLString];
+            NSLog(@"Setting Proxy ON with: %@", newPreferences);
+        } else {//Turn proxy configuration OFF
+            [newPreferences setValue:[NSNumber numberWithInt:0] forKey:(NSString*)kSCPropNetProxiesProxyAutoConfigEnable];
+            NSLog(@"Setting Proxy OFF");
+        }
+        
+        success = SCNetworkProtocolSetConfiguration(proxyProtocolRef, (__bridge CFDictionaryRef)newPreferences);
+        if(!success) {
+            NSLog(@"Failed to set Protocol Configuration");
+            goto freeProxyProtocolRef;
+        }
+        
+    freeProxyProtocolRef:
+        CFRelease(proxyProtocolRef);
+    }
+    
+    success = SCPreferencesCommitChanges(prefsRef);
+    if(!success) {
+        NSLog(@"Failed to Commit Changes");
+        goto freeNetworkServicesArrayRef;
+    }
+    
+    success = SCPreferencesApplyChanges(prefsRef);
+    if(!success) {
+        NSLog(@"Failed to Apply Changes");
+        goto freeNetworkServicesArrayRef;
+    }
+    //Free Resources
+freeNetworkServicesArrayRef:
+    CFRelease(networkServicesArrayRef);
+freeNetworkSetRef:
+    CFRelease(networkSetRef);
+freePrefsRef:
+    SCPreferencesUnlock(prefsRef);
+    CFRelease(prefsRef);
+    
+    return;
 }
+
 
 int main() {
     NSArray *args = [[NSProcessInfo processInfo] arguments];
-
+    
     // Become root in order to support reconfiguring network services
     setuid(0);
     
-    configureNetworkServices([args objectAtIndex:1],
-                             [args objectAtIndex:2]);
-
+    toggleAutoProxyConfigFile([args objectAtIndex:1],
+                              [args objectAtIndex:2]);
+    
     return 0;
 }
