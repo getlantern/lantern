8ac9cff58579bcf5edf8c127b48834a6fdf35324
José Carlos Nieto <jose.carlos@menteslibres.net>
2015-01-29 11:53:27 -0600

    Fixing tests to make sure we explain what's the intention behind them.

--- src/github.com/getlantern/lantern-android/client/client_test.go
+++ src/github.com/getlantern/lantern-android/client/client_test.go
@@ -7,16 +7,88 @@ import (
 	"net"
 	"net/http"
 	"net/url"
+	"sync"
 	"testing"
 	"time"
 )
 
 const listenProxyAddr = "127.0.0.1:9997"
 
+var globalClient *Client
+
 var testURLs = map[string][]byte{
 	"http://www.google.com/humans.txt": []byte("Google is built by a large team of engineers, designers, researchers, robots, and others in many different sites across the globe. It is updated continuously, and built with more tools and technologies than we can shake a stick at. If you'd like to help us out, see google.com/careers.\n"),
 	// TODO: This is not working, we actually need to implement a CONNECT request to proxy HTTPs traffic.
 	// "https://www.google.com/humans.txt": []byte("Google is built by a large team of engineers, designers, researchers, robots, and others in many different sites across the globe. It is updated continuously, and built with more tools and technologies than we can shake a stick at. If you'd like to help us out, see google.com/careers.\n"),
+}
+
+// Attempt to create a server in a goroutine and stop it from other place.
+func TestListenAndServeStop(t *testing.T) {
+
+	// Creating a client.
+	c := NewClient(listenProxyAddr)
+
+	if c == nil {
+		t.Fatal("You should be able to create a client.")
+	}
+
+	// Make the client listen on a goroutine.
+	go func() {
+		c.ListenAndServe()
+	}()
+
+	// Allow it some seconds to start.
+	time.Sleep(time.Millisecond * 100)
+
+	// Attempt to stop server.
+	if err := c.Stop(); err != nil {
+		t.Fatal("You should be able to close listening client.")
+	}
+
+}
+
+func TestListenAndServeAgain(t *testing.T) {
+	// Since we've closed out server, we should be able to launch another at the
+	// same address.
+
+	go func() {
+		globalClient = NewClient(listenProxyAddr)
+
+		if err := globalClient.ListenAndServe(); err != nil {
+			t.Fatal(err)
+		}
+
+	}()
+
+	// Allow it some seconds to start.
+	time.Sleep(time.Millisecond * 100)
+}
+
+func TestListenAndServeProxy(t *testing.T) {
+	var wg sync.WaitGroup
+
+	// Testing the client we've just opened.
+	for uri, expectedContent := range testURLs {
+		wg.Add(1)
+
+		go func(wg *sync.WaitGroup) {
+			if err := testReverseProxy(uri, expectedContent); err != nil {
+				t.Fatal(err)
+			}
+			wg.Done()
+		}(&wg)
+
+	}
+
+	wg.Wait()
+}
+
+func TestCloseClient(t *testing.T) {
+
+	// Closing the client that is still opened.
+	if err := globalClient.Stop(); err != nil {
+		t.Fatal("You should be able to close listening client.")
+	}
 }
 
 func testReverseProxy(uri string, expectedContent []byte) (err error) {
@@ -72,37 +144,4 @@ func testReverseProxy(uri string, expectedContent []byte) (err error) {
 	}
 
 	return nil
-}
-
-func TestListenAndServeStop(t *testing.T) {
-
-	c := NewClient(listenProxyAddr)
-
-	go func() {
-		c.ListenAndServe()
-	}()
-
-	time.Sleep(time.Millisecond * 100)
-
-	c.Stop()
-}
-
-func TestListenAndServeAgain(t *testing.T) {
-
-	go func() {
-		c := NewClient(listenProxyAddr)
-		var err error
-		if err = c.ListenAndServe(); err != nil {
-			t.Fatal(err)
-		}
-	}()
-
-}
-
-func TestListenAndServeProxy(t *testing.T) {
-	for uri, expectedContent := range testURLs {
-		if err := testReverseProxy(uri, expectedContent); err != nil {
-			t.Fatal(err)
-		}
-	}
 }
--- src/github.com/getlantern/lantern-android/client/listener_test.go
+++ src/github.com/getlantern/lantern-android/client/listener_test.go
@@ -14,10 +14,12 @@ var ln *listener
 func TestListenerSpawn(t *testing.T) {
 	var err error
 
+	// Are we capable of creating a listener?
 	if ln, err = newListener(listenAddr); err != nil {
 		t.Fatal(err)
 	}
 
+	// Move Accept inside a goroutine.
 	go func() {
 		for {
 			if _, err = ln.Accept(); err != nil {
@@ -28,9 +30,10 @@ func TestListenerSpawn(t *testing.T) {
 
 }
 
-func TestListenerConnect(t *testing.T) {
+func TestListenerConnectToNewListener(t *testing.T) {
 	var err error
 
+	// Attempt to connect to the listener we've created.
 	if _, err = net.Dial("tcp", listenAddr); err != nil {
 		t.Fatal(err)
 	}
@@ -39,22 +42,26 @@ func TestListenerConnect(t *testing.T) {
 func TestListenerStop(t *testing.T) {
 	var err error
 
+	// Attempt to stop the listener we've created.
 	if err = ln.Stop(); err != nil {
 		t.Fatal(err)
 	}
 }
 
-func TestListenerRejectedConn(t *testing.T) {
+func TestListenerConnectToClosedListener(t *testing.T) {
 	var err error
 
+	// We should not be able to connect.
 	if _, err = net.Dial("tcp", listenAddr); err == nil {
 		t.Fatal("Should have failed.")
 	}
 }
 
-func TestListenerRespawn(t *testing.T) {
+func TestListenerRespawnOnSameAddress(t *testing.T) {
 	var err error
 
+	// Attempt to create a listener at the same address should succeed, since
+	// we've closed the old listener.
 	if _, err = newListener(listenAddr); err != nil {
 		t.Fatal(err)
 	}
