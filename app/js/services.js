b0420b2577a15c1667e017030c94482c2971a533
Todd Blose <todd@lana.io>
2015-02-03 10:46:36 -0500

    update rootscope apply check

--- app/js/services.js
+++ app/js/services.js
@@ -44,7 +44,7 @@ angular.module('app.services', [])
     cometd.websocketsEnabled = true;
     cometd.configure({
       url: COMETD_URL,
-      logLevel: 'debug',
+      //logLevel: 'debug',
       backoffIncrement: 100,
       maxBackoff: 500,
       // necessary to work with Faye backend when browser lacks websockets:
@@ -170,7 +170,7 @@ angular.module('app.services', [])
         syncSubscriptionKey;
 
 
-    $rootScope.$apply(handleSync);
+    $rootScope.validatedModel = false;
 
     // XXX use modelValidatorSrvc to validate update before accepting
     function handleSync(msg) {
@@ -181,27 +181,37 @@ angular.module('app.services', [])
         log.debug('ignoring', msg, 'while model has not yet been populated');
         return;
       }
-      var shouldUpdateInstanceStats = false;
-      if (patch[0].path === '') {
-        // XXX jsonpatch can't mutate root object https://github.com/dharmafly/jsonpatch.js/issues/10
-        angular.copy(patch[0].value, model);
-      } else {
-        try {
-            applyPatch(model, patch);
-            for (var i=0; i<patch.length; i++) {
-                if (patch[i].path == "/instanceStats") {
-                    shouldUpdateInstanceStats = true;
-                    break;
+
+      function updateModel() {
+        var shouldUpdateInstanceStats = false;
+        if (patch[0].path === '') {
+            // XXX jsonpatch can't mutate root object https://github.com/dharmafly/jsonpatch.js/issues/10
+            angular.copy(patch[0].value, model);
+          } else {
+            try {
+                applyPatch(model, patch);
+                for (var i=0; i<patch.length; i++) {
+                    if (patch[i].path == "/instanceStats") {
+                        shouldUpdateInstanceStats = true;
+                        break;
+                      }
+                  }
+                } catch (e) {
+                  if (!(e instanceof PatchApplyError || e instanceof InvalidPatch)) throw e;
+                  log.error('Error applying patch', patch);
+                  apiSrvc.exception({exception: e, patch: patch});
                 }
             }
-        } catch (e) {
-            if (!(e instanceof PatchApplyError || e instanceof InvalidPatch)) throw e;
-            log.error('Error applying patch', patch);
-            apiSrvc.exception({exception: e, patch: patch});
+            flashlightStats.updateModel(model, shouldUpdateInstanceStats);
+        }
+
+        if (!$rootScope.validatedModel) { 
+            $rootScope.$apply(updateModel()); 
+            $rootScope.validatedModel = true 
+        } else { 
+            updateModel(); 
         }
       }
-      flashlightStats.updateModel(model, shouldUpdateInstanceStats);
-    }
 
     syncSubscriptionKey = {chan: MODEL_SYNC_CHANNEL, cb: handleSync};
     cometdSrvc.subscribe(syncSubscriptionKey);
