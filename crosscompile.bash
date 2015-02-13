ff305fdb4e3c17f35d4a36f5f698dcccce55d44d
Ox Cart <ox.to.a.cart@gmail.com>
2015-02-12 23:42:15 -0600

    Updated crosscompile script to only do Windows and OS X

--- crosscompile.bash
+++ crosscompile.bash
@@ -5,4 +5,6 @@
 VERSION="`git describe --abbrev=0 --tags --exact-match || git rev-parse --short HEAD`"
 BUILD_DATE="`date -u +%Y%m%d%.%H%M%S`"
 echo "Building flashlight version $VERSION ($BUILD_DATE)"
-gox -ldflags="-w -X main.version $VERSION -X main.buildDate $BUILD_DATE" -osarch="linux/386 linux/amd64 windows/386 darwin/amd64" github.com/getlantern/flashlight
+#gox -ldflags="-w -X main.version $VERSION -X main.buildDate $BUILD_DATE" -osarch="linux/386 linux/amd64 windows/386 darwin/amd64" github.com/getlantern/flashlight
+# Linux currently doesn't support cross compilation because of native dependency in systray
+gox -ldflags="-w -X main.version $VERSION -X main.buildDate $BUILD_DATE" -osarch="windows/386 darwin/amd64" github.com/getlantern/flashlight
