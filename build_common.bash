6783390b40fe7d737d267a0cf97bf8cb64bf02e9
Ox Cart <ox.to.a.cart@gmail.com>
2015-02-15 06:45:34 -0600

    Removed installer support

--- build_common.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# The VERSION is set to the tag for the current commit (if it exists) otherwise
-# just the commit id.
-export VERSION="`git describe --abbrev=0 --tags --exact-match || git rev-parse --short HEAD`"
-export BUILD_DATE="`date -u +%Y%m%d%.%H%M%S`"
-echo "Building flashlight version $VERSION ($BUILD_DATE)"
--- build_windows.bat
+++
@@ -1,3 +0,0 @@
-go build -a -ldflags -H=windowsgui github.com/getlantern/flashlight
-move flashlight.exe lantern.exe
-"C:\Program Files\NSIS\Bin\makensis.exe" lantern.nsi
