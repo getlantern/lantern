6ce147d6774395d5ffc22f00dbfb4b21cadab4de
fffw <fffw@getlantern.org>
2015-02-18 11:22:03 +0800

    do...while() version of wfilter Write()

--- src/github.com/getlantern/wfilter/wfilter.go
+++ src/github.com/getlantern/wfilter/wfilter.go
@@ -13,54 +13,28 @@ type Prepend func(w io.Writer) (int, error)
 // along with the original writer, and it can write whatever it wants. It should
 // return the bytes written and any error encountered.
 func LinePrepender(w io.Writer, prepend Prepend) io.Writer {
-	return &lp{w, prepend, true}
+	return &lp{w, prepend}
 }
 
 type lp struct {
 	io.Writer
-	prepend       Prepend
-	prependNeeded bool
+	prepend Prepend
 }
 
 func (w *lp) Write(buf []byte) (int, error) {
-	if w.prependNeeded {
+	totalN := 0
+	for {
 		_, err := w.prepend(w.Writer)
 		if err != nil {
-			return 0, err
+			return totalN, err
 		}
-		w.prependNeeded = false
-	}
-
-	// Prepend before every newline in the buffer
-	totalN := 0
-
-	for {
 		i := bytes.IndexRune(buf, '\n') + 1
-		if i > 0 {
-			if i < len(buf) {
-				// Newline is in middle of buffer
-				// Write up to newline
-				n, err := w.Writer.Write(buf[:i])
-				totalN += n
-				if err != nil {
-					return totalN, err
-				}
-				// Add prepend
-				_, err = w.prepend(w.Writer)
-				if err != nil {
-					return totalN, err
-				}
-				// Remove processed portion of buffer
-				buf = buf[i:]
-				continue
-			}
-			// Newline is at end of buffer, mark prependNeeded for next write
-			w.prependNeeded = true
+		isLastLine := (i == len(buf))
+		n, err := w.Writer.Write(buf[:i])
+		totalN += n
+		if err != nil || isLastLine {
+			return totalN, err
 		}
-		break
+		buf = buf[i:]
 	}
-
-	n, err := w.Writer.Write(buf)
-	totalN += n
-	return totalN, err
 }
