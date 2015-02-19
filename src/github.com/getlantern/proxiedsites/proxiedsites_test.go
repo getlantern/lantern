e87ce4ffe8d7f2fdff74566be2c0560af9fdff6b
Todd Blose <todd@lana.io>
2015-02-19 18:55:23 -0500

    add missing unit tests

--- src/github.com/getlantern/proxiedsites/proxiedsites_test.go
+++ src/github.com/getlantern/proxiedsites/proxiedsites_test.go
@@ -7,16 +7,12 @@ import (
 	"time"
 )
 
-var (
-	pacFilePath = PacFilename
-)
-
 var mockProxiedSites = []struct {
 	entry *ProxiedSites
 }{
 	{
 		entry: New(&Config{
-			Cloud:     []string{},
+			Cloud:     []string{"golang.org", "swift.com", "twitter.com"},
 			Additions: []string{},
 			Deletions: []string{},
 		}),
@@ -24,8 +20,15 @@ var mockProxiedSites = []struct {
 	{
 		entry: New(&Config{
 			Cloud:     []string{"golang.org", "swift.com", "twitter.com"},
-			Additions: []string{},
-			Deletions: []string{},
+			Additions: []string{"cook.ie", "rand.om"},
+			Deletions: []string{"twitter.com"},
+		}),
+	},
+	{
+		entry: New(&Config{
+			Cloud:     []string{"golang.org", "swift.com", "twitter.com"},
+			Additions: []string{"nytimes.com"},
+			Deletions: []string{"twitter.com", "swift.com", "golang.org", "anoth.er"},
 		}),
 	},
 }
@@ -38,18 +41,30 @@ func testConfig(entries []string) *Config {
 	}
 }
 
+func TestDiffOne(t *testing.T) {
+	proxiedSites := mockProxiedSites[0].entry
+	newProxiedSites := mockProxiedSites[1].entry
+	cfg := proxiedSites.Diff(newProxiedSites)
+	assert.Equal(t, 1, len(cfg.Deletions), "Deletions weren't the same size")
+	assert.Equal(t, 2, len(cfg.Additions), "Additions weren't the same size")
+}
+
+func TestDiffTwo(t *testing.T) {
+	proxiedSites := mockProxiedSites[1].entry
+	newProxiedSites := mockProxiedSites[2].entry
+	cfg := newProxiedSites.Diff(proxiedSites)
+	assert.Equal(t, 0, len(cfg.Deletions), "Deletions weren't the same size")
+	assert.Equal(t, 2, len(cfg.Additions), "Additions weren't the same size")
+}
+
 func TestPacFileUpdated(t *testing.T) {
-	PacFilePath = "proxy_on.pac"
-	PacTmpl = "templates/proxy_on.pac.template"
 
 	for _, mock := range mockProxiedSites {
-		SetPacFile(PacFilePath)
 		mockWl := mock.entry
 		exists, _ := util.FileExists("proxy_on.pac")
 		assert.Equal(t, exists, true, "proxy pac file could not be created")
 		time.Sleep(1000 * time.Millisecond)
 		wl := ParsePacFile()
-		assert.Equal(t, wl.GetEntries(), mockWl.GetEntries(), "Test domains are not equal!")
-
+		assert.Equal(t, wl.GetEntries, mockWl.GetEntries, "Domains weren't equal!")
 	}
 }
