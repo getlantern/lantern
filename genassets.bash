4dae8f580d69390ed62dfaecae20e5969d813f23
Ox Cart <ox.to.a.cart@gmail.com>
2015-02-20 10:00:56 -0600

    Moved ui to lantern-ui to facilitate pushing with gost

--- genassets.bash
+++ genassets.bash
@@ -6,4 +6,4 @@ go install github.com/getlantern/tarfs/tarfs
 dest="src/github.com/getlantern/flashlight/ui/resources.go"
 echo "// +build prod" > $dest
 echo " " >> $dest
-tarfs -pkg ui src/github.com/getlantern/ui/app >> $dest 
+tarfs -pkg ui src/github.com/getlantern/lantern-ui/app >> $dest 
