58c7a1e54e8427b81d5f09444cd4a2cf3114f8fb
Ox Cart <ox.to.a.cart@gmail.com>
2015-02-20 10:09:46 -0600

    Removed lantern-ui

--- src/github.com/getlantern/lantern-ui/scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BASE_DIR=`dirname $0`
-
-echo ""
-echo "Starting Karma Server (http://karma-runner.github.io)"
-echo "-------------------------------------------------------------------"
-
-karma start $BASE_DIR/../config/karma.conf.js $*
