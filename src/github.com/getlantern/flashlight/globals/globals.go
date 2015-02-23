885c814225b5895c047beeae5c9ab47c5a46d732
Ox Cart <ox.to.a.cart@gmail.com>
2015-02-23 17:06:12 -0600

    Fixed unit test

--- src/github.com/getlantern/flashlight/globals/globals.go
+++ src/github.com/getlantern/flashlight/globals/globals.go
@@ -21,7 +21,11 @@ func SetLocation(loc *geolookup.City) {
 }
 
 func GetLocation() *geolookup.City {
-	return location.Load().(*geolookup.City)
+	l := location.Load()
+	if l == nil {
+		return nil
+	}
+	return l.(*geolookup.City)
 }
 
 func GetCountry() string {
