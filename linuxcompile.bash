44ffee7bb9d2a321286d046d9e4f6a0b40696981
Ox Cart <ox.to.a.cart@gmail.com>
2015-02-23 20:31:14 -0600

    linux_compile fixes

--- linuxcompile.bash
+++ linuxcompile.bash
@@ -8,4 +8,4 @@ VERSION="`git describe --abbrev=0 --tags --exact-match || git rev-parse --short 
 BUILD_DATE="`date -u +%Y%m%d%.%H%M%S`"
 echo "Building flashlight version $VERSION ($BUILD_DATE)"
 # Compile for Linux
-CGO_ENABLED=1 go build -o lantern_linux -tags="prod" -ldflags="-w -X main.version $VERSION -X main.buildDate $BUILD_DATE" github.com/getlantern/flashlight
+go build -o lantern_linux -tags="prod" -ldflags="-linkmode internal -w -X main.version $VERSION -X main.buildDate $BUILD_DATE" github.com/getlantern/flashlight
