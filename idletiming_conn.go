ca21ed5d3486f59277e9b5b49a62d5f7f936e53e
Ox Cart <ox.to.a.cart@gmail.com>
2015-03-11 12:50:49 -0500

    Squashed 'src/github.com/getlantern/idletiming/' changes from b614bc6..0e09328
    0e09328 Updated travis builds to 1.4.1
    d70a5cb Merge branch 'devel' into tcpproxying
    88b2887 Merge branch 'devel' into 2127
    a2c5557 getlantern/lantern#2117 Cleaned up onIdle/onClose terminology
    cc82ee1 getlantern/lantern#2117 Comment update
    cdde516 getlantern/lantern#2117 More close cleanup
    13b9987 getlantern/lantern#2117 Ensuring that close callback is called when Close() was called
    52b34a3 getlantern/lantern#2117 Fixed Close method, added test and comment
    f1c23a9 Fixed nil pointer in idletiming.Listener close handling
    42ceb81 getlantern/lantern#2117 Shifted responsibility for closing connections from idletiming.Conn to client of idletiming.Conn
    04d460d getlantern/lantern#2117 idletiming.Conn now closes the underlying connection after idling, also cleaned up a few other items and the unit test
    0078356 Merge branch 'master' into devel
    5d73e72 getlantern/lantern#2121 Breaking out of loop when timeout occurs before deadline set for idle timeout
    9c35001 getlantern/lantern#2121 Synchronizing access to read and write deadlines, fetching them only once at the beginning of our loops
    01099ba getlantern/lantern#2121 Idletiming checks type of timeout error (read vs write)
    8c5507f getlantern/lantern#2121 Reduced CPU usage in waitforserver by increasing sleep time on each loop
    00f647d getlantern/lantern#2127 Fixed race condition in idletiming conn
    36e922a getlantern/lantern#2083 Switched travis builds to use golang.org/x/tools/cmd/cover
    0fb497e getlantern/lantern#2083 Switched travis builds to Go 1.4
    
    git-subtree-dir: src/github.com/getlantern/idletiming
    git-subtree-split: 0e09328c5a420afbe2cfdb94a107acdd3190a189

--- idletiming_conn.go
+++ idletiming_conn.go
@@ -8,30 +8,35 @@ import (
 	"time"
 )
 
+var (
+	epoch = time.Unix(0, 0)
+)
+
 // Conn creates a new net.Conn wrapping the given net.Conn that times out after
 // the specified period. Read and Write calls will timeout if they take longer
-// than the indicated
+// than the indicated idleTimeout.
 //
 // idleTimeout specifies how long to wait for inactivity before considering
 // connection idle.
 //
-// onClose is an optional function to call after the connection has been closed,
-// whether or not that was due to the connection idling.
-func Conn(conn net.Conn, idleTimeout time.Duration, onClose func()) *IdleTimingConn {
+// onIdle is a required function that's called if a connection idles.
+// idletiming.Conn does not close the underlying connection on idle, you have to
+// do that in your onIdle callback.
+func Conn(conn net.Conn, idleTimeout time.Duration, onIdle func()) *IdleTimingConn {
+	if onIdle == nil {
+		panic("onIdle is required")
+	}
+
 	c := &IdleTimingConn{
 		conn:             conn,
 		idleTimeout:      idleTimeout,
 		halfIdleTimeout:  time.Duration(idleTimeout.Nanoseconds() / 2),
-		activeCh:         make(chan bool, 10),
-		closedCh:         make(chan bool, 10),
+		activeCh:         make(chan bool, 1),
+		closedCh:         make(chan bool, 1),
 		lastActivityTime: int64(time.Now().UnixNano()),
 	}
 
 	go func() {
-		if onClose != nil {
-			defer onClose()
-		}
-
 		timer := time.NewTimer(idleTimeout)
 		defer timer.Stop()
 		for {
@@ -42,10 +47,9 @@ func Conn(conn net.Conn, idleTimeout time.Duration, onClose func()) *IdleTimingC
 				atomic.StoreInt64(&c.lastActivityTime, time.Now().UnixNano())
 				continue
 			case <-timer.C:
-				//c.Close()
+				onIdle()
 				return
 			case <-c.closedCh:
-				c.Close()
 				return
 			}
 		}
@@ -60,8 +64,8 @@ type IdleTimingConn struct {
 	conn             net.Conn
 	idleTimeout      time.Duration
 	halfIdleTimeout  time.Duration
-	readDeadline     *time.Time
-	writeDeadline    *time.Time
+	readDeadline     int64
+	writeDeadline    int64
 	activeCh         chan bool
 	closedCh         chan bool
 	lastActivityTime int64
@@ -76,20 +80,22 @@ func (c *IdleTimingConn) TimesOutIn() time.Duration {
 // TimesOutAt returns the time at which this connection will time out, assuming
 // there is no further activity
 func (c *IdleTimingConn) TimesOutAt() time.Time {
-	return time.Unix(0, c.lastActivityTime).Add(c.idleTimeout)
+	return time.Unix(0, atomic.LoadInt64(&c.lastActivityTime)).Add(c.idleTimeout)
 }
 
 // Read implements the method from io.Reader
 func (c *IdleTimingConn) Read(b []byte) (int, error) {
 	totalN := 0
+	readDeadline := time.Unix(0, atomic.LoadInt64(&c.readDeadline))
+
 	// Continually read while we can, always setting a deadline that's less than
 	// our idleTimeout so that we can update our active status before we hit the
 	// idleTimeout.
 	for {
 		maxDeadline := time.Now().Add(c.halfIdleTimeout)
-		if c.readDeadline != nil && !maxDeadline.Before(*c.readDeadline) {
+		if readDeadline != epoch && !maxDeadline.Before(readDeadline) {
 			// Caller's deadline is before ours, use it
-			c.conn.SetReadDeadline(*c.readDeadline)
+			c.conn.SetReadDeadline(readDeadline)
 			n, err := c.conn.Read(b)
 			c.markActive(n)
 			totalN = totalN + n
@@ -100,12 +106,13 @@ func (c *IdleTimingConn) Read(b []byte) (int, error) {
 			n, err := c.conn.Read(b)
 			c.markActive(n)
 			totalN = totalN + n
-			timedOut := isTimeout(err)
-			if timedOut {
-				// Ignore timeouts when using deadline based on IdleTimeout
+			hitMaxDeadline := isTimeout(err) && !time.Now().Before(maxDeadline)
+			if hitMaxDeadline {
+				// Ignore timeouts when encountering deadline based on
+				// IdleTimeout
 				err = nil
 			}
-			if n == 0 || !timedOut {
+			if n == 0 || !hitMaxDeadline {
 				return totalN, err
 			}
 			b = b[n:]
@@ -116,14 +123,16 @@ func (c *IdleTimingConn) Read(b []byte) (int, error) {
 // Write implements the method from io.Reader
 func (c *IdleTimingConn) Write(b []byte) (int, error) {
 	totalN := 0
+	writeDeadline := time.Unix(0, atomic.LoadInt64(&c.writeDeadline))
+
 	// Continually write while we can, always setting a deadline that's less
 	// than our idleTimeout so that we can update our active status before we
 	// hit the idleTimeout.
 	for {
 		maxDeadline := time.Now().Add(c.halfIdleTimeout)
-		if c.writeDeadline != nil && !maxDeadline.Before(*c.writeDeadline) {
+		if writeDeadline != epoch && !maxDeadline.Before(writeDeadline) {
 			// Caller's deadline is before ours, use it
-			c.conn.SetWriteDeadline(*c.writeDeadline)
+			c.conn.SetWriteDeadline(writeDeadline)
 			n, err := c.conn.Write(b)
 			c.markActive(n)
 			totalN = totalN + n
@@ -134,12 +143,13 @@ func (c *IdleTimingConn) Write(b []byte) (int, error) {
 			n, err := c.conn.Write(b)
 			c.markActive(n)
 			totalN = totalN + n
-			timedOut := isTimeout(err)
-			if timedOut {
-				// Ignore timeouts when using deadline based on IdleTimeout
+			hitMaxDeadline := isTimeout(err) && !time.Now().Before(maxDeadline)
+			if hitMaxDeadline {
+				// Ignore timeouts when encountering deadline based on
+				// IdleTimeout
 				err = nil
 			}
-			if n == 0 || !timedOut {
+			if n == 0 || !hitMaxDeadline {
 				return totalN, err
 			}
 			b = b[n:]
@@ -147,8 +157,15 @@ func (c *IdleTimingConn) Write(b []byte) (int, error) {
 	}
 }
 
+// Close this IdleTimingConn. This will close the underlying net.Conn as well,
+// returning the error from calling its Close method.
 func (c *IdleTimingConn) Close() error {
-	c.closedCh <- true
+	select {
+	case c.closedCh <- true:
+		// close accepted
+	default:
+		// already closing, ignore
+	}
 	return c.conn.Close()
 }
 
@@ -167,12 +184,12 @@ func (c *IdleTimingConn) SetDeadline(t time.Time) error {
 }
 
 func (c *IdleTimingConn) SetReadDeadline(t time.Time) error {
-	c.readDeadline = &t
+	atomic.StoreInt64(&c.readDeadline, t.UnixNano())
 	return nil
 }
 
 func (c *IdleTimingConn) SetWriteDeadline(t time.Time) error {
-	c.writeDeadline = &t
+	atomic.StoreInt64(&c.writeDeadline, t.UnixNano())
 	return nil
 }
 
