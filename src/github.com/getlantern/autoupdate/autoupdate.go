31e8f455b111d6c9103120e2dd5b33616ae927dc
Ox Cart <ox.to.a.cart@gmail.com>
2015-03-20 09:18:55 -0500

    Code review updates

--- src/github.com/getlantern/autoupdate/autoupdate.go
+++ src/github.com/getlantern/autoupdate/autoupdate.go
@@ -3,165 +3,130 @@
 package autoupdate
 
 import (
+	"fmt"
 	"net/http"
-	"strings"
 	"time"
 
-	"github.com/getlantern/flashlight/util"
+	"github.com/blang/semver"
 	"github.com/getlantern/go-update"
 	"github.com/getlantern/go-update/check"
 	"github.com/getlantern/golog"
 )
 
-var log = golog.LoggerFor("autoupdate")
-
-// Making sure AutoUpdate and Patch satisfy AutoUpdater and Patcher.
 var (
-	_ = AutoUpdater(&AutoUpdate{})
-	_ = Patcher(&Patch{})
-)
-
-var (
-	// How much time should we wait between update attempts?
-	sleepTime = time.Hour * 4
+	log                  = golog.LoggerFor("autoupdate")
+	defaultCheckInterval = time.Hour * 4
+	defaultHTTPClient    = &http.Client{}
 )
 
 type Config struct {
-	URL       string
+	// CurrentVersion: the current version of the program, must be of the form
+	// vX.Y.Z
+	CurrentVersion string
+
+	version semver.Version
+
+	// URL: the url at which to check for updates
+	URL string
+
+	// PublicKey: the public key against which to check the signature of any
+	// received updates.
 	PublicKey []byte
+
+	// CheckInterval: the interval at which to check for updates, defaults to
+	// 4 hours.
+	CheckInterval time.Duration
+
+	// HTTPClient: (optional), an http.Client to use when checking for updates
+	HTTPClient *http.Client
 }
 
-// SetProxy sets the proxy to use.
-func SetProxy(proxyAddr string) {
+// Apply applies the next available update whenever it is available, blocking
+// until the next update has been applied. If ApplyNext returns without an
+// error, that means that the current program's executable has been udpated in
+// place and you may want to restart. If ApplyNext returns an error, that means
+// that an unrecoverable error has occurred and we can't continue checking for
+// updates.
+func ApplyNext(cfg *Config) error {
+	// Parse the semantic version
 	var err error
+	cfg.version, err = semver.New(cfg.CurrentVersion)
+	if err != nil {
+		return fmt.Errorf("Bad version string: %v", err)
+	}
+	if cfg.CheckInterval == 0 {
+		cfg.CheckInterval = defaultCheckInterval
+		log.Debugf("Defaulted CheckInterval to %v", cfg.CheckInterval)
+	}
+	if cfg.HTTPClient == nil {
+		cfg.HTTPClient = defaultHTTPClient
+		log.Debug("Defaulted HTTPClient")
+	}
+	update.HTTPClient = cfg.HTTPClient
 
-	if proxyAddr != "" {
-		// Create a HTTP proxy and pass it to the update package.
-		if update.HTTPClient, err = util.HTTPClient("", proxyAddr); err != nil {
-			log.Errorf("Could not use proxy: %q\n", err)
+	return cfg.loop()
+}
+
+func (cfg *Config) loop() error {
+	for {
+		res, err := cfg.check()
+
+		if err != nil {
+			log.Errorf("Problem checking for update: %v", err)
+		} else {
+			if res == nil {
+				log.Debug("No update available")
+			} else if cfg.isNewerVersion(res.Version) {
+				log.Debugf("autoupdate: Attempting to update to %s.", res.Version)
+				err, errRecover := res.Update()
+				if errRecover != nil {
+					// This should never happen, if this ever happens it means bad news such as
+					// a missing executable file.
+					return fmt.Errorf("Failed to recover from failed update attempt: %v\n", errRecover)
+				}
+				if err == nil {
+					log.Debugf("autoupdate: Patching succeeded!")
+					return nil
+				}
+				log.Errorf("autoupdate: Patching failed: %q\n", err)
+			} else {
+				log.Debug("autoupdate: Already up to date.")
+			}
 		}
-	} else {
-		update.HTTPClient = &http.Client{}
+
+		time.Sleep(cfg.CheckInterval)
 	}
 }
 
-// AutoUpdate satisfies AutoUpdater and can be used for other programs to
-// configure automatic updates.
-type AutoUpdate struct {
-	*Config
-	v string
-	// When a patch has been applied, the patch's version will be sent to
-	// UpdatedTo.
-	UpdatedTo chan string
-}
-
-// New creates an AutoUpdate struct based on the given *Config.
-func New(cfg *Config) *AutoUpdate {
-	a := &AutoUpdate{
-		UpdatedTo: make(chan string),
-		Config:    cfg,
+func (cfg *Config) isNewerVersion(newer string) bool {
+	nv, err := semver.New(newer)
+	if err != nil {
+		log.Errorf("Bad version string on update: %v", err)
+		return false
 	}
-	return a
-}
-
-// SetVersion sets the version of the process' executable file.
-func (a *AutoUpdate) SetVersion(v string) {
-	if !strings.HasPrefix(v, "v") {
-		// Panicking because versions must begin with "v".
-		panic(`autoupdate: Versions must begin with a "v".`)
-	}
-	if !isVersionTag(v) {
-		panic(`autoupdate: Versions must be in the form vX.Y.Z.`)
-	}
-	a.v = v
-}
-
-// Version returns the internal version value passed to SetVersion(). If
-// SetVersion() has not been called yet, a negative value will be returned
-// instead.
-func (a *AutoUpdate) Version() string {
-	return a.v
+	return nv.GT(cfg.version)
 }
 
 // check uses go-update to look for updates.
-func (a *AutoUpdate) check() (res *check.Result, err error) {
+func (cfg *Config) check() (res *check.Result, err error) {
 	var up *update.Update
 
 	param := check.Params{
-		AppVersion: a.Version(),
+		AppVersion: cfg.CurrentVersion,
 	}
 
 	up = update.New().ApplyPatch(update.PATCHTYPE_BSDIFF)
 
-	if _, err = up.VerifySignatureWithPEM(a.PublicKey); err != nil {
-		return nil, err
+	if _, err = up.VerifySignatureWithPEM(cfg.PublicKey); err != nil {
+		return nil, fmt.Errorf("Problem verifying signature of update: %v", err)
 	}
 
-	if res, err = param.CheckForUpdate(a.URL, up); err != nil {
+	if res, err = param.CheckForUpdate(cfg.URL, up); err != nil {
 		if err == check.NoUpdateAvailable {
 			return nil, nil
 		}
-		return nil, err
+		return nil, fmt.Errorf("Problem fetching update: %v", err)
 	}
 
 	return res, nil
-}
-
-// Query checks if a new version is available and returns a Patcher.
-func (a *AutoUpdate) Query() (Patcher, error) {
-	var res *check.Result
-	var err error
-
-	if res, err = a.check(); err != nil {
-		return nil, err
-	}
-
-	if res == nil {
-		// No new version is available.
-		return &Patch{}, nil
-	}
-
-	return &Patch{res: res, v: res.Version}, nil
-}
-
-func (a *AutoUpdate) loop() {
-	log.Debug("Starting to watch for updates")
-	for {
-		patch, err := a.Query()
-
-		if err == nil {
-			log.Debugf("Old: %v  New: %v", a.Version(), patch.Version())
-			if VersionCompare(a.Version(), patch.Version()) == Higher {
-				log.Debugf("autoupdate: Attempting to update to %s.", patch.Version())
-
-				err = patch.Apply()
-
-				if err == nil {
-					log.Debugf("autoupdate: Patching succeeded!")
-					// Updating version.
-					a.UpdatedTo <- patch.Version()
-					a.SetVersion(patch.Version())
-				} else {
-					log.Errorf("autoupdate: Patching failed: %q\n", err)
-				}
-
-			} else {
-				log.Debug("autoupdate: Already up to date.")
-			}
-		} else {
-			log.Debugf("autoupdate: Could not reach update server: %q\n", err)
-		}
-
-		time.Sleep(sleepTime)
-	}
-}
-
-// Watch spawns a goroutine that will apply updates whenever they're available.
-func (a *AutoUpdate) Watch() {
-	if a.v == "" {
-		// Panicking because Watch is useless without the ability to compare
-		// versions.
-		panic(`autoupdate: You must set the executable version in order to watch for updates!`)
-	}
-	go a.loop()
 }
