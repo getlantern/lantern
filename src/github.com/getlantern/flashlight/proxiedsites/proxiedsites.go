9c5ca992d93a723eceffc7e56e0ee61287a4816a
fffw <fffw@getlantern.org>
2015-04-09 16:17:03 +0800

    fix AddToWl call in flashlight

--- src/github.com/getlantern/flashlight/proxiedsites/proxiedsites.go
+++ src/github.com/getlantern/flashlight/proxiedsites/proxiedsites.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"sync"
-	"time"
 
 	"github.com/getlantern/detour"
 	"github.com/getlantern/golog"
@@ -60,7 +59,7 @@ func updateDetour(delta *proxiedsites.Delta) {
 	// TODO: subscribe changes of geolookup and set country accordingly
 	// safe to hardcode here as IR has all detection rules
 	detour.SetCountry("IR")
-	curWl := detour.DumpWhitelist()
+
 	// for simplicity, detour matches whitelist using host:port string
 	// so we add ports to each proxiedsites
 	for _, v := range delta.Deletions {
@@ -68,8 +67,8 @@ func updateDetour(delta *proxiedsites.Delta) {
 		detour.RemoveFromWl(v + ":443")
 	}
 	for _, v := range delta.Additions {
-		detour.AddToWl(v + ":80")
-		detour.AddToWl(v + ":443")
+		detour.AddToWl(v+":80", true)
+		detour.AddToWl(v+":443", true)
 	}
 }
 
