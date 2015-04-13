ffde75c48e335b23437027f151d758a49277d95e
Todd Blose <todd@lana.io>
2015-04-13 13:15:55 -0400

    remove linux launcher for now

--- src/github.com/getlantern/launcher/launcher_linux.go
+++ src/github.com/getlantern/launcher/launcher_linux.go
@@ -1,41 +1,6 @@
 // Package launcher configures Lantern to run on system start
 package launcher
 
-import (
-	"github.com/getlantern/golog"
-	"github.com/takama/daemon"
-)
-
-var (
-	log = golog.LoggerFor("launcher")
-)
-
 func CreateLaunchFile(autoLaunch bool) {
 
-	service, err := daemon.New("name", "lantern")
-	if err != nil {
-		log.Errorf("Could not create new daemon: %q", err)
-		return
-	}
-
-	if autoLaunch {
-		status, err := service.Status()
-		if err == nil {
-			log.Debugf("Service already installed")
-			return
-		}
-		status, err = service.Install()
-		if err != nil {
-			log.Errorf("Could not install service: %q", err)
-			return
-		}
-		log.Debugf("Successfully installed new service: %s", status)
-	} else {
-		status, err := service.Remove()
-		if err != nil {
-			log.Errorf("Could not remove service: %q", err)
-			return
-		}
-		log.Debugf("Successfully removed Lantern service: %s", status)
-	}
 }
