8456de6a8957377cb24b550c711433a126c8ac60
José Carlos Nieto <jose.carlos@menteslibres.net>
2015-04-20 13:50:25 -0500

    Adding some comments on the ServeHTTP process.

--- src/github.com/getlantern/flashlight/client/handler.go
+++ src/github.com/getlantern/flashlight/client/handler.go
@@ -34,43 +34,51 @@ func (client *Client) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 // connetion and starts piping the data over a new net.Conn obtained from the
 // given dial function.
 func (client *Client) intercept(resp http.ResponseWriter, req *http.Request) {
+	var err error
+
+	// intercept can only by used for CONNECT requests.
 	if req.Method != httpConnectMethod {
 		panic("Intercept used for non-CONNECT request!")
 	}
 
-	// Hijack underlying connection
-	clientConn, _, err := resp.(http.Hijacker).Hijack()
-	if err != nil {
+	// Hijacking underlying connection.
+	var clientConn net.Conn
+	if clientConn, _, err = resp.(http.Hijacker).Hijack(); err != nil {
 		respondBadGateway(resp, fmt.Sprintf("Unable to hijack connection: %s", err))
 		return
 	}
 	defer clientConn.Close()
 
-	host, port, err := net.SplitHostPort(req.Host)
+	// Getting destination host and port.
+	var host, port string
 
-	if err != nil {
+	if host, port, err = net.SplitHostPort(req.Host); err != nil {
 		log.Tracef("net.SplitHostPort: %q", err)
 	}
 
+	// If no port is given, assuming it's 443 for HTTPs.
 	if port == "" {
 		port = "443"
 	}
 
+	// Creating a network address.
 	addr := host + ":" + port
 
-	// Establish outbound connection
+	// Establishing outbound connection with the given address.
 	d := func(network, addr string) (net.Conn, error) {
 		return client.getBalancer().DialQOS("tcp", addr, client.targetQOS(req))
 	}
 
-	connOut, err := detour.Dialer(d)("tcp", addr)
-	if err != nil {
+	// The actual dialer must pass through detour.
+	var connOut net.Conn
+	if connOut, err = detour.Dialer(d)("tcp", addr); err != nil {
 		respondBadGateway(clientConn, fmt.Sprintf("Unable to handle CONNECT request: %s", err))
 		return
 	}
+
 	defer connOut.Close()
 
-	// Pipe data
+	// Piping data between the client and the proxy.
 	pipeData(clientConn, connOut, req)
 }
 
@@ -78,6 +86,7 @@ func (client *Client) intercept(resp http.ResponseWriter, req *http.Request) {
 // header if available, else returns MinQOS.
 func (client *Client) targetQOS(req *http.Request) int {
 	requestedQOS := req.Header.Get(httpXFlashlightQOS)
+
 	if requestedQOS != "" {
 		rqos, err := strconv.Atoi(requestedQOS)
 		if err == nil {
@@ -91,38 +100,39 @@ func (client *Client) targetQOS(req *http.Request) int {
 // pipeData pipes data between the client and proxy connections.  It's also
 // responsible for responding to the initial CONNECT request with a 200 OK.
 func pipeData(clientConn net.Conn, connOut net.Conn, req *http.Request) {
-	// Start piping to proxy
+	// Start piping from client to proxy
 	go io.Copy(connOut, clientConn)
 
 	// Respond OK
-	err := respondOK(clientConn, req)
-	if err != nil {
+	if err := respondOK(clientConn, req); err != nil {
 		log.Errorf("Unable to respond OK: %s", err)
 		return
 	}
 
-	// Then start coyping from out to client
+	// Then start coyping from proxy to client
 	io.Copy(clientConn, connOut)
 }
 
 func respondOK(writer io.Writer, req *http.Request) error {
 	defer req.Body.Close()
+
 	resp := &http.Response{
-		StatusCode: 200,
+		StatusCode: http.StatusOK,
 		ProtoMajor: 1,
 		ProtoMinor: 1,
 	}
+
 	return resp.Write(writer)
 }
 
-func respondBadGateway(w io.Writer, msg string) error {
+func respondBadGateway(w io.Writer, msg string) (err error) {
 	log.Debugf("Responding BadGateway: %v", msg)
 	resp := &http.Response{
-		StatusCode: 502,
+		StatusCode: http.StatusBadGateway,
 		ProtoMajor: 1,
 		ProtoMinor: 1,
 	}
-	err := resp.Write(w)
+	err = resp.Write(w)
 	if err == nil {
 		_, err = w.Write([]byte(msg))
 	}
