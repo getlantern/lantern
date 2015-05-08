ac83a4413c5f337978e786cd58e22e2a5b435429
Alan Hoyte <aranhoide@gmail.com>
2015-05-08 16:45:33 +0200

    take locales as arguments, rather than hardcode them

--- src/github.com/getlantern/flashlight/genconfig/generate-fallbacks-json.bash
+++ src/github.com/getlantern/flashlight/genconfig/generate-fallbacks-json.bash
@@ -5,11 +5,10 @@ function die() {
   exit 1
 }
 
+[ -n "$1" ] || die "Usage: '$0 country-code country-code' ...'    (e.g. '$0 nl jp')"
 [ -n "${production_cloudmaster_IP}" ] || die "You need to set the production_cloudmaster_IP environment variable."
 
-locales=("nl" "jp")
-
-for locale in "${locales[@]}"
+for locale
 do
     echo "Generating list for ${locale} ..."
     ssh $production_cloudmaster_IP "sudo regenerate-fallbacks-list ${locale}" > ${locale}.fallbacks.json || die "Error generating fallbacks list.  Is your id_rsa.pub uploaded to the cloudmaster?"
