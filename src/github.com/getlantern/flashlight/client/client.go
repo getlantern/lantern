025a337b2d968c8fa0293fec89d231fb59ab8d60
fffw <fffw@getlantern.org>
2015-05-15 22:29:31 +0800

    add ProxyAll client config option to yaml

--- src/github.com/getlantern/flashlight/client/client.go
+++ src/github.com/getlantern/flashlight/client/client.go
@@ -33,6 +33,9 @@ type Client struct {
 	// WriteTimeout: (optional) timeout for write ops
 	WriteTimeout time.Duration
 
+	// ProxyAll: (optional)  poxy all sites regardless of being blocked or not
+	ProxyAll bool
+
 	// MinQOS: (optional) the minimum QOS to require from proxies.
 	MinQOS int
 
@@ -99,6 +102,8 @@ func (client *Client) Configure(cfg *ClientConfig) fronted.Dialer {
 
 	log.Debugf("Requiring minimum QOS of %d", cfg.MinQOS)
 	client.MinQOS = cfg.MinQOS
+	log.Debugf("Proxy all traffic or not: %v", cfg.ProxyAll)
+	client.ProxyAll = cfg.ProxyAll
 
 	var bal *balancer.Balancer
 	bal, client.hqfd = client.initBalancer(cfg)
