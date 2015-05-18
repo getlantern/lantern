cec975219938df5a42613cb86a4269ab0fb4bb5e
Myles Horton <myles@getlantern.org>
2015-05-18 12:39:35 -0700

    Changed to record analytics only after the local server is definitely up and running #2540

--- src/github.com/getlantern/lantern-android/client/client.go
+++ src/github.com/getlantern/lantern-android/client/client.go
@@ -30,6 +30,7 @@ type MobileClient struct {
 	client.Client
 	closed  chan bool
 	fronter *http.Client
+	appName string
 }
 
 // init attempts to setup client configuration.
@@ -53,35 +54,11 @@ func NewClient(addr, appName string) *MobileClient {
 
 	hqfd := client.Configure(clientConfig.Client)
 
-	// store GA session event
-	sessionPayload := &analytics.Payload{
-		HitType: analytics.EventType,
-		Event: &analytics.Event{
-			Category: "Session",
-			Action:   "Start",
-			Label:    runtime.GOOS,
-		},
-	}
-
-	// attach our app-specific analytics tracking code
-	// to session info
-	if appName != "" {
-		if trackingId, ok := trackingCodes[appName]; ok {
-			sessionPayload.TrackingId = trackingId
-		}
-	}
-
-	httpClient, er := util.HTTPClient(cfg.CloudConfigCA, cfg.Addr)
-	if er != nil {
-		log.Errorf("Could not create HTTP client %v", er)
-	} else {
-		analytics.SessionEvent(httpClient, sessionPayload)
-	}
-
 	return &MobileClient{
 		Client:  client,
 		closed:  make(chan bool),
 		fronter: hqfd.NewDirectDomainFronter(),
+		appName: appName,
 	}
 }
 
@@ -94,6 +71,7 @@ func (client *MobileClient) ServeHTTP() {
 	go func() {
 		onListening := func() {
 			log.Printf("Now listening for connections...")
+			go client.recordAnalytics()
 		}
 		if err := client.ListenAndServe(onListening); err != nil {
 			// Error is not exported: https://golang.org/src/net/net.go#L284
@@ -103,6 +81,39 @@ func (client *MobileClient) ServeHTTP() {
 		}
 	}()
 	go client.pollConfiguration()
+}
+
+func (client *MobileClient) recordAnalytics() {
+
+	// store GA session event
+	sessionPayload := &analytics.Payload{
+		HitType: analytics.EventType,
+		Event: &analytics.Event{
+			Category: "Session",
+			Action:   "Start",
+			Label:    runtime.GOOS,
+		},
+	}
+
+	// attach our app-specific analytics tracking code
+	// to session info
+	if client.appName != "" {
+		if trackingId, ok := trackingCodes[client.appName]; ok {
+			sessionPayload.TrackingId = trackingId
+		}
+	}
+
+	// Report analytics, proxying through the local client. Note this
+	// is a little unorthodox by Lantern standards because it doesn't
+	// pin the certificate of the cloud.yaml root CA, instead relying
+	// on the go defaults.
+	httpClient, er := util.HTTPClient("", client.Client.Addr)
+	if er != nil {
+		log.Fatalf("Could not create HTTP client %v", er)
+	} else {
+		analytics.SessionEvent(httpClient, sessionPayload)
+	}
+
 }
 
 // updateConfig attempts to pull a configuration file from the network using
