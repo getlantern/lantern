48ea85a7a4b270b445b68a7a1763e7ae47a1eae4
Ulysses Aalto <uaalto@getlantern.org>
2015-06-09 18:35:04 +0200

    Multicast local discovery: initial commit

---
+++ src/github.com/getlantern/multicast/multicast.go
@@ -0,0 +1,75 @@
+// Use SSM multicast to discover other processes on the same local network
+// See RFC-3569 for SSM description
+// See RFC-5771 for IANA IPv4 Multicast address assignments
+
+package multicast
+
+import (
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+const (
+	// Addresses for SSM multicasting are in the space 232/8
+	// See https://www.iana.org/assignments/multicast-addresses/multicast-addresses.xhtml#multicast-addresses-10
+	multicastAddress = "232.77.77.77:9864"
+)
+
+type Multicast struct {
+        Conn        *net.UDPConn
+        Addr        *net.UDPAddr
+	Period      int // multicast period (in secs, 10 by default)
+}
+
+func JoinMulticast() (*Multicast) {
+        udpAddr, e := net.ResolveUDPAddr("udp4", multicastAddress)
+        if e != nil {
+		log.Fatal(e)
+                return nil
+        }
+
+        c, e := net.ListenMulticastUDP("udp4", nil, udpAddr)
+        if e != nil {
+                return nil
+        }
+        return &Multicast{c, udpAddr, 10}
+}
+
+func (mc *Multicast) StartMulticast() {
+	// Periodically announce ourselves to the network
+	go mc.sendHellos()
+}
+
+func (mc *Multicast) LeaveMulticast() error {
+        return mc.Conn.Close()
+}
+
+func (mc *Multicast) write(b []byte) (int, error) {
+        return mc.Conn.WriteTo(b, mc.Addr)
+}
+
+func (mc *Multicast) read(b []byte) (int, error) {
+        return mc.Conn.Read(b)
+}
+
+func (mc *Multicast) sendHellos () {
+	c := time.Tick(time.Duration(mc.Period) * time.Second)
+	for range c {
+		host, _ := os.Hostname()
+		addrs, _ := net.LookupIP(host)
+		mc.write( addressesToMsg(addrs) )
+	}
+}
+
+func addressesToMsg(addrs []net.IP) []byte {
+	msg := "IPv4: "
+	for _, addr := range addrs {
+		if ipv4 := addr.To4(); ipv4 != nil {
+			msg += ipv4.String()
+			msg += " |"
+		}
+	}
+	return []byte(msg)
+}
---
+++ src/github.com/getlantern/multicast/multicast_test.go
@@ -0,0 +1,154 @@
+package multicast
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"syscall"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestMulticast(t *testing.T) {
+	t.SkipNow()
+
+        mc1 := JoinMulticast()
+        if mc1 == nil {
+                t.Fatal()
+        } else {
+		log.Println("Joined and listening to multicast IP", mc1.Addr.IP, "on port", mc1.Addr.Port)
+	}
+
+	// Enable Multicast looping for testing
+	f, err := mc1.Conn.File()
+	err = syscall.SetsockoptInt(int(f.Fd()), syscall.IPPROTO_IP, syscall.IP_MULTICAST_LOOP, 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	// Sender node
+        go func(mc *Multicast) {
+		defer wg.Done()
+
+		// Give some time to the other goroutines to build up
+		time.Sleep(time.Millisecond * 200)
+
+                msg := "Multicast Hello World!"
+                n, e := mc.write(([]byte)(msg))
+                if e != nil {
+                        t.Fatal(e)
+                }
+		log.Printf("--> Sent %d bytes: %s\n", n, msg)
+                e = mc.LeaveMulticast()
+
+                if e != nil {
+                        t.Fatal(e)
+                } else {
+			log.Println("Leaving multicast IP", mc.Addr.IP, "on port", mc.Addr.Port)
+		}
+        }(mc1)
+
+	nNodes := 9
+	for i := 0; i < nNodes; i++ {
+		wg.Add(1)
+		go receiverNode(t, &wg, i+1)
+	}
+	wg.Wait()
+}
+
+func receiverNode(t *testing.T, wg *sync.WaitGroup, id int) {
+	defer wg.Done()
+
+        mc := JoinMulticast()
+        if mc == nil {
+                t.Fatal()
+        } else {
+		log.Println("Joined and listening to multicast IP", mc.Addr.IP, "on port", mc.Addr.Port)
+	}
+
+        b := make([]byte, 1000)
+        n, e := mc.read(b)
+        if e != nil {
+                t.Fatal(e)
+        }
+        if n > 0 {
+                log.Println("Node", id , "<-- Received", n, "bytes:", string(b))
+        } else {
+                log.Println("Received 0 bytes")
+        }
+
+        e = mc.LeaveMulticast()
+        if e != nil {
+                t.Fatal(e)
+        } else {
+		log.Println("Node", id, "leaving multicast IP", mc.Addr.IP, "on port", mc.Addr.Port)
+	}
+}
+
+func TestMulticastAnnouncing(t *testing.T) {
+	mc1 := JoinMulticast()
+        if mc1 == nil {
+                t.Fatal()
+        }
+
+	// Enable Multicast looping for testing
+	f, err := mc1.Conn.File()
+	err = syscall.SetsockoptInt(int(f.Fd()), syscall.IPPROTO_IP, syscall.IP_MULTICAST_LOOP, 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	mc1.Period = 1
+	mc1.StartMulticast()
+	/*
+	go func(mc *Multicast) {
+		// Give some time to the other goroutines to build up
+		time.Sleep(time.Millisecond * 200)
+
+                n, e := mc.write(([]byte)(msg))
+                if e != nil {
+                        t.Fatal(e)
+                }
+		log.Printf("--> Sent %d bytes: %s\n", n, msg)
+                e = mc.LeaveMulticast()
+
+                if e != nil {
+                        t.Fatal(e)
+                }
+        }(mc1)
+*/
+
+        mc2 := JoinMulticast()
+        if mc2 == nil {
+                t.Fatal()
+        }
+
+        b := make([]byte, 1000)
+        n, e := mc2.read(b)
+        if e != nil {
+                t.Fatal(e)
+        }
+        if n > 0 {
+                log.Println("<-- Received", n, "bytes:", string(b[:n]))
+		host, _ := os.Hostname()
+		addrs, _ := net.LookupIP(host)
+		if string(addressesToMsg (addrs)) != string(b[:n]) {
+			// Print bytes, not string, to see if any padding occurred 
+			fmt.Printf("%x\n",string(addressesToMsg(addrs)))
+			fmt.Printf("%x\n",string(b[:n]))
+			t.Fatal("Multicast Hello message is incorrectly formatted")
+		}
+        } else {
+                log.Println("Received 0 bytes")
+        }
+
+        e = mc2.LeaveMulticast()
+        if e != nil {
+                t.Fatal(e)
+        }
+}
