1f2f96ee8086b2de16b7b13dae9f802f12ef62e3
Ulysses Aalto <uaalto@getlantern.org>
2015-07-28 15:58:36 +0200

    Restore rotator tests; Sort test packages.txt

--- src/github.com/getlantern/rotator/size_rotator_test.go
+++ src/github.com/getlantern/rotator/size_rotator_test.go
@@ -65,11 +65,11 @@ func TestSizeRotation(t *testing.T) {
 	rotator.WriteString("0123456789")
 	stat, _ = os.Lstat(path)
 	assert.NotNil(t, stat)
-	assert.Equal(t, stat.Size(), 10)
+	assert.EqualValues(t, stat.Size(), 10)
 
 	stat, _ = os.Lstat(path + ".1")
 	assert.NotNil(t, stat)
-	assert.Equal(t, stat.Size(), 10)
+	assert.EqualValues(t, stat.Size(), 10)
 
 }
 
@@ -89,11 +89,11 @@ func TestSizeAppendExist(t *testing.T) {
 
 	stat, _ := os.Lstat(path)
 	assert.NotNil(t, stat)
-	assert.Equal(t, 8, stat.Size())
+	assert.EqualValues(t, 8, stat.Size())
 
 	stat, _ = os.Lstat(path + ".1")
 	assert.NotNil(t, stat)
-	assert.Equal(t, 5, stat.Size())
+	assert.EqualValues(t, 5, stat.Size())
 
 }
 
@@ -117,18 +117,20 @@ func TestSizeMaxRotation(t *testing.T) {
 
 	stat, _ = os.Lstat(path + ".1")
 	assert.NotNil(t, stat)
-	assert.Equal(t, stat.Size(), 10)
+	assert.EqualValues(t, stat.Size(), 10)
 
 	stat, _ = os.Lstat(path + ".2")
 	assert.NotNil(t, stat)
-	assert.Equal(t, stat.Size(), 10)
+	assert.EqualValues(t, stat.Size(), 10)
 
 	stat, _ = os.Lstat(path + ".3")
 	assert.NotNil(t, stat)
-	assert.Equal(t, stat.Size(), 10)
+	assert.EqualValues(t, stat.Size(), 10)
 
-	// it should fail rotation
-	_, err := rotator.WriteString("0123456789")
-	assert.NotNil(t, err)
-	assert.Equal(t, err.Error(), "rotation count has been exceeded")
+	// It should overwrite the first log of the rotation
+	rotator.WriteString("ASDF")
+
+	stat, _ = os.Lstat(path)
+	assert.NotNil(t, stat)
+	assert.EqualValues(t, stat.Size(), 4)
 }
