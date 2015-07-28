356df48493961e70e37a732b2ba8eeccab553b6f
Ulysses Aalto <uaalto@getlantern.org>
2015-07-28 11:44:09 +0200

    Errcheck'ed github.com/getlantern/go-update

--- src/github.com/getlantern/go-update/update.go
+++ src/github.com/getlantern/go-update/update.go
@@ -280,7 +280,11 @@ func (u *Update) FromFile(path string) (err error, errRecover error) {
 	if err != nil {
 		return
 	}
-	defer fp.Close()
+	defer func() {
+		if err := fp.Close(); err != nil {
+			fmt.Errorf("Unable to close file: %v\n", err)
+		}
+	}()
 
 	// do the update
 	return u.FromStream(fp)
@@ -369,12 +373,18 @@ func (u *Update) FromStream(updateWith io.Reader) (err error, errRecover error) 
 	if err != nil {
 		return
 	}
-	defer fp.Close()
-	_, err = io.Copy(fp, bytes.NewReader(newBytes))
+	// We won't log this error, because it's always going to happen.
+	// TODO: Document why are we doing this second fp.Close() at all?
+	defer func() { _ = fp.Close() }()
+	if _, err = io.Copy(fp, bytes.NewReader(newBytes)); err != nil {
+		fmt.Errorf("Unable to copy data: %v\n", err)
+	}
 
 	// if we don't call fp.Close(), windows won't let us move the new executable
 	// because the file will still be "in use"
-	fp.Close()
+	if err := fp.Close(); err != nil {
+		fmt.Errorf("Unable to close file: %v\n", err)
+	}
 
 	// this is where we'll move the executable to so that we can swap in the updated replacement
 	oldPath := filepath.Join(updateDir, fmt.Sprintf(".%s.old", filename))
@@ -428,7 +438,9 @@ func (u *Update) CanUpdate() (err error) {
 	if err != nil {
 		return
 	}
-	fp.Close()
+	if err := fp.Close(); err != nil {
+		fmt.Errorf("Unable to close file: %v\n", err)
+	}
 
 	_ = os.Remove(newPath)
 	return
@@ -440,7 +452,11 @@ func applyPatch(patch io.Reader, updatePath string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer old.Close()
+	defer func() {
+		if err := old.Close(); err != nil {
+			fmt.Errorf("Unable to close file: %v\n", err)
+		}
+	}()
 
 	// apply the patch
 	applied := new(bytes.Buffer)
@@ -470,7 +486,11 @@ func ChecksumForFile(path string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer f.Close()
+	defer func() {
+		if err := f.Close(); err != nil {
+			fmt.Errorf("Unable to close file: %v\n", err)
+		}
+	}()
 
 	return ChecksumForReader(f)
 }
