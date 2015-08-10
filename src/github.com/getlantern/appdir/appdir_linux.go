925d7499e786fa30a1bbaf8976eebe4ba05dc71a
Ulysses Aalto <uaalto@getlantern.org>
2015-08-10 12:46:42 +0300

    Closes #2908

--- src/github.com/getlantern/appdir/appdir_linux.go
+++ src/github.com/getlantern/appdir/appdir_linux.go
@@ -4,14 +4,19 @@ package appdir
 import (
 	"fmt"
 	"path/filepath"
+	"runtime"
 	"strings"
 )
 
 func general(app string) string {
-	// It is more common on Linux to expect application related directories
-	// in all lowercase. The lantern wrapper also expects a lowercased
-	// directory.
-	return InHomeDir(fmt.Sprintf(".%s", strings.ToLower(app)))
+	if runtime.GOOS == "android" {
+		return fmt.Sprintf(".%s", strings.ToLower(app))
+	} else {
+		// It is more common on Linux to expect application related directories
+		// in all lowercase. The lantern wrapper also expects a lowercased
+		// directory.
+		return InHomeDir(fmt.Sprintf(".%s", strings.ToLower(app)))
+	}
 }
 
 func logs(app string) string {
