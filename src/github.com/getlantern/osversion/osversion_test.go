a9a279179837dd869666a3284b27382be16d21cf
Ulysses Aalto <uaalto@getlantern.org>
2015-08-14 17:03:37 +0300

    Windows tests

--- src/github.com/getlantern/osversion/osversion_test.go
+++ src/github.com/getlantern/osversion/osversion_test.go
@@ -7,18 +7,13 @@ import (
 )
 
 func TestString(t *testing.T) {
-	switch runtime.GOOS {
-	case "darwin":
-		str, err := GetString()
-		if err != nil {
-			t.Fatal("Error getting string")
-		}
-		reg := regexp.MustCompile("^([0-9])+\\.([0-9])+\\.([0-9])$")
-		if !reg.MatchString(str) {
-			t.Fatal("Improper string format")
-		}
-	default:
-		t.Fatal("Unsupported OS detected")
+	reg := regexp.MustCompile("^([0-9])+\\.([0-9])+\\.([0-9])$")
+	str, err := GetString()
+	if err != nil {
+		t.Fatal("Error getting string")
+	}
+	if !reg.MatchString(str) {
+		t.Fatal("Improper string format")
 	}
 }
 
@@ -30,6 +25,15 @@ func TestHumanReadable(t *testing.T) {
 			t.Fatal("Error getting string")
 		}
 		reg := regexp.MustCompile("OS X 10\\..+")
+		if !reg.MatchString(str) {
+			t.Fatalf("Improper human readable format: %s", str)
+		}
+	case "windows":
+		str, err := GetHumanReadable()
+		if err != nil {
+			t.Fatal("Error getting string")
+		}
+		reg := regexp.MustCompile("Windows .+")
 		if !reg.MatchString(str) {
 			t.Fatal("Improper human readable format")
 		}
