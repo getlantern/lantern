19bb14f1a04668c77bfdb55a8ea064264d11b3f0
Ulysses Aalto <uaalto@getlantern.org>
2015-08-18 21:15:48 +0300

    Use GetSemanticVersion in Android test app

--- src/github.com/getlantern/osversion/cmd/main_android.go
+++ src/github.com/getlantern/osversion/cmd/main_android.go
@@ -13,13 +13,19 @@ func main() {
 	app.Main(func(a app.App) {
 		str, err := osversion.GetString()
 		if err != nil {
-			log.Println("Error")
+			log.Printf("Error in osversion.GetString: %v", err)
 		}
 		log.Println(str)
 
+		semVer, err := osversion.GetSemanticVersion()
+		if err != nil {
+			log.Printf("Error in osversion.GetSemanticVersion: %v", err)
+		}
+		log.Println(semVer.String())
+
 		str, err = osversion.GetHumanReadable()
 		if err != nil {
-			log.Println("Error")
+			log.Printf("Error in osversion.GetHumanReadable: %v", err)
 		}
 		log.Println(str)
 	})
