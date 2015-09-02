c746da61fd60eeadb9084b51d1f88fe190da09a3
Myles Horton <myles@getlantern.org>
2015-09-01 21:17:47 -0700

    small test for config

---
+++ src/github.com/getlantern/flashlight/config/config_test.go
@@ -0,0 +1,13 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMajorVersion(t *testing.T) {
+	ver := majorVersion("22.0.2")
+
+	assert.Equal(t, "2", ver, "Could not read version")
+}
