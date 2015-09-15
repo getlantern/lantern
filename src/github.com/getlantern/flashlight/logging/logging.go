f081e96fda4d643e92e7ba545d201d4b719dc87f
fffw <fffw@getlantern.org>
2015-09-15 12:27:31 +0800

    code review updates

--- src/github.com/getlantern/flashlight/logging/logging.go
+++ src/github.com/getlantern/flashlight/logging/logging.go
@@ -297,8 +297,8 @@ func NonStopWriter(writers ...io.Writer) io.Writer {
 // It never fails and always return the length of bytes passed in
 func (t *nonStopWriter) Write(p []byte) (int, error) {
 	for _, w := range t.writers {
-		// not checking for errors
-		w.Write(p)
+		// intentionally not checking for errors
+		_, _ = w.Write(p)
 	}
 	return len(p), nil
 }
