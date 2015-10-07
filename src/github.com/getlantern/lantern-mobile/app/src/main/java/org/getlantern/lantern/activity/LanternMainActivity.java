87238bf2fe46d425706cddf221ebeb1234a27804
Todd Blose <todd@lana.io>
2015-10-07 15:07:19 -0700

    use custom share screen

--- src/github.com/getlantern/lantern-mobile/app/src/main/java/org/getlantern/lantern/activity/LanternMainActivity.java
+++ src/github.com/getlantern/lantern-mobile/app/src/main/java/org/getlantern/lantern/activity/LanternMainActivity.java
@@ -2,8 +2,12 @@ package org.getlantern.lantern.activity;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.Context;
+import android.content.pm.LabeledIntent;
+import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -17,10 +21,12 @@ import android.graphics.drawable.TransitionDrawable;
 import android.graphics.PorterDuff; 
 import android.net.VpnService;
 import android.net.Uri;
+import android.content.pm.PackageManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
+import android.text.Html;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -39,6 +45,7 @@ import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.getlantern.lantern.config.LanternConfig;
@@ -161,7 +168,7 @@ public class LanternMainActivity extends ActionBarActivity implements Handler.Ca
         });
 
         menuMap.put("Share", new Command() { 
-            public void runCommand() { shareOption(); } 
+            public void runCommand() { customShareOption(); } 
         });   
 
         // DrawerLayout
@@ -251,13 +258,67 @@ public class LanternMainActivity extends ActionBarActivity implements Handler.Ca
         }
     }
 
-    private void shareOption() {
-        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND); 
-        sharingIntent.setType("text/plain");
-        String shareBody = "You can download Lantern on Android here: ";
-        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Lantern Android");
-        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
-        startActivity(Intent.createChooser(sharingIntent, "Share via"));
+    private void customShareOption() {
+        Resources resources = getResources();
+
+        Intent emailIntent = new Intent();
+        emailIntent.setAction(Intent.ACTION_SEND);
+        // Native email client doesn't currently support HTML, but it doesn't hurt to try in case they fix it
+        emailIntent.putExtra(Intent.EXTRA_TEXT, Html.fromHtml(resources.getString(R.string.share_email_native)));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, resources.getString(R.string.share_email_subject));
+        emailIntent.setType("message/rfc822");
+
+        PackageManager pm = getPackageManager();
+        Intent sendIntent = new Intent(Intent.ACTION_SEND);     
+        sendIntent.setType("text/plain");
+
+
+        Intent openInChooser = Intent.createChooser(emailIntent, resources.getString(R.string.share_chooser_text));
+
+        List<ResolveInfo> resInfo = pm.queryIntentActivities(sendIntent, 0);
+        List<LabeledIntent> intentList = new ArrayList<LabeledIntent>();        
+        
+
+        for (int i = 0; i < resInfo.size(); i++) {
+            // Extract the label, append it, and repackage it in a LabeledIntent
+            ResolveInfo ri = resInfo.get(i);
+            String packageName = ri.activityInfo.packageName;
+            if(packageName.contains("android.email")) {
+                emailIntent.setPackage(packageName);
+            } else if ("com.twitter.android.composer.ComposerActivity".equals(ri.activityInfo.name) || 
+                packageName.contains("facebook") || 
+                "com.tencent.mm.ui.tools.ShareImgUI".equals(ri.activityInfo.name) ||
+                packageName.contains("weibo") ||
+                packageName.contains("mms") || packageName.contains("android.gm")) {
+                
+                Intent intent = new Intent();
+                intent.setComponent(new ComponentName(packageName, ri.activityInfo.name));
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                if(packageName.contains("twitter") || packageName.contains("tencent.mm")) {
+                    intent.putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.share_twitter));
+                } else if(packageName.contains("facebook")) {
+                    // Warning: Facebook IGNORES our text. They say "These fields are intended for users to express themselves. Pre-filling these fields erodes the authenticity of the user voice."
+                    // One workaround is to use the Facebook SDK to post, but that doesn't allow the user to choose how they want to share. We can also make a custom landing page, and the link
+                    // will show the <meta content ="..."> text from that page with our link in Facebook.
+                    intent.putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.share_facebook));
+                } else if(packageName.contains("mms")) {
+                    intent.putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.share_sms));
+                } else if(packageName.contains("android.gm")) { // If Gmail shows up twice, try removing this else-if clause and the reference to "android.gm" above
+                    intent.putExtra(Intent.EXTRA_TEXT, Html.fromHtml(resources.getString(R.string.share_email_gmail)));
+                    intent.putExtra(Intent.EXTRA_SUBJECT, resources.getString(R.string.share_email_subject));               
+                    intent.setType("message/rfc822");
+                }
+
+                intentList.add(new LabeledIntent(intent, packageName, ri.loadLabel(pm), ri.icon));
+            }
+        }
+
+        // convert intentList to array
+        LabeledIntent[] extraIntents = intentList.toArray( new LabeledIntent[ intentList.size() ]);
+
+        openInChooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, extraIntents);
+        startActivity(openInChooser);     
     }
 
     // opens an e-mail message with some default options
