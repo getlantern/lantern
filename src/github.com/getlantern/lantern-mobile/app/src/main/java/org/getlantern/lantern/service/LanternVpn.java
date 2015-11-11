e036248a70030d8b10028ec9882686b9919edafa
Todd Blose <todd@lana.io>
2015-11-11 14:25:23 -0800

    show alert when issue encountered sending e-mail

--- src/github.com/getlantern/lantern-mobile/app/src/main/java/org/getlantern/lantern/service/LanternVpn.java
+++ src/github.com/getlantern/lantern-mobile/app/src/main/java/org/getlantern/lantern/service/LanternVpn.java
@@ -1,49 +1,27 @@
-/*
- * Copyright (C) 2011 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.getlantern.lantern.service;
 
-import android.app.PendingIntent;
 import android.content.Intent;
-import android.net.VpnService;
 import android.os.Handler;
 import android.os.Message;
-import android.os.ParcelFileDescriptor;
 import android.util.Log;
 import android.widget.Toast;
-
 
 import org.getlantern.lantern.config.LanternConfig;
 import org.getlantern.lantern.model.Lantern;
 import org.getlantern.lantern.model.LanternUI;
-import org.getlantern.lantern.android.vpn.Tun2Socks;
+import org.getlantern.lantern.model.VpnBuilder;
 
-public class LanternVpn extends VpnService implements Handler.Callback {
+public class LanternVpn extends VpnBuilder implements Handler.Callback {
     private static final String TAG = "LanternVpn";
 
-    private PendingIntent mConfigureIntent;
     private String mSessionName = "LanternVpn";
 
     private Handler mHandler;
     private Thread mThread;
 
     public static LanternUI UI;
-    private Lantern lantern = null;
 
-    private ParcelFileDescriptor mInterface;
+    private Lantern lantern = null;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -58,7 +36,7 @@ public class LanternVpn extends VpnService implements Handler.Callback {
         // shut down Lantern and close the VPN connection
         if (action.equals(LanternConfig.DISABLE_VPN)) {
 
-            stopLantern();
+            stop();
 
             if (mHandler != null) {
                 mHandler.postDelayed(new Runnable () {
@@ -86,13 +64,12 @@ public class LanternVpn extends VpnService implements Handler.Callback {
         // Make sure we check for null here
         // as on start command can run multiple times
         if (mThread == null || (!mThread.isAlive())) {
-            startLantern();
+            start();
         }
         return START_STICKY;
     }
 
-    private synchronized void startLantern() {
-
+    private synchronized void start() {
         Log.d(TAG, "Loading Lantern library");
         final LanternVpn service = this;
         mThread = new Thread() {
@@ -101,7 +78,7 @@ public class LanternVpn extends VpnService implements Handler.Callback {
                     lantern = new Lantern(service);
                     lantern.start();
                     Thread.sleep(3000);
-                    startRun();
+                    startVpn();
                 } catch (Exception uhe) {
                     Log.e(TAG, "Error starting Lantern with given host: " + uhe);
                 }
@@ -110,28 +87,18 @@ public class LanternVpn extends VpnService implements Handler.Callback {
         mThread.start();
     }
 
-    public void stopLantern() {
+    public void stop() {
         try {
             if (lantern != null) {
                 lantern.stop(); 
                 lantern = null;
             }
-            stopTun2Socks();
+            Log.d(TAG, "Closing VPN interface and stopping Lantern..");
+            super.close();
+            mThread = null;
         } catch (Exception e) {
             Log.e(TAG, "Could not stop Lantern: " + e);
         }
-    }
-
-    public void stopTun2Socks() throws Exception {
-        Log.d(TAG, "Closing VPN interface and stopping Lantern..");
-
-        if (mInterface != null) {
-            mInterface.close();
-            mInterface = null;
-        }
-
-        Tun2Socks.Stop();
-        mThread = null;
     }
 
     @Override
@@ -141,7 +108,7 @@ public class LanternVpn extends VpnService implements Handler.Callback {
         }
 
         try {
-            stopLantern();
+            stop();
         } catch (Exception e) {
 
         }
@@ -155,70 +122,13 @@ public class LanternVpn extends VpnService implements Handler.Callback {
         return true;
     }
 
-    /*@Override
-    public synchronized void run() {
+    private void startVpn() {
         try {
-            if (!isRunning()) {
-                Log.i(TAG, "Starting VPN");
-                startRun();
-            }
-        } catch (Exception e) {
-            Log.e(TAG, "Error trying to start VPN: " + e);
-            throw new RuntimeException("Couldn't configure VPN");
-        }
-    }*/
-
-    private boolean isRunning() {
-        return mInterface != null;
-    }
-
-    private void startRun() throws Exception {
-        try {
-            configure();
+            super.configure();
         } catch (Exception e) {
             Log.e(TAG, "Error with VPN" + e);
         }
 
     }
 
-    // Obtain a interface for the TUN. We can only have one
-    // active TUN interface for a
-    private void configure() throws Exception {
-        // If the old interface has exactly the same parameters, use it!
-        if (mInterface != null) {
-            Log.i(TAG, "Using the previous interface");
-            return;
-        }
-
-        // Configure a builder while parsing the parameters.
-        Builder builder = new Builder();
-        builder.setMtu(1500);
-        builder.addRoute("0.0.0.0", 0);
-        builder.addDnsServer("8.8.4.4");
-        builder.addAddress("10.0.0.1", 28);
-
-        // Close the old interface since the parameters have been changed.
-        try {
-            mInterface.close();
-        } catch (Exception e) {
-            // ignore
-        }
-
-        // Create a new interface using the builder and save the parameters.
-        mInterface = builder.setSession(mSessionName)
-            .setConfigureIntent(mConfigureIntent)
-            .establish();
-
-        Log.i(TAG, "New interface: " + mInterface);
-
-        Tun2Socks.Start(
-                mInterface,
-                1500,
-                "10.0.0.2",
-                "255.255.255.0",
-                "127.0.0.1:" + String.valueOf(LanternConfig.SOCKS_PORT),
-                LanternConfig.UDPGW_SERVER,
-                true
-        );
-    }
 }
