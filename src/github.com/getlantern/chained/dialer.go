78155eda635ef818fe13eefadc3fd4811e7ac6e6
Todd Blose <todd@lana.io>
2015-11-20 15:38:05 -0800

    set auth token

--- src/github.com/getlantern/chained/dialer.go
+++ src/github.com/getlantern/chained/dialer.go
@@ -22,6 +22,9 @@ type Config struct {
 
 	// Label: a optional label for debugging.
 	Label string
+
+	// AuthToken: the authtoken to present to the upstream server
+	Token string
 }
 
 // dialer is an implementation of proxy.Dialer that proxies traffic via an
@@ -68,6 +71,9 @@ func (d *dialer) sendCONNECT(network, addr string, conn net.Conn) error {
 	}
 
 	req, err := buildCONNECTRequest(addr, d.OnRequest)
+
+	req.Header.Set("X-LANTERN-AUTH-TOKEN", d.Token)
+
 	if err != nil {
 		return fmt.Errorf("Unable to construct CONNECT request: %s", err)
 	}
