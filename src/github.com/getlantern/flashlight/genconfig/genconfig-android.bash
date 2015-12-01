4cf3112c6b2baf3dda2ccd05e147f3366f85c4fd
Todd Blose <todd@lana.io>
2015-12-01 00:33:39 -0800

    update genconfig to include fetchcfg option

---
+++ src/github.com/getlantern/flashlight/genconfig/genconfig-android.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function die() {
+  echo $*
+  exit 1
+}
+
+go run genconfig.go -blacklist="blacklist.txt" -masquerades="masquerades.txt" -proxiedsites="proxiedsites" -fetchcfg || die "Could not generate config?"
+
+mkdir lantern-yaml-temp || die "Could not make directory"
+cp lantern.yaml lantern-yaml-temp || die "Could not copy yaml"
+go install github.com/getlantern/tarfs/tarfs || die "Could not install tarfs"
+echo "// +build !stub" > ../config/resources.go
+tarfs -pkg config lantern-yaml-temp >> ../config/resources.go
+
+rm -rf lantern-yaml-temp    
