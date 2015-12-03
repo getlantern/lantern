47a4566cf7564cd68e22fb6eaec99cd1deb33959
fffw <fffw@getlantern.org>
2015-12-03 15:56:21 +0800

    miscellaneous updates to lantern-ui

--- src/github.com/getlantern/lantern-ui/gulpfile.js
+++ src/github.com/getlantern/lantern-ui/gulpfile.js
@@ -1,5 +1,6 @@
 (function () {
   'use strict';
+  var console = require('console');
   var gulp = require('gulp');
   var compass = require('gulp-compass');
   var usemin = require('gulp-usemin');
@@ -8,8 +9,10 @@
   var minifyCss = require('gulp-minify-css');
   var rev = require('gulp-rev');
   var livereload = require('gulp-livereload');
+  var ngConfig = require('gulp-ng-config');
   var del = require('del');
-  var path = require('path');
+  var fs = require('fs');
+  var raml_mock = require('raml-mocker-server');
 
   var scssGlob = 'app/scss/*.scss';
 
@@ -18,11 +21,10 @@
     .pipe(compass({
       config_file: 'config/compass.rb',
       css: 'app/_css'
-    }))
-    .pipe(gulp.dest('app/assets/temp'));
+    }));
   });
 
-  gulp.task('usemin', ['compass', 'clean'], function () {
+  gulp.task('usemin', ['compass', 'dist-env', 'clean'], function () {
     return gulp.src('app/index.html')
     .pipe(usemin({
       css: [minifyCss(), 'concat', rev()],
@@ -34,8 +36,6 @@
   });
 
   gulp.task('copy', ['clean'], function () {
-    gulp.src('app/data/*')
-    .pipe(gulp.dest('dist/data'));
     gulp.src('app/font/*')
     .pipe(gulp.dest('dist/font'));
     gulp.src('app/locale/*')
@@ -54,14 +54,46 @@
     // place code for your default task here
   });
 
-  //watch
+  gulp.task('mock', function() {
+    fs.access('pro-spec', function(err) {
+      if (err) {
+        console.log('please `ln -s` pro-spec folder first!');
+      } else {
+        raml_mock({
+          path: "pro-spec",
+          port: 3030,
+          debug: true,
+          watch: true
+        });
+      }
+    });
+  });
+
+  gulp.task('dev-env', function() {
+    gulp.src('config/env.json')
+    .pipe(ngConfig('app.constants', {
+      environment: 'dev',
+      createModule: false
+    }))
+    .pipe(gulp.dest('app/js/'));
+  });
+
+  gulp.task('dist-env', function() {
+    gulp.src('config/env.json')
+    .pipe(ngConfig('app.constants', {
+      environment: 'dist',
+      createModule: false
+    }))
+    .pipe(gulp.dest('app/js/'));
+  });
+
   gulp.task('watch', function() {
     livereload.listen();
     //watch .scss files
     gulp.watch(scssGlob, ['compass']);
   });
 
-  gulp.task('default', ['watch'], function() {
+  gulp.task('default', ['watch', 'dev-env', 'mock'], function() {
     // place code for your default task here
   });
 }());
