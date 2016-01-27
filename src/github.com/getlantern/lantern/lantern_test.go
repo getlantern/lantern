344324f4506b84c19f73865c5182190215bd2d1f
Ox Cart <ox.to.a.cart@gmail.com>
2016-01-27 15:58:40 -0600

    Added unit test for lantern library

---
+++ src/github.com/getlantern/lantern/lantern_test.go
@@ -0,0 +1,78 @@
+package lantern
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
+	"testing"
+	"time"
+
+	"github.com/getlantern/testify/assert"
+)
+
+const expectedBody = "Google is built by a large team of engineers, designers, researchers, robots, and others in many different sites across the globe. It is updated continuously, and built with more tools and technologies than we can shake a stick at. If you'd like to help us out, see google.com/careers.\n"
+
+type testProtector struct{}
+
+func TestProxying(t *testing.T) {
+	addr, err := Start("testapp", 5*time.Second)
+	if assert.NoError(t, err, "Should have been able to start lantern") {
+		newAddr, err := Start("testapp", 5*time.Second)
+		if assert.NoError(t, err, "Should have been able to start lantern twice") {
+			if assert.Equal(t, addr, newAddr, "2nd start should have resulted in the same address") {
+				err = testProxiedRequest(newAddr)
+				assert.NoError(t, err, "Proxying request should have worked")
+			}
+		}
+	}
+}
+
+func testProxiedRequest(proxyAddr string) error {
+	var req *http.Request
+
+	req = &http.Request{
+		Method: "GET",
+		URL: &url.URL{
+			Scheme: "http",
+			Host:   "www.google.com",
+			Path:   "http://www.google.com/humans.txt",
+		},
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+		Header: http.Header{
+			"Host": {"www.google.com:80"},
+		},
+	}
+
+	client := &http.Client{
+		Timeout: time.Second * 5,
+		Transport: &http.Transport{
+			Dial: func(n, a string) (net.Conn, error) {
+				//return net.Dial("tcp", "127.0.0.1:9898")
+				return net.Dial("tcp", proxyAddr)
+			},
+		},
+	}
+
+	var res *http.Response
+	var err error
+
+	if res, err = client.Do(req); err != nil {
+		return err
+	}
+
+	var buf []byte
+
+	buf, err = ioutil.ReadAll(res.Body)
+
+	fmt.Printf(string(buf))
+
+	if string(buf) != expectedBody {
+		return errors.New("Expecting another response.")
+	}
+
+	return nil
+}
