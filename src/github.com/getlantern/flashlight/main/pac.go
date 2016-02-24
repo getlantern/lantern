09eb7bb112051c5aaae65804356faf3a54bb9476
Ox Cart <ox.to.a.cart@gmail.com>
2016-02-24 14:41:49 -0600

    Reverted pac.go

--- src/github.com/getlantern/flashlight/main/pac.go
+++ src/github.com/getlantern/flashlight/main/pac.go
@@ -76,27 +76,31 @@ func genPACFile() {
 	}
 	formatter :=
 		`var bypassDomains = %s;
-	function FindProxyForURL(url, host) {
-		if (isPlainHostName(host) // including localhost
-		|| shExpMatch(host, "*.local")) {
-			return "DIRECT";
-		}
-		// only checks plain IP addresses to avoid leaking domain name
-		if (/^[0-9.]+$/.test(host)) {
-			if (isInNet(host, "10.0.0.0", "255.0.0.0") ||
-			isInNet(host, "172.16.0.0",  "255.240.0.0") ||
-			isInNet(host, "192.168.0.0",  "255.255.0.0") ||
-			isInNet(host, "127.0.0.0", "255.255.255.0")) {
+		function FindProxyForURL(url, host) {
+			if (isPlainHostName(host) // including localhost
+			|| shExpMatch(host, "*.local")) {
 				return "DIRECT";
 			}
-		}
-		for (var d in bypassDomains) {
-			if (host == bypassDomains[d]) {
+			// only checks plain IP addresses to avoid leaking domain name
+			if (/^[0-9.]+$/.test(host)) {
+				if (isInNet(host, "10.0.0.0", "255.0.0.0") ||
+				isInNet(host, "172.16.0.0",  "255.240.0.0") ||
+				isInNet(host, "192.168.0.0",  "255.255.0.0") ||
+				isInNet(host, "127.0.0.0", "255.255.255.0")) {
+					return "DIRECT";
+				}
+			}
+			// Lantern desktop version proxies only http and https
+			if (url.substring(0, 4) != 'http') {
 				return "DIRECT";
 			}
-		}
-		return "PROXY %s; DIRECT";
-	}`
+			for (var d in bypassDomains) {
+				if (host == bypassDomains[d]) {
+					return "DIRECT";
+				}
+			}
+			return "PROXY %s; DIRECT";
+		}`
 	proxyAddr, ok := client.Addr(5 * time.Minute)
 	if !ok {
 		panic("Unable to get proxy address within 5 minutes")
