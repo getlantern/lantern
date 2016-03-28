608b18e0906b7b082393f800f2d26b255c023a03
Myles Horton <myles@getlantern.org>
2016-03-28 11:08:35 -0700

    moved to 127 instead of localhost to bypass any platform-specific resolution issues closes #3859

--- src/github.com/getlantern/flashlight/main/main.go
+++ src/github.com/getlantern/flashlight/main/main.go
@@ -14,11 +14,11 @@ import (
 	"time"
 
 	"github.com/getlantern/eventual"
+	"github.com/getlantern/flashlight"
 	"github.com/getlantern/golog"
 	"github.com/getlantern/i18n"
 	"github.com/getlantern/profiling"
 
-	"github.com/getlantern/flashlight"
 	"github.com/getlantern/flashlight/analytics"
 	"github.com/getlantern/flashlight/autoupdate"
 	"github.com/getlantern/flashlight/client"
@@ -154,12 +154,12 @@ func doMain() error {
 
 		listenAddr := *addr
 		if listenAddr == "" {
-			listenAddr = "localhost:8787"
+			listenAddr = "127.0.0.1:8787"
 		}
 
 		err := flashlight.Run(
 			listenAddr,
-			"localhost:8788",
+			"127.0.0.1:8788",
 			*configdir,
 			*stickyConfig,
 			settings.GetProxyAll,
