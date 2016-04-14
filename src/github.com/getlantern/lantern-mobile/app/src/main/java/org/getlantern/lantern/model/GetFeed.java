49f23ecee96ae0dcccd671268a1608ce4cde61e1
Todd Blose <todd@lana.io>
2016-04-14 14:12:15 -0700

    add accept gzip encoding header

--- src/github.com/getlantern/lantern-mobile/app/src/main/java/org/getlantern/lantern/model/GetFeed.java
+++ src/github.com/getlantern/lantern-mobile/app/src/main/java/org/getlantern/lantern/model/GetFeed.java
@@ -11,46 +11,46 @@ import  java.util.Locale;
 import go.lantern.Lantern;
 
 public class GetFeed extends AsyncTask<String, Void, ArrayList<String>> {
-	private static final String TAG = "GetFeed";
+    private static final String TAG = "GetFeed";
 
-	private LanternMainActivity activity;
-	private String proxyAddr = "";
-	private ArrayList<String> sources = new ArrayList<String>();
+    private LanternMainActivity activity;
+    private String proxyAddr = "";
+    private ArrayList<String> sources = new ArrayList<String>();
 
-	public GetFeed(LanternMainActivity activity, String proxyAddr) {
-		this.activity = activity;
-		this.proxyAddr = proxyAddr;
-	}
+    public GetFeed(LanternMainActivity activity, String proxyAddr) {
+        this.activity = activity;
+        this.proxyAddr = proxyAddr;
+    }
 
-	@Override
-	protected ArrayList<String> doInBackground(String... params) {
-		try {
-			String locale = Locale.getDefault().toString();
-			Log.d(TAG, "Locale is " + locale + " proxy addr is " + proxyAddr);
+    @Override
+    protected ArrayList<String> doInBackground(String... params) {
+        try {
+            String locale = Locale.getDefault().toString();
+            Log.d(TAG, "Locale is " + locale + " proxy addr is " + proxyAddr);
 
-			Lantern.GetFeed(locale, proxyAddr, new Lantern.FeedProvider.Stub() {
-				public void AddSource(String source) {
-					sources.add(source);
-				}
+            Lantern.GetFeed(locale, proxyAddr, new Lantern.FeedProvider.Stub() {
+                public void AddSource(String source) {
+                    sources.add(source);
+                }
 
                 public void DisplayError(String errMsg) {
 
                 }
 
-			});
+            });
 
-			return sources;
+            return sources;
 
-		} catch (Exception e) {
-			Log.v("Error Parsing Data", e + "");
-		}
-		return null;
-	}
+        } catch (Exception e) {
+            Log.v("Error Parsing Data", e + "");
+        }
+        return null;
+    }
 
-	@Override
-	protected void onPostExecute(ArrayList<String> sources) {
-		super.onPostExecute(sources);
-		activity.setupFeed(sources);
-	}
+    @Override
+    protected void onPostExecute(ArrayList<String> sources) {
+        super.onPostExecute(sources);
+        activity.setupFeed(sources);
+    }
 }   
 
