02d4eb77719b332749696b05a633b6fc0dadd83d
Todd Blose <todd@lana.io>
2016-04-13 22:10:01 -0700

    move GetFeed call to onResume

--- src/github.com/getlantern/lantern-mobile/app/src/main/java/org/getlantern/lantern/LanternApp.java
+++ src/github.com/getlantern/lantern-mobile/app/src/main/java/org/getlantern/lantern/LanternApp.java
@@ -9,34 +9,11 @@ import android.util.Log;
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 
-import org.lantern.mobilesdk.Lantern;
-import org.lantern.mobilesdk.StartResult;
-import org.lantern.mobilesdk.LanternNotRunningException;
-
-public class LanternApp extends Application implements Application.ActivityLifecycleCallbacks {
+public class LanternApp extends Application {
     private static final String TAG = "LanternApp";
 
     @Override
     public void onCreate() {
-        registerActivityLifecycleCallbacks(this);
         Fabric.with(this, new Crashlytics());
     }
-
-    public void onActivityResumed(Activity activity) {
-        Log.d(TAG, "Activity resumed");
-    }
-
-    // Below unused
-    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {}
-
-    public void onActivityDestroyed(Activity activity) {}
-
-    public void onActivityPaused(Activity activity) {}
-
-    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {}
-
-    public void onActivityStarted(Activity activity) {}
-
-    public void onActivityStopped(Activity activity) {}
-
 }
