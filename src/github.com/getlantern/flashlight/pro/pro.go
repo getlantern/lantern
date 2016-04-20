e552c6c172e59389de082298f543ab0ed4235b21
José Carlos Nieto <jose.carlos@menteslibres.net>
2016-04-20 22:17:15 +0000

    Proxying requests to PRO server.

---
+++ src/github.com/getlantern/flashlight/pro/pro.go
@@ -0,0 +1,68 @@
+package pro
+
+import (
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/http/httputil"
+	"sync"
+
+	"github.com/getlantern/eventual"
+	"github.com/getlantern/flashlight/util"
+)
+
+const (
+	proAPIHost = "quiet-island-5559.herokuapp.com"
+)
+
+var (
+	cf   util.HTTPFetcher
+	cfMu sync.RWMutex
+)
+
+type proxyTransport struct {
+	// Satisfies http.RoundTripper
+}
+
+func (pt *proxyTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	cfMu.RLock()
+	defer cfMu.RUnlock()
+	if cf == nil {
+		return nil, errors.New("Missing client")
+	}
+	if req.Method == "OPTIONS" {
+		// No need to proxy the OPTIONS request.
+		res := &http.Response{
+			StatusCode: http.StatusOK,
+			Header: http.Header{
+				"Connection":                   {"keep-alive"},
+				"Access-Control-Allow-Methods": {"GET, POST"},
+				"Access-Control-Allow-Origin":  {req.Header.Get("Origin")},
+				"Access-Control-Allow-Headers": {req.Header.Get("Access-Control-Request-Headers")},
+				"Via": {"Lantern Client"},
+			},
+			Body: ioutil.NopCloser(bytes.NewBuffer([]byte("preflight complete"))),
+		}
+		return res, nil
+	}
+	return cf.Do(req)
+}
+
+var ProxyHandler = &httputil.ReverseProxy{
+	Transport: &proxyTransport{},
+	Director: func(r *http.Request) {
+		r.URL.Scheme = "https"
+		r.URL.Host = proAPIHost
+		r.Host = r.URL.Host
+		r.RequestURI = "" // http: Request.RequestURI can't be set in client requests.
+		r.Header.Set("Lantern-Fronted-URL", r.URL.String())
+		r.Header.Set("Access-Control-Allow-Headers", "X-Lantern-Device-Id, X-Lantern-Pro-Token, X-Lantern-User-Id")
+	},
+}
+
+func Configure(proxyAddrFN eventual.Getter) {
+	cfMu.Lock()
+	defer cfMu.Unlock()
+	cf = util.NewChainedAndFronted(proxyAddrFN)
+}
--- src/github.com/getlantern/flashlight/ui/proxy.go
+++ src/github.com/getlantern/flashlight/ui/proxy.go
@@ -2,22 +2,13 @@ package ui
 
 import (
 	"net/http"
-	"net/http/httputil"
-)
 
-var proxyHandler = httputil.ReverseProxy{
-	Director: func(r *http.Request) {
-		r.Header.Set("Access-Control-Request-Headers", "X-Lantern-Device-Id, X-Lantern-Pro-Token, X-Lantern-User-Id")
-		r.URL.Scheme = "https"
-		r.URL.Host = "quiet-island-5559.herokuapp.com"
-		r.Host = r.URL.Host
-		log.Debugf("REQUEST: %v", r)
-	},
-}
+	"github.com/getlantern/flashlight/pro"
+)
 
 func initProServer(addr string) {
 	go func() {
-		err := http.ListenAndServe(addr, &proxyHandler)
+		err := http.ListenAndServe(addr, pro.ProxyHandler)
 		log.Fatal(err)
 	}()
 }
