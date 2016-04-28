0dc088b8fe725b59398758efe4652842731efb99
Ox Cart <ox.to.a.cart@gmail.com>
2016-04-28 07:28:45 -0500

    Cleaned up rot13 package

--- src/github.com/getlantern/rot13/rot13.go
+++ src/github.com/getlantern/rot13/rot13.go
@@ -1,26 +1,29 @@
+// Package rot13 provides ROT13 "encryption" and "decryption".
 package rot13
 
 import (
 	"io"
 )
 
-type ROT13Writer struct {
+type rot13Writer struct {
 	wrapped io.Writer
 }
 
-type ROT13Reader struct {
+type rot13Reader struct {
 	wrapped io.Reader
 }
 
+// NewWriter creates a new io.Writer that encrypts with ROT13.
 func NewWriter(wrapped io.Writer) io.Writer {
-	return &ROT13Writer{wrapped}
+	return &rot13Writer{wrapped}
 }
 
+// NewReader creates a new io.Reader that encrypts with ROT13.
 func NewReader(wrapped io.Reader) io.Reader {
-	return &ROT13Reader{wrapped}
+	return &rot13Reader{wrapped}
 }
 
-func (r *ROT13Writer) Write(p []byte) (int, error) {
+func (r *rot13Writer) Write(p []byte) (int, error) {
 	o := make([]byte, len(p))
 	for i := 0; i < len(p); i++ {
 		o[i] = p[i] + 13
@@ -28,7 +31,7 @@ func (r *ROT13Writer) Write(p []byte) (int, error) {
 	return r.wrapped.Write(o)
 }
 
-func (r *ROT13Reader) Read(p []byte) (int, error) {
+func (r *rot13Reader) Read(p []byte) (int, error) {
 	n, err := r.wrapped.Read(p)
 	if err != nil {
 		return n, err
