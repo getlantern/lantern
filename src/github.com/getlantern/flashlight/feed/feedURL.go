372c59923d00e1eb1ae7fd1ce10c374cb3a369d8
Myles Horton <myles@getlantern.org>
2016-04-29 20:45:35 -0700

    added constant for en_US

--- src/github.com/getlantern/flashlight/feed/feedURL.go
+++ src/github.com/getlantern/flashlight/feed/feedURL.go
@@ -13,11 +13,12 @@ const (
 	// the Feed endpoint where recent content is published to
 	// mostly just a compendium of RSS feeds
 	feedEndpoint = "https://feeds.getiantem.org/%s/feed.json"
+	en           = "en_US"
 )
 
 var (
 	log            = golog.LoggerFor("feed")
-	EnFeedEndpoint = fmt.Sprintf(feedEndpoint, "en_US")
+	EnFeedEndpoint = fmt.Sprintf(feedEndpoint, en)
 )
 
 // GetFeedURL returns the URL to use for looking up the feed by looking up
@@ -36,7 +37,7 @@ func determineLocale(defaultLocale string) string {
 	// language are Iran and Malaysia. In both countries english is the most
 	// common language on people's machines. We can therefor optimize a little
 	// bit here and skip the country lookup if the locale is not en_US.
-	if !strings.EqualFold("en_US", defaultLocale) {
+	if !strings.EqualFold(en, defaultLocale) {
 		return defaultLocale
 	}
 	country := geolookup.GetCountry(time.Duration(10) * time.Second)
