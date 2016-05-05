bbabd07e3d14898b956381149344e35173eb057f
Todd Blose <todd@lana.io>
2016-05-05 11:55:30 -0700

    clean-up isEmailValid

--- src/github.com/getlantern/lantern-mobile/app/src/main/java/org/getlantern/lantern/model/Utils.java
+++ src/github.com/getlantern/lantern-mobile/app/src/main/java/org/getlantern/lantern/model/Utils.java
@@ -60,17 +60,10 @@ public class Utils {
     }
 
     public static boolean isEmailValid(String email) {
-        boolean isValid = false;
-
         String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
-        CharSequence inputStr = email;
-
         Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(inputStr);
-        if (matcher.matches()) {
-            isValid = true;
-        }
-        return isValid;
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 
     public static void showAlertDialog(Activity activity, String title, String msg) {
