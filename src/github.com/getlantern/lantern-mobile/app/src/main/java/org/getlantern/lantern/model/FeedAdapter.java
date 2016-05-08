c3ca7237204884fffe0fe65ce67757ac1be326e2
Todd Blose <todd@lana.io>
2016-05-08 16:27:30 -0700

    remove log import

--- src/github.com/getlantern/lantern-mobile/app/src/main/java/org/getlantern/lantern/model/FeedAdapter.java
+++ src/github.com/getlantern/lantern-mobile/app/src/main/java/org/getlantern/lantern/model/FeedAdapter.java
@@ -1,7 +1,6 @@
 package org.getlantern.lantern.model;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
