7ea0b5d5bc4dbaf229d7dca663754ee816e93543
Todd Blose <todd@lana.io>
2016-05-23 16:07:37 -0700

    update segmented color

--- src/github.com/getlantern/lantern-mobile/app/src/main/java/org/lantern/activity/PaymentActivity.java
+++ src/github.com/getlantern/lantern-mobile/app/src/main/java/org/lantern/activity/PaymentActivity.java
@@ -82,7 +82,7 @@ public class PaymentActivity extends FragmentActivity implements ProResponse, Vi
         mContext = this.getApplicationContext();
         session = LanternApp.getSession();
 
-        segmented.setTintColor(getResources().getColor(R.color.pro_blue_color));
+        segmented.setTintColor(getResources().getColor(R.color.pink));
 
         cardBtn.setOnClickListener(this);
         alipayBtn.setOnClickListener(this);
