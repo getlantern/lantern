5ca5a36fedb4f71271400247e769e71a95db4502
Myles Horton <myles@getlantern.org>
2016-06-02 22:14:36 -0700

    Squashed 'src/github.com/getlantern/notifier/' changes from cfccb60..828f818
    828f818 fixed no op
    
    git-subtree-dir: src/github.com/getlantern/notifier
    git-subtree-split: 828f8189afe6cd13ce779ed5efd356ab30535869

--- NoOpNotifier.go
+++ NoOpNotifier.go
@@ -2,18 +2,6 @@
 
 package notify
 
-type emptyNotifier struct {
-}
-
 func newNotifier() (Notifier, error) {
 	return &emptyNotifier{}, nil
-}
-
-type osxNotifier struct {
-	path string
-}
-
-// Notify sends a notification to the user.
-func (n *osxNotifier) Notify(msg *Notification) error {
-	return nil
 }
