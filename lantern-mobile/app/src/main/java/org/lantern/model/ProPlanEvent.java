01acf8777bb85455e554607df2ca01029dae4044
Todd Blose <todd@lana.io>
2016-06-11 13:50:47 -0700

    merge latest from pro backend

---
+++ lantern-mobile/app/src/main/java/org/lantern/model/ProPlanEvent.java
@@ -0,0 +1,30 @@
+package org.lantern.model;
+
+public class ProPlanEvent {
+
+    private String plan;
+    private String description;
+    private boolean bestValue;
+    private long numYears;
+    private long price;
+
+    public ProPlanEvent(String plan, String description, boolean bestValue, long numYears, long price) {
+        this.plan = plan;
+        this.description = description;
+        this.bestValue = bestValue;
+        this.numYears = numYears;
+        this.price = price;
+    }
+
+    public Long numYears() {
+        return numYears;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public String getPlan() {
+        return plan;
+    }
+}
