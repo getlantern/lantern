669a7203443655af476aa338ad1265c03d508b65
Myles Horton <myles@getlantern.org>
2016-07-22 11:49:26 -0700

    more naming fixes

--- src/github.com/getlantern/flashlight/genconfig/genglobal.bash
+++ src/github.com/getlantern/flashlight/genconfig/genglobal.bash
@@ -22,10 +22,10 @@ mv lantern.yaml global.yaml
 cp global.yaml yaml-temp || die "Could not copy yaml"
 go install github.com/getlantern/tarfs/tarfs || die "Could not install tarfs"
 
-tarfs -pkg config -var GlobalConfig yaml-temp >> ../config/global.go
+tarfs -pkg config -var GlobalConfig yaml-temp >> ../config/embeddedGlobal.go
 
 rm -rf yaml-temp
 
-git add ../config/global.go || die "Could not add resources?"
+git add ../config/embeddedGlobal.go || die "Could not add resources?"
 
 echo "Finished generating resources and added ../config/global.go. Please simply commit that file after confirming the process seemed to have correctly generatated everything -- check lantern.yaml in particular, but no need to check that in"
