14a77b907eaedf5135a0d6918be343b4960f8d08
fffw <fffw@getlantern.org>
2016-07-28 12:20:26 +0800

    put generated go file in generated folder

--- src/github.com/getlantern/flashlight/genconfig/genglobal.bash
+++ src/github.com/getlantern/flashlight/genconfig/genglobal.bash
@@ -11,9 +11,9 @@ go run genconfig.go \
    -masquerades="masquerades.txt" \
    -masquerades-out="../../fronted/masquerades.go" \
    -proxiedsites="proxiedsites" \
-   -proxiedsites-out="../config/proxiedsites.go" \
+   -proxiedsites-out="../config/generated/proxiedsites.go" \
    -fallbacks="fallbacks.yaml" \
-   -fallbacks-out= "../config/fallbacks.go" \
+   -fallbacks-out= "../config/generated/fallbacks.go" \
    \
     || die "Could not generate config?"
 
@@ -22,10 +22,10 @@ mv lantern.yaml global.yaml
 cp global.yaml yaml-temp || die "Could not copy yaml"
 go install github.com/getlantern/tarfs/tarfs || die "Could not install tarfs"
 
-tarfs -pkg config -var GlobalConfig yaml-temp >> ../config/embeddedGlobal.go
+tarfs -pkg generated -var GlobalConfig yaml-temp > ../config/generated/embeddedGlobal.go
 
 rm -rf yaml-temp
 
-git add ../config/embeddedGlobal.go || die "Could not add resources?"
+git add ../config/generated/embeddedGlobal.go || die "Could not add resources?"
 
-echo "Finished generating resources and added ../config/global.go. Please simply commit that file after confirming the process seemed to have correctly generatated everything -- check lantern.yaml in particular, but no need to check that in"
+echo "Finished generating resources and added ../config/generated/global.go. Please simply commit that file after confirming the process seemed to have correctly generatated everything -- check lantern.yaml in particular, but no need to check that in"
--- src/github.com/getlantern/flashlight/genconfig/genproxies.bash
+++ src/github.com/getlantern/flashlight/genconfig/genproxies.bash
@@ -5,8 +5,20 @@ function die() {
   exit 1
 }
 
+if [ -z "$LANTERN_AWS_PATH" ]; then
+  echo "LANTERN_AWS_PATH is not set, defaults to $HOME/lantern_aws"
+  lantern_aws_path=$HOME/lantern_aws
+else
+  lantern_aws_path=$LANTERN_AWS_PATH
+fi
+
+etc=$lantern_aws_path/etc
+if [ ! -d "$etc" ]; then
+  die "$etc doesn't exist or is not a directory"
+fi
+
 echo "Generating proxies..."
-cd $HOME/lantern_aws/etc
+cd $etc
 git checkout master || die "Could not checkout master?"
 git pull || die "Could not pull latest code?"
 ./fetchcfg.py sea > proxies.yaml || die "Could not fetch proxy in sea region?"
@@ -16,11 +28,11 @@ cd -
 go install github.com/getlantern/tarfs/tarfs || die "Could not install tarfs"
 
 mkdir proxies-yaml-temp || die "Could not make proxies temp dir"
-cp $HOME/lantern_aws/etc/proxies.yaml proxies-yaml-temp || die "Could not copy proxies.yaml"
+cp $etc/proxies.yaml proxies-yaml-temp || die "Could not copy proxies.yaml"
 
-tarfs -pkg config -var EmbeddedProxies proxies-yaml-temp > ../config/embeddedProxies.go
-git add ../config/embeddedProxies.go || die "Could not add proxies?"
+tarfs -pkg generated -var EmbeddedProxies proxies-yaml-temp > ../config/generated/embeddedProxies.go
+git add ../config/generated/embeddedProxies.go || die "Could not add proxies?"
 
 rm -rf proxies-yaml-temp
 
-echo "Finished generating proxies and added ../config/embeddedProxies.go. Please simply commit that file after confirming the process seemed to have correctly generatated everything -- check lantern.yaml in particular, but no need to check that in"
+echo "Finished generating proxies and added ../config/generated/embeddedProxies.go. Please simply commit that file after confirming the process seemed to have correctly generatated everything -- check lantern.yaml in particular, but no need to check that in"
