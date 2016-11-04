3794255c439f36d1e87400301d8ebf9593006d9f
Myles Horton <myles@getlantern.org>
2016-11-03 20:33:53 -0700

    cleanups

--- src/github.com/getlantern/cfscanner/cfscanner.go
+++ src/github.com/getlantern/cfscanner/cfscanner.go
@@ -9,7 +9,6 @@ import (
 	"io/ioutil"
 	"net"
 	"net/http"
-	"net/http/httputil"
 	"os"
 	"strings"
 	"sync"
@@ -17,7 +16,6 @@ import (
 
 	"github.com/getlantern/golog"
 	"github.com/getlantern/keyman"
-	"github.com/getlantern/tlsdialer"
 )
 
 var (
@@ -40,23 +38,10 @@ func webSlurp(url string) ([]byte, error) {
 	return body, nil
 }
 
-type masquerade struct {
-	Domain    string
-	IpAddress string
-	RootCA    *castat
-}
-
 type castat struct {
 	CommonName string
 	Cert       string
 	freq       float64
-}
-
-// DirectDomainTransport is a wrapper struct enabling us to modify the protocol of outgoing
-// requests to make them all HTTP instead of potentially HTTPS, which breaks our particular
-// implemenation of direct domain fronting.
-type DirectDomainTransport struct {
-	http.Transport
 }
 
 func main() {
@@ -156,7 +141,7 @@ func checkIPs(ipch <-chan string, resch chan<- string, wg *sync.WaitGroup) {
 func checkIP(ip string) (string, error) {
 	cfg := tls.Config{InsecureSkipVerify: true}
 	conn, err := tls.DialWithDialer(
-		&net.Dialer{Timeout: 10 * time.Second},
+		&net.Dialer{Timeout: 5 * time.Second},
 		"tcp",
 		ip+":443",
 		&cfg)
@@ -213,11 +198,11 @@ func findVerifiedChain(dnsNames []string, conn *tls.Conn, cfg *tls.Config) (stri
 
 func enumerateRange(cidr string, ch chan<- string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	ip, ipnet, err := net.ParseCIDR(cidr)
+	cidrIP, ipnet, err := net.ParseCIDR(cidr)
 	if err != nil {
 		log.Fatal(err)
 	}
-	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
+	for ip := cidrIP.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
 		if ip.IsGlobalUnicast() {
 			ch <- ip.String()
 		}
@@ -251,135 +236,4 @@ func verifyServerCerts(conn *tls.Conn, serverName string, config *tls.Config) ([
 		opts.Intermediates.AddCert(cert)
 	}
 	return certs[0].Verify(opts)
-}
-
-//XXX: DRY: copied & pasted from genconfigWithVerify.go
-func verifyMasquerade(masq *masquerade) bool {
-	httpClient := NewDirectDomainFronter(masq)
-	country, ip, err := lookupIp(httpClient)
-	if err != nil {
-		log.Errorf("Could not lookup IP: %v", err)
-		return false
-	}
-	log.Debugf("Got country %v and ip %v", country, ip)
-
-	return len(country) > 1
-}
-
-//XXX: DRY: copied & pasted from genconfigWithVerify.go
-func lookupIp(httpClient *http.Client) (string, string, error) {
-	httpClient.Timeout = 60 * time.Second
-
-	var err error
-	var req *http.Request
-	var resp *http.Response
-
-	// Note this will typically be an HTTP client that uses direct domain fronting to
-	// hit our server pool in the Netherlands.
-	if req, err = http.NewRequest("HEAD", "http://nl.fallbacks.getiantem.org", nil); err != nil {
-		return "", "", fmt.Errorf("Could not create request: %q", err)
-	}
-
-	if resp, err = httpClient.Do(req); err != nil {
-		return "", "", fmt.Errorf("Could not get response from server: %q", err)
-	}
-	defer func() {
-		if err := resp.Body.Close(); err != nil {
-			log.Debugf("Unable to close reponse body: %v", err)
-		}
-	}()
-
-	if resp.StatusCode != http.StatusOK {
-		if full, err := httputil.DumpResponse(resp, true); err != nil {
-			log.Errorf("Could not read full response %v", err)
-		} else {
-			log.Errorf("Unexpected response to geo IP lookup: %v", string(full))
-		}
-		return "", "", fmt.Errorf("Unexpected response status %d", resp.StatusCode)
-	}
-
-	ip := resp.Header.Get("Lantern-Ip")
-	country := resp.Header.Get("Lantern-Country")
-
-	log.Debugf("Got IP and country: %v, %v", ip, country)
-	return country, ip, nil
-}
-
-// Creates a new http.Client that does direct domain fronting.
-func NewDirectDomainFronter(masq *masquerade) *http.Client {
-	log.Debugf("Creating new direct domain fronter.")
-	return &http.Client{
-		Transport: &DirectDomainTransport{
-			Transport: http.Transport{
-				Dial: func(network, addr string) (net.Conn, error) {
-					log.Debugf("Dialing %s with direct domain fronter", addr)
-					return dialServerWith(masq)
-				},
-				TLSHandshakeTimeout: 40 * time.Second,
-				DisableKeepAlives:   true,
-			},
-		},
-	}
-}
-
-func dialServerWith(masq *masquerade) (net.Conn, error) {
-	dialTimeout := 30 * time.Second
-
-	// Note - we need to suppress the sending of the ServerName in the client
-	// handshake to make host-spoofing work with Fastly.  If the client Hello
-	// includes a server name, Fastly checks to make sure that this matches the
-	// Host header in the HTTP request and if they don't match, it returns
-	// a 400 Bad Request error.
-	sendServerNameExtension := false
-
-	cwt, err := tlsdialer.DialForTimings(
-		&net.Dialer{
-			Timeout: dialTimeout,
-		},
-		"tcp",
-		addressForServer(masq),
-		sendServerNameExtension,
-		tlsConfig(masq))
-
-	if err != nil && masq != nil {
-		err = fmt.Errorf("Unable to dial masquerade %s: %s", masq.Domain, err)
-	}
-	return cwt.Conn, err
-}
-
-// Get the address to dial for reaching the server
-//func addressForServer(masq *masquerade) string {
-//	return fmt.Sprintf("%s:%d", masq.IpAddress, 443)
-//}
-
-func addressForServer(masq *masquerade) string {
-	return fmt.Sprintf("%s:%d", serverHost(masq), 443)
-}
-
-func serverHost(masq *masquerade) string {
-	if masq.IpAddress != "" {
-		return masq.IpAddress
-	}
-	return masq.Domain
-}
-
-// tlsConfig builds a tls.Config for dialing the upstream host. Constructed
-// tls.Configs are cached on a per-masquerade basis to enable client session
-// caching and reduce the amount of PEM certificate parsing.
-func tlsConfig(masq *masquerade) *tls.Config {
-	/*
-			caCert, err := keyman.LoadCertificateFromPEMBytes([]byte(masq.RootCA.Cert))
-		if err != nil {
-			return nil
-		}
-	*/
-	serverName := masq.Domain
-	tlsConfig := &tls.Config{
-		//ClientSessionCache: tls.NewLRUClientSessionCache(1000),
-		InsecureSkipVerify: true,
-		ServerName:         serverName,
-		//		RootCAs:            caCert.PoolContainingCert(),
-	}
-
-	return tlsConfig
 }
