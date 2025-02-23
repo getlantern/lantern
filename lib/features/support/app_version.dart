55cecc4c6b61bbf4e733d1fc0d3bcee960a9de92
atavism <paul@getlantern.org>
2025-02-23 14:47:07 -0800

    Add support screen

---
+++ lib/features/support/app_version.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:lantern/core/common/app_text_styles.dart';
+
+class AppVersion extends StatelessWidget {
+  final String version;
+
+  const AppVersion({super.key, required this.version});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Version',
+            style: AppTestStyles.bodyMedium.copyWith(
+              height: 23 / 14,
+              letterSpacing: 0,
+            ),
+          ),
+          Text(
+            version,
+            style: AppTestStyles.titleSmall.copyWith(
+              height: 20 / 14,
+              letterSpacing: 0,
+              color: Color(0xFF005F61),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/support/support.dart
@@ -0,0 +1,71 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
+import 'package:lantern/core/common/app_urls.dart';
+import 'package:lantern/core/common/common.dart';
+import 'package:lantern/core/widgets/base_screen.dart';
+import 'package:lantern/features/support/app_version.dart';
+
+@RoutePage(name: 'Support')
+class Support extends HookConsumerWidget {
+  const Support({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final List<AppTile> tiles = [
+      AppTile(
+        icon: Icons.error_outline,
+        label: 'Report an Issue',
+      ),
+      AppTile(
+        icon: Icons.code_outlined,
+        label: 'Diagnostic Logs',
+        dividerPadding: EdgeInsets.symmetric(vertical: 4),
+      ),
+      AppTile.link(
+        icon: Icons.forum_outlined,
+        label: 'Lantern User Forum',
+        url: AppUrls.support,
+      ),
+      AppTile.link(
+        icon: Icons.info_outlined,
+        label: 'Frequently Asked Questions',
+        url: AppUrls.faq,
+      ),
+      AppTile.link(
+        icon: Icons.privacy_tip_outlined,
+        label: 'Privacy Policy',
+        url: AppUrls.privacyPolicy,
+      ),
+      AppTile.link(
+        icon: Icons.description_outlined,
+        label: 'Terms of Service',
+        url: AppUrls.termsOfService,
+        dividerPadding: EdgeInsets.symmetric(vertical: 8),
+      ),
+    ];
+
+    return BaseScreen(
+      title: toBeginningOfSentenceCase('support'.i18n),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Center(
+            child: AppAsset(
+              path: AppImagePaths.supportIllustration,
+              type: AssetType.svg,
+              height: 180.h,
+              width: 180.w,
+            ),
+          ),
+          SizedBox(height: defaultSize),
+          ...tiles,
+        ],
+      ),
+      // TODO: update to use dynamic version
+      bottomNavigationBar: AppVersion(version: '8.1.4'),
+    );
+  }
+}
