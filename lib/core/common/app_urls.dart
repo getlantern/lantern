4866be6fcb842a6ad6f2cf22c4266eb0e41a154c
atavism <paul@getlantern.org>
2025-02-23 14:45:50 -0800

    add app urls and url utils

---
+++ lib/core/common/app_urls.dart
@@ -0,0 +1,7 @@
+class AppUrls {
+  static String baseUrl = 'https://lantern.io';
+  static String support = 'https://support.lantern.io';
+  static String faq = '$baseUrl/faq';
+  static String privacyPolicy = '$baseUrl/privacy';
+  static String termsOfService = '$baseUrl/terms';
+}
