58da88dc9ed0a53b251d38a0ec3b82415e22f45a
atavism <paul@getlantern.org>
2025-03-05 18:20:25 -0800

    use appLogger

--- lib/core/utils/log_utils.dart
+++ lib/core/utils/log_utils.dart
@@ -1,16 +1,17 @@
 import 'dart:io';
+import 'package:lantern/core/services/logger_service.dart';
 import 'package:path_provider/path_provider.dart';
 
 Future<String> getAppLogDirectory() async {
   final libraryDir = await getLibraryDirectory();
   final logDir = Directory("${libraryDir.path}/Logs/Lantern");
 
-  // Ensure the directory exists
+  // Make sure the directory exists
   if (!logDir.existsSync()) {
     logDir.createSync(recursive: true);
   }
 
-  print("Using log directory $logDir");
+  appLogger.debug("Using log directory $logDir");
 
   return logDir.path;
 }
