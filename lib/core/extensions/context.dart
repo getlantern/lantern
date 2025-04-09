b8a91a3941f2640933e97fcb59a34f1dfd400950
Jigar-f <jigar@getlantern.org>
2025-04-09 17:15:49 +0530

    Upate snakebar design.

--- lib/core/extensions/context.dart
+++ lib/core/extensions/context.dart
@@ -1,25 +1,41 @@
 // Extension for showing error SnackBars.
 import 'package:flutter/material.dart';
+import 'package:lantern/core/common/app_colors.dart';
 import 'package:loader_overlay/loader_overlay.dart';
-import 'package:path/path.dart';
+
+import '../common/app_dimens.dart' show defaultPadding;
 
 extension SnackBarExtensions on BuildContext {
-  void showSnackBarError(String message) {
+  void showSnackBarError(String message, {bool closeButton = false}) {
+    final textTheme = Theme.of(this).textTheme.bodyMedium;
     ScaffoldMessenger.of(this).showSnackBar(
-      SnackBar(content: Text(message)),
+      SnackBar(
+        behavior: SnackBarBehavior.floating,
+        padding: defaultPadding,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        backgroundColor: AppColors.blue9,
+        showCloseIcon: closeButton,
+        closeIconColor: AppColors.white,
+        content: Text(
+          message,
+          style: textTheme!.copyWith(
+            color: AppColors.white,
+          ),
+        ),
+        duration: Duration(seconds: 5),
+      ),
     );
   }
 }
 
-
 extension DialogExtensions on BuildContext {
-
   void showLoadingDialog() {
-   loaderOverlay.show();
+    loaderOverlay.show();
   }
 
   void hideLoadingDialog() {
     loaderOverlay.hide();
   }
-
 }
