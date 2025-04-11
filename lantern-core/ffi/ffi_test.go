96517599183cc850eb029525d4f036e409c708a9
atavism <paul@getlantern.org>
2025-04-11 12:47:54 -0700

    Fix tests

--- lantern-core/ffi/ffi_test.go
+++ lantern-core/ffi/ffi_test.go
@@ -5,11 +5,12 @@ import (
 	"testing"
 
 	"github.com/getlantern/radiance"
+	"github.com/getlantern/radiance/client"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateNewServer(t *testing.T) {
-	r, err := radiance.NewRadiance("", nil)
+	r, err := radiance.NewRadiance(client.Options{})
 	if err != nil {
 		slog.Error("Unable to create Radiance", "error", err)
 
