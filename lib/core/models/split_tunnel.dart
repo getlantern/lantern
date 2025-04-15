4ce5e26daa34c1f18fc270534253378c365f3f4f
atavism <paul@getlantern.org>
2025-04-15 04:51:50 -0700

    Add split tunnel model

---
+++ lib/core/models/split_tunnel.dart
@@ -0,0 +1,63 @@
+import 'dart:isolate';
+
+enum SplitTunnelFilterType {
+  domain,
+  domainSuffix,
+  domainKeyword,
+  domainRegex,
+  processName,
+  packageName;
+
+  String get value {
+    switch (this) {
+      case SplitTunnelFilterType.domain:
+        return 'domain';
+      case SplitTunnelFilterType.domainSuffix:
+        return 'domainSuffix';
+      case SplitTunnelFilterType.domainKeyword:
+        return 'domainKeyword';
+      case SplitTunnelFilterType.domainRegex:
+        return 'domainRegex';
+      case SplitTunnelFilterType.processName:
+        return 'processName';
+      case SplitTunnelFilterType.packageName:
+        return 'packageName';
+    }
+  }
+}
+
+enum SplitTunnelActionType {
+  add,
+  remove;
+
+  String get value {
+    switch (this) {
+      case SplitTunnelActionType.add:
+        return 'add';
+      case SplitTunnelActionType.remove:
+        return 'remove';
+    }
+  }
+}
+
+enum SplitTunnelingMode { automatic, manual }
+
+extension SplitTunnelingModeExtension on SplitTunnelingMode {
+  String get displayName {
+    switch (this) {
+      case SplitTunnelingMode.automatic:
+        return "Automatic";
+      case SplitTunnelingMode.manual:
+        return "Manual";
+    }
+  }
+}
+
+class SplitTunnelMessage {
+  final SplitTunnelFilterType type;
+  final String value;
+  final SplitTunnelActionType action;
+  final SendPort replyPort;
+
+  SplitTunnelMessage(this.type, this.value, this.action, this.replyPort);
+}
