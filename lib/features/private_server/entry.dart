478b6710ac3c996b1935ce8cd917de178d32e12a
atavism <paul@getlantern.org>
2025-06-08 13:58:14 -0700

    clean-ups

--- lib/features/private_server/entry.dart
+++ lib/features/private_server/entry.dart
@@ -1,11 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:intl/intl.dart';
 import 'package:lantern/core/common/app_text_styles.dart';
 import 'package:lantern/core/common/common.dart';
+import 'package:lantern/features/private_server/provider_card.dart';
 import 'package:lantern/features/private_server/provider_carousel.dart';
-import 'package:lantern/features/support/app_version.dart';
 
 @RoutePage(name: 'PrivateServerSetup')
 class PrivateServerSetup extends StatelessWidget {
@@ -14,7 +13,7 @@ class PrivateServerSetup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BaseScreen(
-      title: 'server_setup'.i18n,
+      title: 'setup_private_server'.i18n,
       padded: true,
       body: SafeArea(
         child: ListView(
@@ -31,15 +30,13 @@ class PrivateServerSetup extends StatelessWidget {
             SizedBox(height: 16),
             ProviderCarousel(
               cards: [
-                _buildProviderCard(
-                  context,
+                ProviderCard(
                   title: 'server_setup_do'.i18n,
                   price: 'server_setup_do_price'.i18n.fill(['\$8']),
                   onContinue: () {},
                   icon: AppImagePaths.digitalOceanIcon,
                 ),
-                _buildProviderCard(
-                  context,
+                ProviderCard(
                   title: 'server_setup_aws'.i18n,
                   price: 'server_setup_aws_price'.i18n.fill(['\$9']),
                   onContinue: () {},
@@ -50,81 +47,13 @@ class PrivateServerSetup extends StatelessWidget {
             const SizedBox(height: 24),
             ContinueButton(label: 'continue_with_do'.i18n, onPressed: () {}),
             const SizedBox(height: 16),
-            OutlineButton(label: 'server_setup_manual'.i18n, onPressed: () {}),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildProviderCard(
-    BuildContext context, {
-    required String title,
-    required String price,
-    required VoidCallback onContinue,
-    required String icon,
-  }) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16),
-        side: BorderSide(
-          color: AppColors.gray2,
-        ),
-      ),
-      elevation: 4,
-      shadowColor: AppColors.shadowColor,
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            AppTile(
-              icon: icon,
-              label: title,
-              tileTextStyle: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-                color: AppColors.black1,
-              ),
-            ),
-            Divider(color: AppColors.gray2),
-            CheckmarkTile(
-              text: 'handle_configuration'.i18n,
-            ),
-            CheckmarkTile(text: price),
-            CheckmarkTile(text: 'seamless_integration'.i18n),
-            CheckmarkTile(
-              text: 'choose_location'.i18n,
-              trailing:
-                  AppIconButton(path: AppImagePaths.info, onPressed: () => {}),
-            ),
-            CheckmarkTile(
-              text: 'one_month_included'.i18n.fill([1]),
+            OutlineButton(
+              label: 'server_setup_manual'.i18n,
+              onPressed: () {},
             ),
           ],
         ),
       ),
-    );
-  }
-}
-
-class CheckmarkTile extends StatelessWidget {
-  final String text;
-  final Widget? trailing;
-
-  const CheckmarkTile({
-    super.key,
-    required this.text,
-    this.trailing,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return AppTile(
-      icon: AppImagePaths.checkmark,
-      label: text,
-      trailing: trailing,
     );
   }
 }
@@ -153,10 +82,8 @@ class ContinueButton extends StatelessWidget {
       onPressed: onPressed,
       child: Text(
         label,
-        style: TextStyle(
+        style: AppTestStyles.primaryButtonTextStyle.copyWith(
           color: AppColors.gray1,
-          fontSize: 16,
-          fontWeight: FontWeight.w600,
         ),
       ),
     );
--- lib/features/private_server/provider_carousel.dart
+++ lib/features/private_server/provider_carousel.dart
@@ -1,45 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:lantern/core/common/common.dart';
 
-class ProviderCarousel extends StatefulWidget {
+class ProviderCarousel extends HookConsumerWidget {
   final List<Widget> cards;
 
   const ProviderCarousel({super.key, required this.cards});
 
   @override
-  State<ProviderCarousel> createState() => _ProviderCarouselState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final current = useState(0);
+    final controller = usePageController();
+    void goTo(int page) {
+      if (page < 0 || page >= cards.length) return;
+      controller.animateToPage(
+        page,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
 
-class _ProviderCarouselState extends State<ProviderCarousel> {
-  late final PageController _controller;
-  int _current = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = PageController();
-  }
-
-  void _goTo(int page) {
-    if (page < 0 || page >= widget.cards.length) return;
-    _controller.animateToPage(
-      page,
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeInOut,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Column(
       children: [
         SizedBox(
           height: 360,
           child: PageView.builder(
-            controller: _controller,
-            itemCount: widget.cards.length,
-            onPageChanged: (idx) => setState(() => _current = idx),
-            itemBuilder: (context, idx) => widget.cards[idx],
+            controller: controller,
+            itemCount: cards.length,
+            onPageChanged: (idx) => current.value = idx,
+            itemBuilder: (context, idx) => cards[idx],
           ),
         ),
         const SizedBox(height: 12),
@@ -48,8 +38,9 @@ class _ProviderCarouselState extends State<ProviderCarousel> {
           children: [
             IconButton(
               icon: Icon(Icons.arrow_back_ios),
-              onPressed: _current > 0 ? () => _goTo(_current - 1) : null,
-              color: _current > 0 ? Colors.black : Colors.grey[400],
+              onPressed:
+                  current.value > 0 ? () => goTo(current.value - 1) : null,
+              color: current.value > 0 ? Colors.black : Colors.grey[400],
               tooltip: 'previous'.i18n,
               iconSize: 24,
               padding: EdgeInsets.zero,
@@ -58,8 +49,8 @@ class _ProviderCarouselState extends State<ProviderCarousel> {
             const SizedBox(width: 8),
             Row(
               mainAxisSize: MainAxisSize.min,
-              children: List.generate(widget.cards.length, (idx) {
-                final isActive = idx == _current;
+              children: List.generate(cards.length, (idx) {
+                final isActive = idx == current.value;
                 return Container(
                   margin: const EdgeInsets.symmetric(horizontal: 4),
                   width: 12,
@@ -75,10 +66,10 @@ class _ProviderCarouselState extends State<ProviderCarousel> {
             const SizedBox(width: 8),
             IconButton(
               icon: Icon(Icons.arrow_forward_ios),
-              onPressed: _current < widget.cards.length - 1
-                  ? () => _goTo(_current + 1)
+              onPressed: current.value < cards.length - 1
+                  ? () => goTo(current.value + 1)
                   : null,
-              color: _current < widget.cards.length - 1
+              color: current.value < cards.length - 1
                   ? Colors.black
                   : Colors.grey[400],
               tooltip: 'next'.i18n,
