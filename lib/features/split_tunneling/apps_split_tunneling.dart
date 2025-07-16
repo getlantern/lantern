57af05dd85061cc7782c57d212f0f6273a2b12ce
atavism <paul@getlantern.org>
2025-07-16 14:26:09 -0700

    merge latest

--- lib/features/split_tunneling/apps_split_tunneling.dart
+++ lib/features/split_tunneling/apps_split_tunneling.dart
@@ -7,11 +7,11 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:lantern/core/common/app_text_styles.dart';
 import 'package:lantern/core/common/common.dart';
 import 'package:lantern/core/models/app_data.dart';
-import 'package:lantern/core/widgets/loading_indicator.dart';
 import 'package:lantern/core/widgets/search_bar.dart';
 import 'package:lantern/core/widgets/section_label.dart';
 import 'package:lantern/features/split_tunneling/provider/apps_data_provider.dart';
 import 'package:lantern/features/split_tunneling/provider/apps_notifier.dart';
+import 'package:lantern/features/split_tunneling/provider/search_query.dart';
 
 const lanternPackageName = "org.getlantern.lantern";
 
--- lib/features/split_tunneling/website_split_tunneling.dart
+++ lib/features/split_tunneling/website_split_tunneling.dart
@@ -6,6 +6,7 @@ import 'package:lantern/core/common/common.dart';
 import 'package:lantern/core/models/website.dart';
 import 'package:lantern/core/widgets/search_bar.dart';
 import 'package:lantern/core/widgets/section_label.dart';
+import 'package:lantern/features/split_tunneling/provider/search_query.dart';
 import 'package:lantern/features/split_tunneling/provider/website_notifier.dart';
 import 'package:lantern/features/split_tunneling/website_domain_input.dart';
 
