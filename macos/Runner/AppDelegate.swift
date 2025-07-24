da9e73e18d56fc41c17befb20470f205104fe61f
atavism <paul@getlantern.org>
2025-07-24 11:40:30 -0700

    additional macos updates

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -8,16 +8,18 @@ class AppDelegate: FlutterAppDelegate {
 
   private let vpnManager = VPNManager.shared
 
-    
-    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-      return true
-    }
+  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    return true
+  }
 
-    override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-      return true
-    }
-    
+  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    return true
+  }
+
   override func applicationDidFinishLaunching(_ aNotification: Notification) {
+
+    let systemExtensionManager = SystemExtensionManager()
+    systemExtensionManager.activateExtension()
 
     guard let controller = mainFlutterWindow?.contentViewController as? FlutterViewController else {
       fatalError("contentViewController is not a FlutterViewController")
