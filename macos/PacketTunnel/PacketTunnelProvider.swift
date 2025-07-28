6d5b233da95f8d279f6e8ca51e1a4658358ea3fc
atavism <paul@getlantern.org>
2025-07-28 12:55:20 -0700

    use system extension

--- macos/PacketTunnel/PacketTunnelProvider.swift
+++ macos/PacketTunnel/PacketTunnelProvider.swift
@@ -6,31 +6,24 @@
 //
 
 import NetworkExtension
+import OSLog
 
-class PacketTunnelProvider: NEPacketTunnelProvider {
+class PacketTunnelProvider: ExtensionProvider {
 
-    override func startTunnel(options: [String : NSObject]?, completionHandler: @escaping (Error?) -> Void) {
-        // Add code here to start the process of connecting the tunnel.
+    static let logger = Logger(
+        subsystem: "org.getlantern.lantern", category: "PacketTunnelProvider")
+
+    let logger: Logger
+
+    override init() {
+        self.logger = Self.logger
+        logger.log(level: .debug, "PacketTunnel first light")
+        super.init()
     }
-    
-    override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
-        // Add code here to start the process of stopping the tunnel.
-        completionHandler()
+
+    override func startTunnel(options: [String: NSObject]?) async throws {
+        logger.log("PacketTunnelProvider starting tunnel")
+        try await super.startTunnel(options: options)
     }
-    
-    override func handleAppMessage(_ messageData: Data, completionHandler: ((Data?) -> Void)?) {
-        // Add code here to handle the message.
-        if let handler = completionHandler {
-            handler(messageData)
-        }
-    }
-    
-    override func sleep(completionHandler: @escaping () -> Void) {
-        // Add code here to get ready to sleep.
-        completionHandler()
-    }
-    
-    override func wake() {
-        // Add code here to wake up.
-    }
+
 }
