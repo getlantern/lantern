7d09a4a51b730f5d33f42beec843203387af2f81
atavism <paul@getlantern.org>
2025-07-28 10:58:21 -0700

    use system extension

---
+++ macos/OldPacketTunnel/PacketTunnelProvider.swift
@@ -0,0 +1,37 @@
+//
+//  PacketTunnelProvider.swift
+//  PacketTunnel
+//
+//  Created by Todd B on 7/28/25.
+//
+
+import NetworkExtension
+import OSLog
+
+class PacketTunnelProvider: NEPacketTunnelProvider {
+
+    override func startTunnel(options: [String : NSObject]?, completionHandler: @escaping (Error?) -> Void) {
+        // Add code here to start the process of connecting the tunnel.
+    }
+    
+    override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
+        // Add code here to start the process of stopping the tunnel.
+        completionHandler()
+    }
+    
+    override func handleAppMessage(_ messageData: Data, completionHandler: ((Data?) -> Void)?) {
+        // Add code here to handle the message.
+        if let handler = completionHandler {
+            handler(messageData)
+        }
+    }
+    
+    override func sleep(completionHandler: @escaping () -> Void) {
+        // Add code here to get ready to sleep.
+        completionHandler()
+    }
+    
+    override func wake() {
+        // Add code here to wake up.
+    }
+}
