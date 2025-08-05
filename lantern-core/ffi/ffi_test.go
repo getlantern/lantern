290940351f1b4c68c8af336fc7c59d1b7e27915c
atavism <paul@getlantern.org>
2025-08-05 07:02:51 -0700

    combine changes from radiance refactor and system extension

--- lantern-core/ffi/ffi_test.go
+++ lantern-core/ffi/ffi_test.go
@@ -1,28 +1,19 @@
 package main
 
-import (
-	"log/slog"
-	"os"
-	"testing"
+// func radianceOptions() radiance.Options {
+// 	return radiance.Options{
+// 		DataDir:  os.TempDir(),
+// 		LogDir:   os.TempDir(),
+// 		DeviceID: "test-123",
+// 		Locale:   "en-us",
+// 	}
+// }
 
-	"github.com/getlantern/radiance"
-	"github.com/stretchr/testify/assert"
-)
-
-func radianceOptions() radiance.Options {
-	return radiance.Options{
-		DataDir:  os.TempDir(),
-		LogDir:   os.TempDir(),
-		DeviceID: "test-123",
-		Locale:   "en-us",
-	}
-}
-
-func TestCreateNewServer(t *testing.T) {
-	r, err := radiance.NewRadiance(radianceOptions())
-	if err != nil {
-		slog.Error("Unable to create Radiance", "error", err)
-	}
-	assert.Nil(t, err)
-	assert.NotNil(t, r)
-}
+// func TestCreateNewServer(t *testing.T) {
+// 	r, err := radiance.NewRadiance(radianceOptions())
+// 	if err != nil {
+// 		slog.Error("Unable to create Radiance", "error", err)
+// 	}
+// 	assert.Nil(t, err)
+// 	assert.NotNil(t, r)
+// }
