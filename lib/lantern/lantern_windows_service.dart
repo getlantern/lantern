baf2c384acc5b0ac465961e49d89ea3863ed1534
atavism <paul@getlantern.org>
2025-08-21 05:12:59 -0700

    quick test

--- lib/lantern/lantern_windows_service.dart
+++ lib/lantern/lantern_windows_service.dart
@@ -15,7 +15,7 @@ class LanternServiceWindows {
   Future<void> init() async {
     await _pipe.connect();
     _pollTimer?.cancel();
-    _pollTimer = Timer.periodic(const Duration(seconds: 2), (_) async {
+    _pollTimer = Timer.periodic(const Duration(milliseconds: 2), (_) async {
       try {
         final res = await _pipe.call(ServiceCommand.status.wire);
         final s = ((res['state'] as String?) ?? 'disconnected').toLowerCase();
