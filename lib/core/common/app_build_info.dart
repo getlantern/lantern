0c8db8eb765b6de98be7d76dab278aa3f8f0e35d
atavism <paul@getlantern.org>
2025-09-25 12:26:44 -0700

    update app version

---
+++ lib/core/common/app_build_info.dart
@@ -0,0 +1,15 @@
+import 'package:package_info_plus/package_info_plus.dart';
+
+class AppBuildInfo {
+  static String get buildType =>
+      String.fromEnvironment('BUILD_TYPE', defaultValue: 'production');
+  static String get version =>
+      String.fromEnvironment('VERSION', defaultValue: '');
+}
+
+Future<String> resolveAppVersionLabel() async {
+  if (AppBuildInfo.version.isNotEmpty) return AppBuildInfo.version;
+
+  final info = await PackageInfo.fromPlatform();
+  return '${info.version}+${info.buildNumber}';
+}
