ff8fd8a898996ec430839708969bfab775898b7e
atavism <paul@getlantern.org>
2025-10-03 12:20:55 -0700

    Add configure desktop window

---
+++ lib/core/desktop/desktop_window.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+import 'package:lantern/core/common/common.dart';
+import 'package:window_manager/window_manager.dart';
+
+Future<void> configureDesktopWindow() async {
+  if (!Platform.isWindows && !Platform.isLinux && !Platform.isMacOS) return;
+
+  await windowManager.ensureInitialized();
+
+  final opts = const WindowOptions(
+    size: desktopWindowSize,
+    minimumSize: desktopWindowSize,
+    maximumSize: desktopWindowSize,
+    center: true,
+    titleBarStyle: TitleBarStyle.normal,
+  );
+
+  await windowManager.setResizable(false);
+  await windowManager.setPreventClose(true);
+
+  windowManager.waitUntilReadyToShow(opts, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+}
