e583622aa8188c4a85d1333d6e203e90d12a0e7e
atavism <paul@getlantern.org>
2025-11-11 07:54:18 -0800

    Fix issues building list

---
+++ lib/features/split_tunneling/provider/app_list_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_list_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$appListHash() => r'628aa554c6181339d04a460920ddfabd6c0232af';
+
+/// See also [appList].
+@ProviderFor(appList)
+final appListProvider = StreamProvider<List<AppData>>.internal(
+  appList,
+  name: r'appListProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$appListHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef AppListRef = StreamProviderRef<List<AppData>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
