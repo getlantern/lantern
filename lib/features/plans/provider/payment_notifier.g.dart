f74d5b0860f6d4dd7293bc574f99b99c27462faf
atavism <paul@getlantern.org>
2025-11-24 06:37:13 -0800

    updates to enable stripe on Android sideload builds

---
+++ lib/features/plans/provider/payment_notifier.g.dart
@@ -0,0 +1,57 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'payment_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(PaymentNotifier)
+const paymentProvider = PaymentNotifierProvider._();
+
+final class PaymentNotifierProvider
+    extends $NotifierProvider<PaymentNotifier, void> {
+  const PaymentNotifierProvider._()
+      : super(
+          from: null,
+          argument: null,
+          retry: null,
+          name: r'paymentProvider',
+          isAutoDispose: true,
+          dependencies: null,
+          $allTransitiveDependencies: null,
+        );
+
+  @override
+  String debugGetCreateSourceHash() => _$paymentNotifierHash();
+
+  @$internal
+  @override
+  PaymentNotifier create() => PaymentNotifier();
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(void value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<void>(value),
+    );
+  }
+}
+
+String _$paymentNotifierHash() => r'3d7ad82794a174b926d36072deb09c171ee79205';
+
+abstract class _$PaymentNotifier extends $Notifier<void> {
+  void build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    build();
+    final ref = this.ref as $Ref<void, void>;
+    final element = ref.element as $ClassProviderElement<
+        AnyNotifier<void, void>, void, Object?, Object?>;
+    element.handleValue(ref, null);
+  }
+}
