3c2b28c36e7c14651aaf9267f22f6105b5100af6
atavism <paul@getlantern.org>
2026-02-23 10:37:57 -0800

    Disable tray manager during Linux integration tests

--- lib/features/system_tray/system_tray_wrapper.dart
+++ lib/features/system_tray/system_tray_wrapper.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:lantern/core/common/app_build_info.dart';
 import 'package:lantern/features/system_tray/provider/system_tray_notifier.dart';
 
 class SystemTrayWrapper extends ConsumerWidget {
@@ -12,6 +13,9 @@ class SystemTrayWrapper extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    if (AppBuildInfo.disableSystemTray) {
+      return child;
+    }
     ref.watch(systemTrayProvider);
     return child;
   }
