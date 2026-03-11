8b5faa5303efabbe85bcb36fd7bb8cf6badcfcec
atavism <paul@getlantern.org>
2026-03-11 10:14:04 -0700

    Refactor desktop connect smoke tests into shared harness

--- integration_test/vpn/windows_connect_smoke_test.dart
+++ integration_test/vpn/windows_connect_smoke_test.dart
@@ -4,14 +4,11 @@ import 'package:lantern/main.dart' as app;
 
 import 'connect_smoke_harness.dart';
 
-const _enableIpCheck =
-    bool.fromEnvironment('ENABLE_IP_CHECK', defaultValue: false);
-
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('Linux VPN connect/disconnect smoke', (tester) async {
+  testWidgets('Windows VPN connect/disconnect smoke', (tester) async {
     app.main();
-    await runConnectSmokeHarness(tester, enableIpCheck: _enableIpCheck);
+    await runConnectSmokeHarness(tester);
   });
 }
