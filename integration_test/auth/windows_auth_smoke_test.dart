0e50b5476c64d56261dd9733b31092db9170026a
atavism <paul@getlantern.org>
2026-04-02 07:28:44 -0700

    auth flow test updates

---
+++ integration_test/auth/windows_auth_smoke_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'auth_smoke_harness.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('Windows auth smoke suite', (tester) async {
+    await runAuthSmokeHarness(tester);
+  });
+}
