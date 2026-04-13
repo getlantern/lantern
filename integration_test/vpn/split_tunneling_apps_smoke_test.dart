4b0e95ec9058e8cb800256daed038662553d2afe
atavism <paul@getlantern.org>
2026-04-13 13:25:30 -0700

    code review updates

--- integration_test/vpn/split_tunneling_apps_smoke_test.dart
+++ integration_test/vpn/split_tunneling_apps_smoke_test.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:lantern/main.dart' as app;
 
 import 'split_tunneling_apps_smoke_harness.dart';
 
@@ -12,7 +14,10 @@ const _splitTunnelSmokeAppName = String.fromEnvironment(
 );
 
 void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
   testWidgets('Apps split tunneling smoke', (tester) async {
+    await app.main();
     await runSplitTunnelingAppsSmokeHarness(
       tester,
       expectedAppName: _splitTunnelSmokeAppName,
