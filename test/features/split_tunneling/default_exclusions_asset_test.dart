634b105403104167b8057364aae8d88505e32e83
Ilya Yakelzon <reflog@gmail.com>
2026-05-15 18:15:32 +0200

    Use flutter_test in exclusion asset test

--- test/features/split_tunneling/default_exclusions_asset_test.dart
+++ test/features/split_tunneling/default_exclusions_asset_test.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('stealth direct-connection defaults are valid package names', () async {
