f9543645949f538d61fdbd452ca8b29035699d09
garmr-ulfr <104022054+garmr-ulfr@users.noreply.github.com>
2026-07-06 12:08:02 -0700

    feat(mobile): adopt radiance memory monitor, remove Go-side memory logger (#8893)
    * bump radiance - mobile memory monitor
    
    * remove dead memory logger
    
    * remove dangling comment
    
    * point radiance to main

--- lantern-core/mobile/mobile.go
+++ lantern-core/mobile/mobile.go
@@ -308,9 +308,6 @@ func StartIPCServer(platform utils.PlatformInterface, opts *utils.Opts) error {
 		if err := ipcServer.Start(); err != nil {
 			return struct{}{}, err
 		}
-		// Start the Go-side memory logger for the tunnel process, mirroring the
-		// Swift PacketTunnelProvider logger. Stopped from CloseIPCServer.
-		startMemoryLogger()
 		return struct{}{}, nil
 	})
 	return err
@@ -320,7 +317,6 @@ func CloseIPCServer() error {
 	_, err := utils.RunOffCgoStack(func() (struct{}, error) {
 		ipcMu.Lock()
 		defer ipcMu.Unlock()
-		stopMemoryLogger()
 		if ipcBackend != nil {
 			ipcBackend.Close()
 			ipcBackend = nil
